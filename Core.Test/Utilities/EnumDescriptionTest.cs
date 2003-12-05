fb55de83da19237a9e086c297cb69d1bd6d6a4c4
Stefan Wenig <stefan.wenig@rubicon.eu>
2003-12-05 16:21:00 +0000

    Label: "1.1.1.0" User: Stefan.wenig     Date: 28.11.03   Time: 16:43 Labeled

---
+++ Core.Test/Utilities/EnumDescriptionTest.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Threading;
+using System.Globalization;
+using Rubicon;
+using NUnit.Framework;
+
+
+namespace Rubicon.Core.UnitTests
+{
+
+public enum EnumWithDescriptions
+{
+  [EnumDescription("Value One")]
+  Value1 = 1,
+  [EnumDescription("Value 2")]
+  Value2 = 2,
+  [EnumDescription("Value III")]
+  Value3 = 3
+}
+
+[EnumDescriptionResource("Rubicon.Core.UnitTests.Resources.strings")]
+public enum EnumFromResource
+{
+  Value1 = 1,
+  Value2 = 2,
+  Value3 = 3
+}
+
+[TestFixture]
+public class EnumDescriptionTest
+{
+  [SetUp]
+  public void SetUp ()
+  {
+    Thread.CurrentThread.CurrentUICulture = Thread.CurrentThread.CurrentCulture;
+  }
+
+  [Test]
+  public void TestGetDescriptionForEnumWithDescriptions()
+  {
+    // try twice to test caching
+    for (int i = 0; i < 2; ++i)
+    {
+      Assertion.AssertEquals ("Value One", EnumDescription.GetDescription (EnumWithDescriptions.Value1));
+      Assertion.AssertEquals ("Value 2", EnumDescription.GetDescription (EnumWithDescriptions.Value2));
+      Assertion.AssertEquals ("Value III", EnumDescription.GetDescription (EnumWithDescriptions.Value3));
+    }
+  }
+
+  [Test]
+  public void TestGetAvailableValuesForEnumWithDescriptions()
+  {
+    // try twice to test caching
+    for (int i = 0; i < 2; ++i)
+    {
+      EnumValue[] enumValues = EnumDescription.GetAllValues (typeof (EnumWithDescriptions));
+      Assertion.AssertEquals (3, enumValues.Length);
+      Assertion.AssertEquals (EnumWithDescriptions.Value1, enumValues[0].Value);
+      Assertion.AssertEquals ("Value One",                 enumValues[0].Description);
+      Assertion.AssertEquals (EnumWithDescriptions.Value2, enumValues[1].Value);
+      Assertion.AssertEquals ("Value 2",                   enumValues[1].Description);
+      Assertion.AssertEquals (EnumWithDescriptions.Value3, enumValues[2].Value);
+      Assertion.AssertEquals ("Value III",                 enumValues[2].Description);
+    }
+  }
+
+  [Test]
+  public void TestGetDescriptionForEnumFromResource()
+  {
+    Assertion.AssertEquals ("Wert Eins", EnumDescription.GetDescription (EnumFromResource.Value1));
+    Assertion.AssertEquals ("Wert 2", EnumDescription.GetDescription (EnumFromResource.Value2));
+    Assertion.AssertEquals ("Wert III", EnumDescription.GetDescription (EnumFromResource.Value3));
+
+    CultureInfo culture = new CultureInfo ("en-US");
+    Assertion.AssertEquals ("Val1", EnumDescription.GetDescription (EnumFromResource.Value1, culture));
+    Assertion.AssertEquals ("Val2", EnumDescription.GetDescription (EnumFromResource.Value2, culture));
+    Assertion.AssertEquals ("Val3", EnumDescription.GetDescription (EnumFromResource.Value3, culture));
+  }
+
+  [Test]
+  public void TestGetAvailableValuesForEnumFromResource ()
+  {
+    // try twice to test caching
+    for (int i = 0; i < 2; ++i)
+    {
+      EnumValue[] enumValues = EnumDescription.GetAllValues (typeof (EnumFromResource));
+      Assertion.AssertEquals (3, enumValues.Length);
+      Assertion.AssertEquals (EnumFromResource.Value1, enumValues[0].Value);
+      Assertion.AssertEquals ("Wert Eins",             enumValues[0].Description);
+      Assertion.AssertEquals (EnumFromResource.Value2, enumValues[1].Value);
+      Assertion.AssertEquals ("Wert 2",                enumValues[1].Description);
+      Assertion.AssertEquals (EnumFromResource.Value3, enumValues[2].Value);
+      Assertion.AssertEquals ("Wert III",              enumValues[2].Description);
+    }
+  }
+}
+
+}
---
+++ Core.Test/Utilities/WorkContextTest.cs
@@ -0,0 +1,194 @@
+using System;
+using Rubicon;
+using NUnit.Framework;
+
+namespace Rubicon.Core.UnitTests
+{
+
+[TestFixture]
+public class WorkContextTest
+{
+  enum ThrowLocation { main_inside, main_outside, sub1_inside, sub1_outside, sub2_inside, sub2_outside, sub2_1_inside}
+
+  // use inner catch
+
+  [Test]
+  public void TestCatchMainInside ()
+  {
+    Assertion.AssertEquals (
+      "? main",
+      PerformTest (ThrowLocation.main_inside, true));
+  }
+
+  [Test]
+  public void TestCatchMainOutside ()
+  {
+    Assertion.AssertEquals (
+      "",
+      PerformTest (ThrowLocation.main_outside, true));
+  }
+
+  [Test]
+  public void TestCatchSub1Inside()
+  {
+    Assertion.AssertEquals (
+      "main\n" + 
+      "? sub1",
+      PerformTest (ThrowLocation.sub1_inside, true));
+  }
+
+  [Test]
+  public void TestCatchSub1Outside()
+  {
+    Assertion.AssertEquals (
+      "main",
+      PerformTest (ThrowLocation.sub1_outside, true));
+  }
+
+  [Test]
+  public void TestCatchSub2Inside()
+  {
+    Assertion.AssertEquals (
+      "main\n" + 
+      "? sub2",
+      PerformTest (ThrowLocation.sub2_inside, true));
+  }
+
+  [Test]
+  public void TestCatchSub2Outside()
+  {
+    Assertion.AssertEquals (
+      "main",
+      PerformTest (ThrowLocation.sub2_outside, true));
+  }
+
+  [Test]
+  public void TestCatchSub2_1Inside()
+  {
+    Assertion.AssertEquals (
+      "main\n" + 
+      "? sub2\n" + 
+      "? sub2.1",
+      PerformTest (ThrowLocation.sub2_1_inside, true));
+  }
+
+  // do not use inner catch
+
+  [Test]
+  public void TestNoCatchMainInside ()
+  {
+    Assertion.AssertEquals (
+      "? main",
+      PerformTest (ThrowLocation.main_inside, false));
+  }
+
+  [Test]
+  public void TestNoCatchMainOutside ()
+  {
+    Assertion.AssertEquals (
+      "",
+      PerformTest (ThrowLocation.main_outside, false));
+  }
+
+  [Test]
+  public void TestNoCatchSub1Inside()
+  {
+    Assertion.AssertEquals (
+      "? main\n" + 
+      "? sub1",
+      PerformTest (ThrowLocation.sub1_inside, false));
+  }
+
+  [Test]
+  public void TestNoCatchSub1Outside()
+  {
+    Assertion.AssertEquals (
+      "? main",
+      PerformTest (ThrowLocation.sub1_outside, false));
+  }
+
+  [Test]
+  public void TestNoCatchSub2Inside()
+  {
+    Assertion.AssertEquals (
+      "? main\n" + 
+      "? sub2",
+      PerformTest (ThrowLocation.sub2_inside, false));
+  }
+
+  [Test]
+  public void TestNoCatchSub2Outside()
+  {
+    Assertion.AssertEquals (
+      "? main",
+      PerformTest (ThrowLocation.sub2_outside, false));
+  }
+
+  [Test]
+  public void TestNoCatchSub2_1Inside()
+  {
+    Assertion.AssertEquals (
+      "? main\n" + 
+      "? sub2\n" + 
+      "? sub2.1",
+      PerformTest (ThrowLocation.sub2_1_inside, false));
+  }
+
+  private string PerformTest (ThrowLocation location, bool catchInInnerHandler)
+  {
+    try
+    {
+      using (WorkContext ctxMain = WorkContext.EnterNew ("main"))
+      {
+        try
+        {
+          using (WorkContext ctxSub1 = WorkContext.EnterNew ("sub1"))
+          {
+            if (location == ThrowLocation.sub1_inside) throw new Exception (location.ToString());
+            ctxSub1.Done();
+          }
+          if (location == ThrowLocation.sub1_outside) throw new Exception (location.ToString());
+        }
+        catch (Exception e)
+        {
+          if (!catchInInnerHandler)
+            throw;
+          Assertion.AssertEquals (location.ToString(), e.Message);
+          return WorkContext.Stack.ToString();
+        }
+        try
+        {
+          using (WorkContext ctxSub2 = WorkContext.EnterNew ("sub2"))
+          {
+            using (WorkContext ctxSub2_1 = WorkContext.EnterNew ("sub2.1"))
+            {
+              if (location == ThrowLocation.sub2_1_inside) throw new Exception (location.ToString());
+              ctxSub2_1.Done();
+            }
+            if (location == ThrowLocation.sub2_inside) throw new Exception (location.ToString());
+            ctxSub2.Done();
+          }
+          if (location == ThrowLocation.sub2_outside) throw new Exception (location.ToString());
+        }
+        catch (Exception e)
+        {
+          if (!catchInInnerHandler)
+            throw;
+          Assertion.AssertEquals (location.ToString(), e.Message);
+          return WorkContext.Stack.ToString();
+        }
+        if (location == ThrowLocation.main_inside) throw new Exception (location.ToString());
+        ctxMain.Done();
+      }
+      if (location == ThrowLocation.main_outside) throw new Exception (location.ToString());
+      return null;
+    }
+    catch (Exception e)
+    {
+      Assertion.AssertEquals (location.ToString(), e.Message);
+      return WorkContext.Stack.ToString();
+    }
+  }
+}
+
+}
