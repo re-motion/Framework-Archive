9f84a2a5725de91ce9767c0bc0d3352de914c092
Stefan Wenig <stefan.wenig@rubicon.eu>
2003-12-05 16:22:00 +0000

    Label: "1.1.1.0" User: Stefan.wenig     Date: 28.11.03   Time: 16:43 Labeled

--- Core/Utilities/EnumDescription.cs
+++ Core/Utilities/EnumDescription.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Specialized;
 using System.Reflection;
+using System.Resources;
+using System.Globalization;
 
 namespace Rubicon
 {
@@ -25,16 +27,142 @@ public class EnumDescriptionAttribute: Attribute
   }
 }
 
+[AttributeUsage (AttributeTargets.Enum, AllowMultiple = false)]
+public class EnumDescriptionResourceAttribute: Attribute
+{
+  private string _baseName;
+
+  public EnumDescriptionResourceAttribute (string baseName)
+  {
+    _baseName = baseName;
+  }
+
+  public string BaseName
+  {
+    get { return _baseName; }
+  }
+}
+
+public struct EnumValue
+{
+  public readonly Enum Value;
+  public readonly string Description;
+
+  public EnumValue (Enum value, string description)
+  {
+    Value = value;
+    Description = description;
+  }
+}
+
+/// <summary>
+/// Use this class to get the clear text representations of enumeration values.
+/// </summary>
 public sealed class EnumDescription
 {
-  /// <summary>
-  /// IDictionary&lt;Type, IDictionary&lt;System.Enum, string&gt;&gt;
-  /// </summary>
-  private static IDictionary s_typeDescriptions = new HybridDictionary ();
+  /// <summary> IDictionary&lt;Type, IDictionary&lt;System.Enum, string&gt;&gt; </summary>
+  /// <remarks> This is for enums with the EnumDescriptionAttribute on values. </remarks>
+  private static IDictionary s_typeDescriptions;
+
+  /// <summary> IDictionary&lt;string resourceKey, ResourceManager&gt; </summary>
+  /// <remarks> This is for enums with the EnumDescriptionResourceAttribute. </remarks>
+  private static IDictionary s_enumResourceManagers;
+
+  static EnumDescription ()
+  {
+    lock (typeof (EnumDescription))
+    {
+      s_typeDescriptions = new HybridDictionary ();
+      s_enumResourceManagers = new HybridDictionary ();
+    }
+  }
+
+
+  public static EnumValue[] GetAllValues (Type enumType)
+  {
+    return GetAllValues (enumType, null);
+  }
+  
+  public static EnumValue[] GetAllValues (Type enumType, CultureInfo culture)
+  {
+    EnumDescriptionResourceAttribute[] resourceAttributes = (EnumDescriptionResourceAttribute[]) 
+          enumType.GetCustomAttributes (typeof (EnumDescriptionResourceAttribute), false);
+    if (resourceAttributes.Length > 0)
+    {
+      FieldInfo[] fields = enumType.GetFields (BindingFlags.Static | BindingFlags.Public);
+      EnumValue[] values = new EnumValue[fields.Length];
+      ResourceManager rm = GetResourceManager (resourceAttributes[0].BaseName, enumType.Assembly);
+      
+      for (int i = 0; i < fields.Length; ++i)
+      {
+        Enum value = (Enum) fields[i].GetValue(null);
+        string description = rm.GetString (enumType.FullName + "." + value.ToString(), culture);
+        values[i] = new EnumValue (value, description);
+      }
+      return values;
+    }
+    else
+    {
+      IDictionary descriptions = GetOrCreateDescriptions (enumType);
+      EnumValue[] values = new EnumValue[descriptions.Count];
+      int i = 0;
+      foreach (Enum value in descriptions.Keys)
+      {
+        values[i] = new EnumValue (value, (string) descriptions[value]);
+        ++i;
+      }
+      return values;
+    }
+  }
+
+  private static ResourceManager GetResourceManager (string baseName, Assembly assembly)
+  {
+    string resourceKey = baseName + " in " + assembly.FullName;
+
+    ResourceManager rm = (ResourceManager) s_enumResourceManagers[resourceKey];
+    if (rm == null)
+    {
+      lock (typeof (EnumDescription))
+      {
+        rm = (ResourceManager) s_enumResourceManagers[resourceKey];
+        if (rm == null)
+        {
+          rm = new ResourceManager (baseName, assembly, null);
+          s_enumResourceManagers[resourceKey] = rm;
+        }
+      }
+    }
+    return rm;
+  }
 
   public static string GetDescription (System.Enum value)
   {
+    return GetDescription (value, null);
+  }
+
+  public static string GetDescription (System.Enum value, CultureInfo culture)
+  {
     Type enumType = value.GetType();
+    EnumDescriptionResourceAttribute[] resourceAttributes = (EnumDescriptionResourceAttribute[]) 
+          enumType.GetCustomAttributes (typeof (EnumDescriptionResourceAttribute), false);
+    if (resourceAttributes.Length > 0)
+    {
+      ResourceManager rm = GetResourceManager (resourceAttributes[0].BaseName, enumType.Assembly);
+      return rm.GetString (enumType.FullName + "." + value.ToString(), culture);
+    }
+    else
+    {
+      IDictionary descriptions = GetOrCreateDescriptions (enumType);
+      string description = (string) descriptions[value];
+      if (description != null)
+        return description;
+
+      return value.ToString();
+    }
+  }
+
+  private static IDictionary GetOrCreateDescriptions (Type enumType)
+  {
     IDictionary descriptions = (IDictionary) s_typeDescriptions[enumType];
     if (descriptions == null)
     {
@@ -48,11 +176,7 @@ public sealed class EnumDescription
         }
       }
     }
-    string description = (string) descriptions[value];
-    if (description != null)
-      return description;
-
-    return value.ToString();
+    return descriptions;
   }
 
   /// <returns>IDictionary&lt;System.Enum, string&gt;</returns>
