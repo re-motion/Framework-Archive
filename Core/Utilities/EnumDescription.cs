c81f0f72f6b565f95043c66888130d665e5137e8
Stefan Wenig <stefan.wenig@rubicon.eu>
2003-12-12 11:42:00 +0000

    Label: "1.1.3.0" User: Stefan.wenig     Date: 10.12.03   Time: 16:50 Labeled

--- Core/Utilities/EnumDescription.cs
+++ Core/Utilities/EnumDescription.cs
@@ -47,6 +47,10 @@ public struct EnumValue
 {
   public readonly Enum Value;
   public readonly string Description;
+  public long NumericValue
+  {
+    get { return Convert.ToInt64 (Value); }
+  }
 
   public EnumValue (Enum value, string description)
   {
