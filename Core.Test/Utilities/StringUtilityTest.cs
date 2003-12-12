f644e45e8cc514d0039388b502c924811fb45d2d
Stefan Wenig <stefan.wenig@rubicon.eu>
2003-12-12 11:42:00 +0000

    Label: "1.1.3.0" User: Stefan.wenig     Date: 10.12.03   Time: 16:50 Labeled

---
+++ Core.Test/Utilities/StringUtilityTest.cs
@@ -0,0 +1,39 @@
+using System;
+using NUnit.Framework;
+using Rubicon;
+
+namespace Rubicon.Core.UnitTests
+{
+
+[TestFixture]
+public class StringUtilityTest
+{
+  [Test]
+	public void NullToEmpty()
+	{
+    Assertion.AssertEquals (string.Empty, StringUtility.NullToEmpty (null));
+    Assertion.AssertEquals ("1", StringUtility.NullToEmpty ("1"));
+	}
+
+  [Test]
+  public void IsNullOrEmpty()
+  {
+    Assertion.AssertEquals (true, StringUtility.IsNullOrEmpty (null));
+    Assertion.AssertEquals (true, StringUtility.IsNullOrEmpty (""));
+    Assertion.AssertEquals (false, StringUtility.IsNullOrEmpty (" "));
+  }
+
+  [Test]
+  public void AreEqual()
+  {
+    Assertion.AssertEquals (true, StringUtility.AreEqual ("test1", "test1", false));
+    Assertion.AssertEquals (true, StringUtility.AreEqual ("test1", "test1", true));
+    Assertion.AssertEquals (false, StringUtility.AreEqual ("test1", "TEST1", false));
+    Assertion.AssertEquals (true, StringUtility.AreEqual ("test1", "TEST1", true));
+    Assertion.AssertEquals (false, StringUtility.AreEqual ("täst1", "TÄST1", false));
+    Assertion.AssertEquals (true, StringUtility.AreEqual ("täst1", "TÄST1", true));
+  }
+
+}
+
+}
