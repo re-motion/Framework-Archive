7bdd583983e433176f6c82d9d8f56a30298ac992
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2003-12-12 14:28:00 +0000

    Label: "1.1.4.0" User: Stefan.wenig     Date: 12.12.03   Time: 12:44 Labeled

---
+++ Web/Legacy/UI/Controls/ExtendedDropDownList.cs
@@ -0,0 +1,102 @@
+using System;
+using System.Collections;
+using System.Web.UI.WebControls;
+
+using Rubicon;
+using Rubicon.Findit.Globalization.Classes;
+
+namespace Rubicon.Findit.Client.Controls
+{
+public class ExtendedDropDownList : DropDownList, IResourceDispatchTarget
+{
+  public void InitListFromEnum (Type resourceEnumType)
+  {
+    InitListFromEnum (resourceEnumType, -1);
+  }
+
+  public virtual void ClearItems ()
+  {
+    this.Items.Clear ();
+  }
+
+  public void InitListFromEnum (Type resourceEnumType, int selectedValue)
+  {
+    ClearItems ();
+
+    EnumValue[] enumValues = EnumDescription.GetAllValues (resourceEnumType);
+
+    foreach (EnumValue enumValue in enumValues)
+    {
+      
+      this.Add (enumValue.Description, (int) enumValue.NumericValue);
+    }
+
+    if (selectedValue != -1)
+      this.SelectedValue = selectedValue;
+  }
+
+  public void Add (string text, int value)
+  {
+    this.Items.Add (new ListItem (text, value.ToString()));
+  }
+
+  public ListItem GetListItemByValue (string value)
+  {
+    foreach (ListItem item in this.Items)
+    {
+      if (item.Value == value)
+      {
+        return item;
+      }
+    }
+
+    return null;
+  }
+
+  public void Dispatch (IDictionary values)
+  {
+    foreach (DictionaryEntry entry in values)
+    {
+      string key = entry.Key.ToString ();
+      int posColon = key.IndexOf (":");
+      if (posColon >=0)
+      {
+        string value = key.Substring (0, posColon);
+        string text = entry.Value.ToString ();        
+
+        ListItem item = GetListItemByValue (value);
+        if (item != null)
+          item.Text = text;
+      }
+    }
+  }
+
+  public new int SelectedValue
+  {
+    get 
+    { 
+      return int.Parse (base.SelectedValue); 
+    }
+    set 
+    {  
+      for (int i = 0; i < this.Items.Count; ++i)
+      {
+        try
+        {
+          if (value == int.Parse (this.Items[i].Value))
+          {
+            SelectedIndex = i;
+            return;
+          }
+        }
+        catch (FormatException)
+        {
+        }
+      }
+
+      throw new ArgumentOutOfRangeException (
+        "value", value, "List does not contain an item with the specified value.");    
+    }
+  }
+}
+}
