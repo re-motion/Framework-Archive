7643b1d263b8ac326ed21a2544cdb609d20efcd5
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2004-06-04 07:50:00 +0000

    Label: "1.1.5.0" User: Stefan.wenig     Date:  4.01.04   Time: 14:22 Labeled

--- Web/Legacy/UI/Controls/CommandHandler.cs
+++ Web/Legacy/UI/Controls/CommandHandler.cs
@@ -34,14 +34,172 @@ public class CommandHandler : Control, IPostBackEventHandler
       OnCommand (e);
   }
 
+  [Obsolete ("Use GetHref (...) instead.")]
   public string GetPostBackClientHyperlink (string eventArgument)
   { 
     return Page.GetPostBackClientHyperlink (this, eventArgument);
   }
 
+  [Obsolete ("Use GetHref (...) instead.")]
   public string GetPostBackClientHyperlink (int eventArgument)
   { 
     return GetPostBackClientHyperlink (eventArgument.ToString ());
+  }
+
+  public string GetHref (int eventArgument)
+  { 
+    return GetHref (eventArgument.ToString ());
+  }
+
+  public string GetHref (string eventArgument)
+  { 
+    return Page.GetPostBackClientHyperlink (this, eventArgument);
+  }
+
+
+  public HyperLink CreateHyperLink (int eventArgument, string linkText)
+  {
+    return CreateHyperLink (eventArgument.ToString (), linkText);
+  }
+
+  public HyperLink CreateHyperLink (string eventArgument, string linkText)
+  {
+    return CreateHyperLink (eventArgument, linkText, null);
+  }
+
+  public HyperLink CreateHyperLink (int eventArgument, string linkText, string cssClass)
+  {
+    return CreateHyperLink (eventArgument.ToString (), linkText, cssClass);
+  }
+
+  public HyperLink CreateHyperLink (string eventArgument, string linkText, string cssClass)
+  {
+    HyperLink hyperLink = new HyperLink ();
+
+    if (cssClass != null && cssClass != string.Empty)
+      hyperLink.CssClass = "standardLink";
+
+    hyperLink.Text = linkText;
+    hyperLink.NavigateUrl = GetHref (eventArgument);
+
+    return hyperLink;
+  }
+
+  public string GetLink (int eventArgument, string linkText)
+  {
+    return GetLink (eventArgument.ToString (), linkText);
+  }
+
+  public string GetLink (string eventArgument, string linkText)
+  {
+    return GetLink (eventArgument, linkText, null);
+  }
+
+  public string GetLink (int eventArgument, string linkText, string cssClass)
+  {
+    return GetLink (eventArgument.ToString (), linkText, cssClass);
+  }
+
+  public string GetLink (string eventArgument, string linkText, string cssClass)
+  {
+    if (cssClass != null && cssClass != string.Empty)
+    {
+      string link = "<a class=\"{0}\" href=\"{1}\">{2}</a>";
+      return string.Format (link, cssClass, GetHref (eventArgument), linkText);
+    }
+    else
+    {
+      string link = "<a href=\"{0}\">{1}</a>";
+      return string.Format (link, GetHref (eventArgument), linkText);
+    }
+  }
+
+  public HyperLink CreateHyperLinkWithConfirmation (
+      int eventArgument, 
+      string linkText, 
+      string confirmationText)
+  {
+    return CreateHyperLinkWithConfirmation (eventArgument.ToString (), linkText, confirmationText);
+  }
+
+  public HyperLink CreateHyperLinkWithConfirmation (
+      string eventArgument, 
+      string linkText, 
+      string confirmationText)
+  {
+    return CreateHyperLinkWithConfirmation (eventArgument, linkText, confirmationText, null);
+  }
+
+  public HyperLink CreateHyperLinkWithConfirmation (
+      int eventArgument, 
+      string linkText, 
+      string confirmationText,
+      string cssClass)
+  {
+    return CreateHyperLinkWithConfirmation (eventArgument.ToString (), linkText, confirmationText, cssClass);
+  }
+
+  public HyperLink CreateHyperLinkWithConfirmation (
+      string eventArgument, 
+      string linkText, 
+      string confirmationText,
+      string cssClass)
+  {
+    HyperLink hyperLink = new HyperLink ();
+
+    if (cssClass != null && cssClass != string.Empty)
+      hyperLink.CssClass = "standardLink";
+
+    hyperLink.Text = linkText;
+    hyperLink.NavigateUrl = "#";
+
+    string onClick = string.Format (
+        "if confirm ({0}) {1}", confirmationText, Page.GetPostBackEventReference (this, eventArgument));
+
+    hyperLink.Attributes["onClick"] = onClick;
+
+    return hyperLink;
+  }
+
+  public string GetLinkWithConfirmation (int eventArgument, string linkText, string confirmationText)
+  {
+    return GetLinkWithConfirmation (eventArgument.ToString (), linkText, confirmationText);
+  }
+
+  public string GetLinkWithConfirmation (string eventArgument, string linkText, string confirmationText)
+  {
+    return GetLinkWithConfirmation (eventArgument, linkText, confirmationText, null);
+  }
+
+  public string GetLinkWithConfirmation (
+      int eventArgument, 
+      string linkText, 
+      string confirmationText, 
+      string cssClass)
+  {
+    return GetLinkWithConfirmation (eventArgument.ToString (), linkText, confirmationText, cssClass);
+  }
+
+  public string GetLinkWithConfirmation (
+      string eventArgument, 
+      string linkText, 
+      string confirmationText, 
+      string cssClass)
+  {
+    if (cssClass != null && cssClass != string.Empty)
+    {
+      string link = "<a class=\"{0}\" href=\"#\" onClick=\"if confirm ({1}) {2}\">{3}</a>";
+      
+      return string.Format (link, cssClass, confirmationText,
+          Page.GetPostBackEventReference (this, eventArgument), linkText);
+    }
+    else
+    {
+      string link = "<a href=\"#\" onClick=\"if confirm ({0}) {1}\">{2}</a>";
+
+      return string.Format (link, confirmationText,
+          Page.GetPostBackEventReference (this, eventArgument), linkText);
+    }
   }
 }
 }
