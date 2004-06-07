42154df1fa4e51df5d9ad0e4ac4a3d346129de40
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2004-06-07 09:03:00 +0000

    Label: "1.1.5.0" User: Stefan.wenig     Date:  4.01.04   Time: 14:22 Labeled

--- Web/Legacy/UI/Controls/TableControl.cs
+++ Web/Legacy/UI/Controls/TableControl.cs
@@ -3,7 +3,7 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-namespace Rubicon.Findit.Client.Controls
+namespace Rubicon.Web.UI.Controls
 {
 public class TableControl : Control
 { 
