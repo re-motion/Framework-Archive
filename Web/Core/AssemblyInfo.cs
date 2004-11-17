fc12002cc788eae0b42c9e598c492c495c380061
Michael Ketting <michael.ketting@rubicon.eu>
2004-11-17 10:45:00 +0000

    CLS Compliance

--- Web/Core/AssemblyInfo.cs
+++ Web/Core/AssemblyInfo.cs
@@ -14,6 +14,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCopyright("(c) 2004 rubicon informationstechnologie gmbh, www.rubicon-it.com")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
+[assembly: System.CLSCompliant(true)]
 
 //
 // Version information for an assembly consists of the following four values:
