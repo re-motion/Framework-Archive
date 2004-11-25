fce61d6485781b304943719ae07f5645b35b8432
Stefan Wenig <stefan.wenig@rubicon.eu>
2004-11-25 10:25:00 +0000

    Label: "1.1.5.0" User: Stefan.wenig     Date:  4.01.04   Time: 14:22 Labeled

---
+++ Core/Text/CommandLine/CommandLineAttributes.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Reflection;
+using Rubicon;
+using Rubicon.NullableValueTypes;
+
+namespace Rubicon.Text.CommandLine
+{
+
+[AttributeUsage (AttributeTargets.Field | AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+public abstract class CommandLineArgumentAttribute: Attribute
+{
+  private CommandLineArgument _argument;
+
+  #region dummy constructor
+  /// <summary> do not use this constructor </summary>
+  /// <remarks> this constructor is necessary because, even in abstract attribute classes, one constructor must have arguments that meet the constraints of attribute declarations. </remarks>
+  [Obsolete]
+  protected CommandLineArgumentAttribute (int doNotUseThisConstructor)
+  {
+    throw new NotSupportedException();
+  }
+  #endregion
+
+  protected CommandLineArgumentAttribute (CommandLineArgument argument)
+  {
+    _argument = argument;
+  }
+
+  public string Name
+  {
+    get { return _argument.Name; }
+    set { _argument.Name = value; }
+  }
+
+  public bool IsOptional
+  {
+    get { return _argument.IsOptional; }
+    set { _argument.IsOptional = value; }
+  }
+
+  public string Placeholder
+  {
+    get { return _argument.Placeholder; }
+    set { _argument.Placeholder = value; }
+  }
+
+  public string Description
+  {
+    get { return _argument.Description; }
+    set { _argument.Description = value; }
+  }
+
+  public CommandLineArgument Argument
+  {
+    get { return _argument; }
+  }
+
+  public virtual void SetMember (MemberInfo fieldOrProperty)
+  {
+  }
+}
+
+[AttributeUsage (AttributeTargets.Field | AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+public class CommandLineStringArgumentAttribute: CommandLineArgumentAttribute
+{
+  public CommandLineStringArgumentAttribute (bool isOptional)
+    : base (new CommandLineStringArgument (isOptional))
+  {
+  }
+
+  public CommandLineStringArgumentAttribute (string name, bool isOptional)
+    : base (new CommandLineStringArgument (name, isOptional))
+  {
+  }
+}
+
+[AttributeUsage (AttributeTargets.Field | AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+public class CommandLineFlagArgumentAttribute: CommandLineArgumentAttribute
+{
+  public CommandLineFlagArgumentAttribute (string name)
+    : base (new CommandLineFlagArgument (name))
+  {
+  }
+
+  public CommandLineFlagArgumentAttribute (string name, bool defaultValue)
+    : base (new CommandLineFlagArgument (name, defaultValue))
+  {
+  }
+}
+
+
+[AttributeUsage (AttributeTargets.Field | AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+public class CommandLineInt32ArgumentAttribute: CommandLineArgumentAttribute
+{
+  public CommandLineInt32ArgumentAttribute (string name, bool isOptional)
+    : base (new CommandLineInt32Argument (name, isOptional))
+  {
+  }
+
+  public CommandLineInt32ArgumentAttribute (bool isOptional)
+    : base (new CommandLineInt32Argument (isOptional))
+  {
+  }
+}
+
+[AttributeUsage (AttributeTargets.Field | AttributeTargets.Property, Inherited = false, AllowMultiple = false)]
+public class CommandLineEnumArgumentAttribute: CommandLineArgumentAttribute
+{
+  public CommandLineEnumArgumentAttribute (bool isOptional)
+    : base (new CommandLineEnumArgument (isOptional, null))
+  {
+  }
+
+  public CommandLineEnumArgumentAttribute (string name, bool isOptional)
+    : base (new CommandLineEnumArgument (name, isOptional, null))
+  {
+  }
+
+  public override void SetMember (MemberInfo fieldOrProperty)
+  {
+    Type enumType = ReflectionUtility.GetFieldOrPropertyType (fieldOrProperty);
+    if (! enumType.IsEnum)
+      throw new ApplicationException (string.Format ("Attribute {0} can only be applied to enumeration fields or properties.", typeof (CommandLineEnumArgumentAttribute).FullName));
+    ((CommandLineEnumArgument) Argument).EnumType = enumType;
+  }
+}
+
+}
---
+++ Core/Text/CommandLine/CommandLineClassParser.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Globalization;
+using System.Text;
+using System.Collections;
+using System.Reflection;
+using Rubicon.Text;
+using Rubicon.NullableValueTypes;
+
+namespace Rubicon.Text.CommandLine
+{
+
+public class CommandLineClassParser: CommandLineParser
+{
+  private Type _argumentClass;
+
+  /// <summary> IDictionary &lt;CommandLineArgument, MemberInfo&gt; </summary>
+  private IDictionary _arguments;
+  
+  public CommandLineClassParser (Type argumentClass)
+  {
+    _argumentClass = argumentClass;
+    _arguments = new System.Collections.Specialized.ListDictionary();
+
+    foreach (MemberInfo member in argumentClass.GetMembers (BindingFlags.Public | BindingFlags.Instance))
+    {
+      if (member.MemberType == MemberTypes.Field || member.MemberType == MemberTypes.Property)
+      {
+        CommandLineArgumentAttribute argumentAttribute = (CommandLineArgumentAttribute) ReflectionUtility.GetSingleAttribute (
+            member, typeof (CommandLineArgumentAttribute), false, false);
+        if (argumentAttribute != null)
+        {
+          argumentAttribute.SetMember (member);
+          this.Arguments.Add (argumentAttribute.Argument);
+          _arguments.Add (argumentAttribute.Argument, member);
+        }
+      }
+    }
+  }
+
+  public new object Parse (string commandLine, bool includeFirstArgument)
+  {
+    return Parse (SplitCommandLine (commandLine, includeFirstArgument));
+  }
+
+  public new object Parse (string[] args)
+  {
+    base.Parse (args);
+    object obj = Activator.CreateInstance (_argumentClass);
+
+    foreach (DictionaryEntry entry in _arguments)
+    {
+      CommandLineArgument argument = (CommandLineArgument) entry.Key;
+      MemberInfo fieldOrProperty = (MemberInfo) entry.Value;
+      Type memberType = ReflectionUtility.GetFieldOrPropertyType (fieldOrProperty);
+      object value = argument.ValueObject;
+      if (value is NaBoolean && memberType == typeof (bool))
+      {
+        NaBoolean naboolval = (NaBoolean) value;
+        if (naboolval.IsNull)
+          throw new ApplicationException (string.Format ("{0} {1}: Cannot convert Rubicon.NaBoolean.Null to System.Boolean. Use NaBoolean type for optional attributes without default values.", fieldOrProperty.MemberType, fieldOrProperty.Name));
+        value = (bool) naboolval;
+      }
+      try
+      {
+        ReflectionUtility.SetFieldOrPropertyValue (obj, fieldOrProperty, value);
+      }
+      catch (Exception e)
+      {
+        throw new ApplicationException (string.Format ("Error setting value of {0} {1}: {2}", fieldOrProperty.MemberType, fieldOrProperty.Name, e.Message), e);
+      }
+    }
+    return obj;
+  }
+}
+
+}
