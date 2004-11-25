c572b21367b4ca9a7eb12fdc7047d4824931fcb5
Stefan Wenig <stefan.wenig@rubicon.eu>
2004-11-25 10:25:00 +0000

    Label: "1.1.5.0" User: Stefan.wenig     Date:  4.01.04   Time: 14:22 Labeled

---
+++ Core.Test/Text/CommandLine/CommandLineClassParserTest.cs
@@ -0,0 +1,33 @@
+using System;
+using NUnit.Framework;
+using Rubicon.NullableValueTypes;
+using Rubicon.Text.CommandLine;
+
+namespace Rubicon.UnitTests.Text.CommandLine
+{
+  public class Arguments
+  {
+    [CommandLineStringArgument (true, Placeholder = "source-directory", Description = "Directory to copy from")]
+    public string SourceDirectory;
+
+    [CommandLineStringArgument (true, Placeholder = "destination-directory", Description = "This is the directory to copy to. This is the directory to copy to. This is the directory to copy to. This is the directory to copy to. This is the directory to copy to. This is the directory to copy to. This is the directory to copy to. This is the directory to copy to.")]
+    public string DestinationDirectory;
+
+    [CommandLineFlagArgument ("b", true, Description = "binary copy on (+, default) or off (-)")]
+    public bool CopyBinary;
+
+    [CommandLineEnumArgument ("rep", true)]
+    public TestOption ReplaceTarget;
+  }
+
+  [TestFixture]
+	public class CommandLineClassParserTest
+	{
+    [Test] 
+    public void TestSerializer ()
+    {
+      CommandLineClassParser parser = new CommandLineClassParser (typeof (Arguments));
+      Arguments arguments = (Arguments) parser.Parse ("sdir ddir /b- /rep:y", true);
+    }
+  }
+}
