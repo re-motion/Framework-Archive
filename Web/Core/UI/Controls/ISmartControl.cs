535354ccd767b1e71b1faa3f06b3569b0d0ef664
Stefan Wenig <stefan.wenig@rubicon.eu>
2004-11-30 14:02:00 +0000

    Label: "Version 3.1.8.0" User: Martin.sinaiberger     Date: 15.11.04   Time: 15:44 Labeled

---
+++ Web/Core/UI/Controls/ISmartControl.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.ComponentModel;
+using Rubicon.NullableValueTypes;
+
+namespace Rubicon.Web.UI.Controls
+{
+
+public interface ISmartControl: IComponent
+{
+  NaBoolean IsRequired { get; }
+  string HelpUrl { get; }
+
+  /// <summary>
+  ///   Creates an appropriate validator for this control.
+  /// </summary>
+  BaseValidator[] CreateValidators(); 
+
+  string TargetControlID { get; }
+  string DisplayName { get; }
+}
+
+}
