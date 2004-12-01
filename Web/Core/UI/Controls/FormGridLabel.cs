c479f60f704e4212e2e6f5d0d70c5badcb4b4332
Stefan Wenig <stefan.wenig@rubicon.eu>
2004-12-01 13:56:00 +0000

    Label: "Version 3.1.8.0" User: Martin.sinaiberger     Date: 15.11.04   Time: 15:44 Labeled

---
+++ Web/Core/UI/Controls/FormGridLabel.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.ComponentModel;
+
+namespace Rubicon.Web.UI.Controls
+{
+
+[ToolboxItemFilter("System.Web.UI")]
+public class FormGridLabel: Label, ISmartControl
+{
+  private bool _required = false;
+  private string _helpUrl = null;
+
+  [Category("Behavior")]
+  [DefaultValue (false)]
+  [Description ("Specifies whether this row will be marked as 'required' in FormGrids.")]
+  public bool Required
+  {
+    get { return _required; }
+    set { _required = value; }
+  }
+
+  [Category("Behavior")]
+  [DefaultValue (null)]
+  [Description ("Specifies the relative URL to the row's help text.")]
+  public string HelpUrl
+  {
+    get { return _helpUrl; }
+    set { _helpUrl = value; }
+  }
+
+  [Browsable (false)]
+  public bool IsRequired
+  {
+    get { return _required; }
+  }
+
+  BaseValidator[] ISmartControl.CreateValidators()
+  {
+    return null;
+  }
+
+  string ISmartControl.TargetControlID
+  {
+    get { return ID; }
+  }
+
+  string ISmartControl.DisplayName
+  {
+    get { return base.Text; }
+  }
+}
+
+}
