13034887e77191732bde8d3376e1fd0bae46597c
Stefan Wenig <stefan.wenig@rubicon.eu>
2004-12-07 15:19:00 +0000

    Label: "1.1.9.0" User: Stefan.wenig     Date:  7.12.04   Time: 10:11 Labeled

--- Core/ObjectBinding/BusinessObject.cs
+++ Core/ObjectBinding/BusinessObject.cs
@@ -4,24 +4,11 @@ using System.Xml.Serialization;
 namespace Rubicon.ObjectBinding
 {
 
-public interface IBusinessObject
-{
-  object GetProperty (IBusinessObjectProperty property);
-  void SetProperty (IBusinessObjectProperty property, object value);
-  object this [IBusinessObjectProperty property] { get; set; }
-
-  object GetProperty (string property);
-  void SetProperty (string property, object value);
-  object this[string property] { get; set; }
-
-  IBusinessObjectClass BusinessObjectClass { get; }
-}
-
 public abstract class BusinessObject: IBusinessObject
 {
   public virtual IBusinessObjectProperty GetBusinessObjectProperty (string propertyIdentifier)
   {
-    return BusinessObjectClass.GetProperty (propertyIdentifier);
+    return BusinessObjectClass.GetPropertyDefinition (propertyIdentifier);
   }
 
   public abstract object GetProperty (IBusinessObjectProperty property);
