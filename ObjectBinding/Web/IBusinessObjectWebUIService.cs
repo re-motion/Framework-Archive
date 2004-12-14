1b47fb20a94b9fdea2828db56269b58ba2d229a0
Michael Ketting <michael.ketting@rubicon.eu>
2004-12-14 07:46:00 +0000

    Label: "1.1.13.0" User: Stefan.wenig     Date: 10.12.04   Time: 12:09 Labeled

--- ObjectBinding/Web/IBusinessObjectWebUIService.cs
+++ ObjectBinding/Web/IBusinessObjectWebUIService.cs
@@ -1,13 +1,40 @@
 using System;
+using System.Drawing;
 using Rubicon.ObjectBinding;
 
 namespace Rubicon.ObjectBinding.Web
 {
-	/// <summary>
-	///   Provides services for business object bound web applications
-	/// </summary>
-	public interface IBusinessObjectWebUIService: IBusinessObjectService
-	{
-    string GetIconPath (IBusinessObjectWithIdentity obj);
-	}
+/// <summary>
+///   Provides services for business object bound web applications
+/// </summary>
+public interface IBusinessObjectWebUIService: IBusinessObjectService
+{
+  IconPrototype GetIcon (IBusinessObjectWithIdentity obj);
+}
+
+public sealed class IconPrototype
+{
+  private string _url;
+
+  private Size _size;
+
+  public IconPrototype (string url, Size size)
+  {
+    _url = url;
+    _size = size;
+  }
+
+  public string Url
+  {
+    get { return _url; }
+    set { _url = value; }
+  }
+
+  public Size Size
+  {
+    get { return _size; }
+    set { _size = value; }
+  }
+}
+
 }
