762a0c01bb69f48f53c701edf8492ac77111569c
Michael Ketting <michael.ketting@rubicon.eu>
2004-12-21 16:23:00 +0000

    Label: "1.1.15.0" User: Stefan.wenig     Date: 20.12.04   Time: 14:49 Labeled

--- ObjectBinding/Web.Test/SingleBocTestBasePage.cs
+++ ObjectBinding/Web.Test/SingleBocTestBasePage.cs
@@ -3,6 +3,7 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.UI.HtmlControls;
 using Rubicon.Web.UI;
+using Rubicon.Web.UI.Utilities;
 
 namespace OBWTest
 {
@@ -11,7 +12,10 @@ public class WebFormBase: Page, IResourceUrlResolver
 {
   public string GetResourceUrl (Type definingType, ResourceType resourceType, string relativeUrl)
   {
-    return Server.MapPath (resourceType.Name + "/" + relativeUrl);
+    if (! ControlHelper.IsDesignMode (this, this.Context))
+      return Server.MapPath (resourceType.Name + "/" + relativeUrl);
+    else
+      return resourceType.Name + "/" + relativeUrl;
   }
 }
 
