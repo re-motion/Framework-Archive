81dfcb1ad016847c1abfb06ed4adf14982ef3b22
Michael Ketting <michael.ketting@rubicon.eu>
2004-12-22 13:30:00 +0000

    Label: "1.1.15.0" User: Stefan.wenig     Date: 20.12.04   Time: 14:49 Labeled

---
+++ ObjectBinding/Web/UI/Controls/BocDateTimeValueValidator.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Rubicon.ObjectBinding.Web.Controls
+{
+	/// <summary>
+	/// Summary description for BocDateTimeValueValidator.
+	/// </summary>
+	public class BocDateTimeValueValidator
+	{
+		public BocDateTimeValueValidator()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+	}
+}
