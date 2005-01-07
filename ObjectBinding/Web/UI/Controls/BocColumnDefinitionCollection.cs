97c8bbb4df31bdd03a04cb647b232190a9038630
Michael Ketting <michael.ketting@rubicon.eu>
2005-01-07 14:35:00 +0000

    Label: "1.1.18.0" User: Michael.ketting     Date: 29.12.04   Time:  9:26 Labeled

---
+++ ObjectBinding/Web/UI/Controls/BocColumnDefinitionCollection.cs
@@ -0,0 +1,364 @@
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing.Design;
+using Rubicon.Utilities;
+using Rubicon.ObjectBinding.Web.Design;
+
+namespace Rubicon.ObjectBinding.Web.Controls
+{
+ 
+/// <summary> A collection of <see cref="BocColumnDefinition"/> objects. </summary>
+[Editor (typeof (BocColumnDefinitionCollectionEditor), typeof (UITypeEditor))]
+public sealed class BocColumnDefinitionCollection : IList, ICollection, IEnumerable
+{
+  /// <summary> <see langword="true"/> if <see cref="BeginEdit"/> was called. </summary>
+  private bool _isEditing;
+  /// <summary>
+  ///   <see langword="true"/> if <see cref="_isEditing"/> <see langword="true"/> and the 
+  ///   collection's values got changed.
+  /// </summary>
+  private bool _isChanged;
+  /// <summary> The items contained on the collection. </summary>
+  private ArrayList _items;
+  /// <summary> 
+  ///   The <see cref="IBusinessObjectBoundWebControl"/> to which this collection belongs to.
+  /// </summary>
+  private IBusinessObjectBoundWebControl _ownerControl;
+
+  /// <summary>
+  ///   The event raised after the items contained in the collection have been changed.
+  /// </summary>
+  public event CollectionChangeEventHandler CollectionChanged;
+
+  /// <summary>
+  ///   Constructor.
+  /// </summary>
+  /// <param name="ownerControl">
+  ///   The <see cref="IBusinessObjectBoundWebControl"/> to which this collection belongs to.
+  /// </param>
+  /// <param name="columnDefinitions">
+  ///   The <see cref="BocColumnDefinition"/> objects to initialize the collection with.
+  /// </param>
+  internal BocColumnDefinitionCollection (
+      IBusinessObjectBoundWebControl ownerControl, 
+      BocColumnDefinition[] columnDefinitions)
+    : this (ownerControl)
+  {
+    AddRange (columnDefinitions);
+  }
+
+  /// <summary>
+  ///   Constructor.
+  /// </summary>
+  /// <param name="ownerControl">
+  ///   The <see cref="IBusinessObjectBoundWebControl"/> to which this collection belongs to.
+  /// </param>
+  internal BocColumnDefinitionCollection (IBusinessObjectBoundWebControl ownerControl)
+  {
+    _ownerControl = ownerControl;
+    _items = new ArrayList();
+  }
+
+  /// <summary> Places the collection into edit mode. </summary>
+  /// <remarks> No individual <see cref="CollectionChanged"/> events are raised during edit mode. </remarks>
+  public void BeginEdit()
+  {
+    _isEditing = true;
+  }
+
+  /// <summary> Returns the collection to normal mode. </summary>
+  /// <remarks> A common <see cref="CollectionChanged"/> event is raised if changes 
+  /// occured during edit-mode. </remarks>
+  public void EndEdit()
+  {
+    if (_isEditing)
+    {
+      _isEditing = false;
+      if (_isChanged)
+      {
+        _isChanged = false;
+        OnCollectionChanged (new CollectionChangeEventArgs (CollectionChangeAction.Refresh, null));
+      }
+    }
+  }
+
+  /// <summary> Raises the <see cref="CollectionChanged"/> event. </summary>
+  /// <remarks> Event is only raised if the collection is not in edit mode. </remarks>
+  /// <param name="e"> The <see cref="EventArgs"/> to be passed to the event.</param>
+  private void OnCollectionChanged (CollectionChangeEventArgs e)
+  {
+    if (CollectionChanged != null && !_isEditing)
+      CollectionChanged(this, e);
+  }
+
+  /// <summary> Removes all items from the IList. </summary>
+  public void Clear()
+  {
+    _items.Clear();
+    _isChanged |= _isEditing;
+   OnCollectionChanged (new CollectionChangeEventArgs (CollectionChangeAction.Refresh, null));
+  }
+
+  /// <summary> Determines whether the <see cref="IList"/> contains a specific value. </summary>
+  /// <param name="value"> 
+  ///   The <see cref="Object"/> to locate in the <see cref="IList"/>.
+  /// </param>
+  /// <returns> 
+  ///   <see langword="true"/> if the <see cref="Object"/> is found in the <see cref="IList"/>;
+  ///   otherwise, <see langword="false"/>.
+  /// </returns>
+  bool IList.Contains (object value)
+  {
+    return Contains ((BocColumnDefinition) value);
+  }
+
+  /// <summary> Determines whether the <see cref="IList"/> contains a specific value. </summary>
+  /// <param name="value"> 
+  ///   The <see cref="BocColumnDefinition"/> to locate in the <see cref="IList"/>. 
+  /// </param>
+  /// <returns> 
+  ///   <see langword="true"/> if the <see cref="BocColumnDefinition"/> is found in the 
+  ///   <see cref="IList"/>; otherwise, <see langword="false"/>.
+  /// </returns>
+  public bool Contains (BocColumnDefinition value)
+  { 
+    int index = IndexOf (value); 
+    return (index != -1);
+  }
+
+  /// <summary> Returns an enumerator that can iterate through a collection. </summary>
+  /// <returns> An <see cref="IEnumerator"/> that can be used to iterate through the collection.</returns>
+  public IEnumerator GetEnumerator()
+  {
+    return _items.GetEnumerator();
+  }
+
+  /// <summary> Determines the index of a specific item in the <see cref="IList"/>. </summary>
+  /// <param name="value"> 
+  ///   The <see cref="Object"/> to locate in the <see cref="IList"/>. 
+  /// </param>
+  /// <returns> The index of <paramref name="value"/> if found in the list; otherwise, -1. </returns>
+  int IList.IndexOf (object value)
+  {
+    return IndexOf ((BocColumnDefinition) value);
+  }
+
+  /// <summary> Determines the index of a specific item in the <see cref="IList"/>. </summary>
+  /// <param name="value">
+  ///   The <see cref="BocColumnDefinition"/> to locate in the <see cref="IList"/>. 
+  /// </param>
+  /// <returns> The index of <paramref name="value"/> if found in the list; otherwise, -1. </returns>
+  public int IndexOf (BocColumnDefinition value)
+  { 
+    return _items.IndexOf (value);
+  }
+
+  /// <summary> Adds an item of type <see cref="BocColumnDefinition"/> to the <see cref="IList"/>. </summary>
+  /// <param name="value"> 
+  ///   The <see cref="BocColumnDefinition"/> to add to the <see cref="IList"/>.
+  /// </param>
+  /// <returns> The position into which the new element was inserted. </returns>
+  int IList.Add (object value)
+  {
+    ArgumentUtility.CheckType ("value", value, typeof (BocColumnDefinition));
+    return Add ((BocColumnDefinition) value);
+  }
+
+  /// <summary> Adds an item to the <see cref="IList"/>. </summary>
+  /// <param name="value"> 
+  ///   The <see cref="BocColumnDefinition"/> to add to the <see cref="IList"/>. 
+  /// </param>
+  /// <returns> The position into which the new element was inserted. </returns>
+  public int Add (BocColumnDefinition value)
+  {
+    int count = _items.Add (value);
+    value.OwnerControl = _ownerControl;
+    _isChanged |= _isEditing;
+    OnCollectionChanged (new CollectionChangeEventArgs (CollectionChangeAction.Add, value));
+    return count;
+  }
+
+  /// <summary> Adds <see cref="BocColumnDefinition"/> array to the <see cref="IList"/>. </summary>
+  /// <param name="columnDefinitions"> 
+  ///   The <see cref="BocColumnDefinition"/> array to add to the <see cref="IList"/>. 
+  ///   Must not be <see langword="null"/>.
+  /// </param>
+  public void AddRange (BocColumnDefinition[] columnDefinitions)
+  {
+    ArgumentUtility.CheckNotNull ("columnDefinitions", columnDefinitions);
+
+    BeginEdit();
+    foreach (BocColumnDefinition columnDefinition in columnDefinitions)
+      Add (columnDefinition);
+    EndEdit();
+  }
+
+  /// <summary> Inserts an item to the <see cref="IList"/> at the specified position. </summary>
+  /// <param name="index"> The zero-based index at which <paramref name="value"/> should be inserted. </param>
+  /// <param name="value">
+  ///   The <see cref="BocColumnDefinition"/> to insert into the <see cref="IList"/>.
+  /// </param>
+  void IList.Insert(int index, object value)
+  {
+    ArgumentUtility.CheckType ("value", value, typeof (BocColumnDefinition));
+    Insert (index, (BocColumnDefinition) value);
+  }
+
+  /// <summary> Inserts an item to the <see cref="IList"/> at the specified position. </summary>
+  /// <param name="index"> The zero-based index at which <paramref name="value"/> should be inserted. </param>
+  /// <param name="value">
+  ///   The <see cref="BocColumnDefinition"/> to insert into the <see cref="IList"/>.
+  /// </param>
+  void Insert(int index, BocColumnDefinition value)
+  {
+    _items.Insert (index, value);
+    value.OwnerControl = _ownerControl;
+    _isChanged |= _isEditing;
+    OnCollectionChanged (new CollectionChangeEventArgs (CollectionChangeAction.Add, value));
+  }
+
+  /// <summary> Removes the first occurrence of a specific object from the <see cref="IList"/>. </summary>
+  /// <param name="value"> 
+  ///   The <see cref="BocColumnDefinition"/> to remove from the <see cref="IList"/>. 
+  /// </param>
+  void IList.Remove (object value)
+  { 
+    ArgumentUtility.CheckType ("value", value, typeof (BocColumnDefinition));
+    Remove ((BocColumnDefinition) value);
+  }
+
+  /// <summary> Removes the first occurrence of a specific object from the <see cref="IList"/>. </summary>
+  /// <param name="value"> 
+  ///   The <see cref="BocColumnDefinition"/> to remove from the <see cref="IList"/>. 
+  /// </param>
+  public void Remove (BocColumnDefinition value)
+  {
+    int removeAt = IndexOf (value);
+    if (removeAt == -1) throw new InvalidOperationException("The passed BocColumnDefinition is not part of this collection");
+    RemoveAt(removeAt);
+  }
+
+  /// <summary> Removes the <see cref="IList"/> item at the specified index. </summary>
+  /// <param name="index"> The zero-based index of the item to remove. </param>
+  public void RemoveAt(int index)
+  {
+    BocColumnDefinition columnDefinition = (BocColumnDefinition) _items[index];
+    _items.RemoveAt(index);
+    _isChanged |= _isEditing;
+    OnCollectionChanged(new CollectionChangeEventArgs(CollectionChangeAction.Remove, columnDefinition));
+  }
+
+  /// <summary>
+  ///  Copies the elements of the <see cref="ICollection "/> to an <see cref="Array"/>, 
+  ///  starting at a particular <c>Array</c> index.
+  /// </summary>
+  /// <param name="array">
+  ///   The one-dimensional <see cref="Array"/> that is the destination of the elements copied 
+  ///   from <see cref="ICollection"/>. The <c>Array</c> must have zero-based indexing.
+  /// </param>
+  /// <param name="index">
+  ///   The zero-based index in <paramref name="array"/> at which copying begins. 
+  /// </param>
+  public void CopyTo (Array array, int index)
+  {
+    _items.CopyTo (array, index);
+  }
+
+  /// <summary> Copies the elements of the <see cref="IList"/> to a new array. </summary>
+  /// <returns> 
+  ///   An <see cref="BocColumnDefinition"/> array containing the elements of the 
+  ///   <see cref="IList"/>.
+  /// </returns>
+  public BocColumnDefinition[] ToArray()
+  {
+    return (BocColumnDefinition[]) _items.ToArray (typeof (BocColumnDefinition));
+  }
+
+  /// <summary> Gets the element at the specified index. </summary>
+  /// <value> The element at the specified index. </value>
+  /// <exception cref="NotSupportedException"> Thrown if the property is set. </exception>
+  object IList.this [int index]
+  {
+    get { return _items[index]; }
+    set { throw new NotSupportedException(); }
+  }
+
+  /// <summary> Gets the element at the specified index. </summary>
+  /// <value> The element at the specified index. </value>
+  public BocColumnDefinition this[int index]
+  {
+    get { return (BocColumnDefinition) _items[index]; }
+  }
+
+  /// <summary> Gets the number of elements contained in the <see cref="ICollection"/>. </summary>
+  /// <value> The number of elements contained in the <see cref="ICollection"/>. </value>
+  [Browsable(false)]
+  [DesignerSerializationVisibility (DesignerSerializationVisibility.Hidden)]
+  [EditorBrowsable(EditorBrowsableState.Advanced)]
+  public int Count
+  {
+    get { return _items.Count; } 
+  }
+
+  /// <summary> Gets a value indicating whether the <see cref="IList"/> is read-only. </summary>
+  /// <value> <see langword="false"/>, the <see cref="IList"/> is not read-only. </value>
+  [Browsable(false)]
+  [DesignerSerializationVisibility (DesignerSerializationVisibility.Hidden)]
+  [EditorBrowsable(EditorBrowsableState.Advanced)]
+  public bool IsReadOnly
+  { 
+    get { return false; }
+  }
+
+  /// <summary>
+  ///   Gets a value indicating whether access to the <see cref="ICollection "/> is synchronized 
+  ///   (thread-safe).
+  /// </summary>
+  /// <value> <see langword="false"/>, access is not synchronized. </value>
+  [Browsable(false)]
+  [DesignerSerializationVisibility (DesignerSerializationVisibility.Hidden)]
+  [EditorBrowsable(EditorBrowsableState.Advanced)]
+  public bool IsSynchronized
+  { 
+    get { return false; }
+  }
+
+  /// <summary> 
+  ///   Gets an object that can be used to synchronize access to the <see cref="ICollection "/>.
+  /// </summary>
+  /// <value> An object that can be used to synchronize access to the <see cref="ICollection"/>. </value>
+  [Browsable(false)]
+  [DesignerSerializationVisibility (DesignerSerializationVisibility.Hidden)]
+  [EditorBrowsable(EditorBrowsableState.Advanced)]
+  public object SyncRoot
+  { 
+    get { return _items.SyncRoot; } 
+  }
+
+  /// <summary> Gets a value indicating whether the <see cref="IList "/> has a fixed size. </summary>
+  /// <value> <see langword="false"/>, the <see cref="IList"/> has no fixed size. </value>
+  [Browsable(false)]
+  [DesignerSerializationVisibility (DesignerSerializationVisibility.Hidden)]
+  [EditorBrowsable(EditorBrowsableState.Advanced)]
+  public bool IsFixedSize
+  {
+    get { return false; }
+  }
+
+  /// <summary>
+  ///   The <see cref="IBusinessObjectBoundWebControl"/> to which this collection belongs to.
+  /// </summary>
+  internal IBusinessObjectBoundWebControl OwnerControl
+  {
+    get { return _ownerControl; }
+    set 
+    {
+      _ownerControl = value; 
+      foreach (BocColumnDefinition columnDefinition in _items)
+        columnDefinition.OwnerControl = _ownerControl;
+    }
+  }
+}
+
+}
