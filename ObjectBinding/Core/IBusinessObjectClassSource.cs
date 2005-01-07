958d9d1cd68984274d367959584c848bbdb86d52
Michael Ketting <michael.ketting@rubicon.eu>
2005-01-07 14:35:00 +0000

    Label: "1.1.18.0" User: Michael.ketting     Date: 29.12.04   Time:  9:26 Labeled

---
+++ ObjectBinding/Core/IBusinessObjectClassSource.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Drawing.Design;
+using System.Web.UI;
+using System.Web.UI.Design;
+using System.Web.UI.WebControls;
+using System.Globalization;
+using Rubicon.Utilities;
+using Rubicon.ObjectBinding.Web.Design;
+
+namespace Rubicon.ObjectBinding.Web.Controls
+{
+/// <summary>
+/// Summary description for BocPropertyPathWrapper.
+/// </summary>
+public class BocPropertyPathWrapper
+{
+  private bool _isPopertyPathEvaluated;
+  private IBusinessObjectDataSource _dataSource;
+  private BusinessObjectPropertyPath _propertyPath;
+  private string _propertyPathIdentifier;
+
+  public BocPropertyPathWrapper (BusinessObjectPropertyPath propertyPath)
+  {
+    ArgumentUtility.CheckNotNull ("propertyPath", propertyPath);
+    _propertyPath = propertyPath;
+  }
+
+  public BocPropertyPathWrapper (string propertyPathIdentifier)
+  {
+    ArgumentUtility.CheckNotNullOrEmpty ("propertyPathIdentifier", propertyPathIdentifier);
+    _propertyPathIdentifier = propertyPathIdentifier;
+  }
+
+  public BocPropertyPathWrapper()
+  {}
+
+  [DesignerSerializationVisibility (DesignerSerializationVisibility.Hidden)]
+  [Browsable (false)]
+  public IBusinessObjectDataSource DataSource
+  {
+    get
+    {
+      return _dataSource; 
+    }
+    set 
+    {
+      _dataSource = value; 
+    }
+  }
+
+  [DesignerSerializationVisibility (DesignerSerializationVisibility.Hidden)]
+  [Browsable (false)]
+  public BusinessObjectPropertyPath PropertyPath
+  {
+    get 
+    {
+      if (! _isPopertyPathEvaluated)
+      {
+        if (_dataSource == null)
+          throw new InvalidOperationException ("PropertyPath could not be resolved because the DataSource is not set.");
+
+        _propertyPath = BusinessObjectPropertyPath.Parse (_dataSource, _propertyPathIdentifier);
+      }
+
+      return _propertyPath;
+    }
+    set 
+    {
+      _propertyPath = value; 
+      _propertyPathIdentifier = (value == null) ? string.Empty : value.ToString();
+    }
+  }
+
+  [PersistenceMode (PersistenceMode.Attribute)]
+  [DefaultValue("")]
+  public string PropertyPathIdentifier
+  {
+    get 
+    {
+      return _propertyPathIdentifier; 
+    }
+    set 
+    { 
+      _propertyPathIdentifier = value;
+      _propertyPath = null;
+      _isPopertyPathEvaluated = false;
+    }
+  }
+}
+}
