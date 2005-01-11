110d3c40d498eff9f5261becac3d73a00bd9310d
Stefan Wenig <stefan.wenig@rubicon.eu>
2005-01-11 12:43:00 +0000

    Label: "1.1.18.0" User: Michael.ketting     Date: 29.12.04   Time:  9:26 Labeled

---
+++ Web/Core/ExecutionEngine/WxeTemplateControl.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Web;
+using System.Web.UI;
+using Rubicon.Collections;
+using Rubicon.Web.UI;
+using Rubicon.Web.UI.Controls;
+
+namespace Rubicon.Web.ExecutionEngine
+{
+
+public interface IWxeTemplateControl: ITemplateControl
+{
+  NameObjectCollection Variables { get; }
+  WxePageStep CurrentStep { get; }
+  WxeFunction CurrentFunction { get; }
+}
+
+public class WxeTemplateControlInfo
+{
+  private WxePageStep _currentStep;
+  private WxeFunction _currentFunction;
+
+  public void OnLoad (IWxeTemplateControl control, HttpContext context)
+  {
+    WxeHandler wxeHandler = context.Handler as WxeHandler;
+    _currentStep = (wxeHandler == null) ? null : wxeHandler.CurrentFunction.ExecutingStep as WxePageStep;
+
+    WxeStep step = _currentStep;
+    do {
+      _currentFunction = step as WxeFunction;
+      if (_currentFunction != null)
+        break;
+      step = step.ParentStep;
+    } while (step != null);
+  }
+
+  public WxePageStep CurrentStep
+  {
+    get { return _currentStep; }
+  }
+  
+  public WxeFunction CurrentFunction
+  {
+    get { return _currentFunction; }
+  }
+
+  public NameObjectCollection Variables 
+  {
+    get { return (_currentStep == null) ? null : _currentStep.Variables; }
+  }
+}
+
+}
---
+++ Web/Core/ExecutionEngine/WxeUserControl.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using Rubicon.Collections;
+using Rubicon.Web.UI.Controls;
+
+namespace Rubicon.Web.ExecutionEngine
+{
+
+public class WxeUserControl: UserControl, IWxeTemplateControl
+{
+  WxeTemplateControlInfo _wxeInfo = new WxeTemplateControlInfo();
+
+  protected override void OnLoad (EventArgs e)
+  {
+    _wxeInfo.OnLoad (this, Context);
+    base.OnLoad (e);
+  }
+
+  public WxePageStep CurrentStep
+  {
+    get { return _wxeInfo.CurrentStep; }
+  }
+  
+  public WxeFunction CurrentFunction
+  {
+    get { return _wxeInfo.CurrentFunction; }
+  }
+
+  public NameObjectCollection Variables 
+  {
+    get { return _wxeInfo.Variables; }
+  }
+}
+
+}
