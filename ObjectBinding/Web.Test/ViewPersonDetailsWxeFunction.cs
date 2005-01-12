f86ca3fd75ec656093023f8ce08f23bf7424bed1
Michael Ketting <michael.ketting@rubicon.eu>
2005-01-12 16:15:00 +0000

    Label: "1.1.18.0" User: Michael.ketting     Date: 29.12.04   Time:  9:26 Labeled

---
+++ ObjectBinding/Web.Test/ViewPersonDetailsWxeFunction.cs
@@ -0,0 +1,27 @@
+using System;
+using Rubicon.Web.ExecutionEngine;
+
+namespace OBWTest
+{
+
+public class ViewPersonDetailsWxeFunction: WxeFunction
+{
+  public ViewPersonDetailsWxeFunction (object id)
+    : base (id)
+  {
+  }
+
+  // parameters and local variables
+
+  [WxeParameter (1, true, WxeParameterDirection.In)]
+  public string ID
+  {
+    get { return (string) Variables["ID"]; }
+    set { Variables["ID"] = value; }
+  }
+
+  // steps
+
+  private WxeStep Step1 = new WxePageStep ("PersonDetailsForm.aspx");
+}
+}
