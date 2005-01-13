9186b0e982c126252013604a42e6b82252a9e17a
Michael Ketting <michael.ketting@rubicon.eu>
2005-01-13 15:32:00 +0000

    Label: "1.1.18.0" User: Michael.ketting     Date: 29.12.04   Time:  9:26 Labeled

---
+++ ObjectBinding/Core/Design/PropertyPathPickerEditor.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.Design;
+using System.Drawing.Design;
+using System.Windows.Forms.Design;
+using System.ComponentModel;
+
+namespace Rubicon.ObjectBinding.Design
+{
+
+public class PropertyPathPickerEditor: UITypeEditor
+{
+  private IWindowsFormsEditorService _editorService = null;
+
+  public override object EditValue (ITypeDescriptorContext context, IServiceProvider provider, object value) 
+  {
+    if (context != null
+        && context.Instance != null
+        && provider != null) 
+    {
+      _editorService = (IWindowsFormsEditorService) provider.GetService (typeof (IWindowsFormsEditorService));
+
+      if (_editorService != null)
+      {
+        IPropertyPathBinding binding = context.Instance as IPropertyPathBinding;
+        if (binding == null)
+          throw new InvalidOperationException ("Cannot use PropertyPathPickerEditor for objects other than IPropertyPathBinding.");
+
+        //PropertyPickerControl pathPickerControl = new PropertyPickerControl (control);
+
+        //pathPickerControl.Value = (string) value;
+        //pathPickerControl.EditorService = _editorService;
+        //_editorService.DropDownControl (pathPickerControl);
+        //value = pathPickerControl.Value;
+      }
+    }
+    return value;
+  }
+
+  public override UITypeEditorEditStyle GetEditStyle (ITypeDescriptorContext context) 
+  {
+    if (context != null && context.Instance != null) 
+      return UITypeEditorEditStyle.DropDown;
+    return base.GetEditStyle(context);
+  }
+}
+
+}
