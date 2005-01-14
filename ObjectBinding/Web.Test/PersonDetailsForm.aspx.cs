35bc5316ed61023e7387dab18a025015b06490b4
Michael Ketting <michael.ketting@rubicon.eu>
2005-01-14 15:53:00 +0000

    Label: "1.1.18.0" User: Michael.ketting     Date: 29.12.04   Time:  9:26 Labeled

---
+++ ObjectBinding/Web.Test/PersonDetailsForm.aspx.cs
@@ -0,0 +1,110 @@
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Web;
+using System.Web.SessionState;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.HtmlControls;
+using System.Text;
+using Rubicon.Web.UI.Controls;
+using Rubicon.Web.Utilities;
+using Rubicon.NullableValueTypes;
+using Rubicon.Utilities;
+using Rubicon.Web.ExecutionEngine;
+
+namespace OBWTest
+{
+
+public class PersonDetailsPage : WebFormBase
+
+{
+  protected System.Web.UI.HtmlControls.HtmlTable FormGrid;
+  protected System.Web.UI.WebControls.Button SaveButton;
+  protected Rubicon.Web.UI.Controls.FormGridManager FormGridManager;
+  protected Rubicon.ObjectBinding.Web.Controls.BocTextValue FirstNameField;
+  protected Rubicon.ObjectBinding.Reflection.ReflectionBusinessObjectDataSource reflectionBusinessObjectDataSource;
+  protected Rubicon.ObjectBinding.Web.Controls.BocEnumValue GenderField;
+  protected Rubicon.ObjectBinding.Web.Controls.BocReferenceValue PartnerField;
+  protected Rubicon.ObjectBinding.Web.Controls.BocDateTimeValue BirthdayField;
+  protected Rubicon.ObjectBinding.Web.Controls.BocTextValue LastNameField;
+  protected System.Web.UI.WebControls.Button NextButton;
+  protected System.Web.UI.WebControls.Button PostBackButton;
+
+	private void Page_Load(object sender, System.EventArgs e)
+	{
+    string id = (string) Variables["ID"];
+    Guid personID = Guid.Empty;
+    if (! StringUtility.IsNullOrEmpty (id))
+      personID = new Guid (id);
+
+    Person person = Person.GetObject (personID);
+    Person partner;
+    if (person != null)
+      partner = person.Partner;
+
+    reflectionBusinessObjectDataSource.BusinessObject = person;
+
+    this.DataBind();
+    if (!IsPostBack)
+    {
+      reflectionBusinessObjectDataSource.LoadValues (false);
+    }
+	}
+
+	override protected void OnInit(EventArgs e)
+	{
+		//
+		// CODEGEN: This call is required by the ASP.NET Web Form Designer.
+		//
+		InitializeComponent();
+		base.OnInit(e);
+
+    if (!IsPostBack)
+      Rubicon.ObjectBinding.Reflection.ReflectionBusinessObjectStorage.Reset();
+	}
+
+	#region Web Form Designer generated code
+	
+	/// <summary>
+	/// Required method for Designer support - do not modify
+	/// the contents of this method with the code editor.
+	/// </summary>
+	private void InitializeComponent()
+	{    
+    this.reflectionBusinessObjectDataSource = new Rubicon.ObjectBinding.Reflection.ReflectionBusinessObjectDataSource();
+    // 
+    // reflectionBusinessObjectDataSource
+    // 
+    this.reflectionBusinessObjectDataSource.BusinessObject = null;
+    this.reflectionBusinessObjectDataSource.EditMode = true;
+    this.reflectionBusinessObjectDataSource.TypeName = "OBWTest.Person, OBWTest";
+    this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+    this.NextButton.Click += new System.EventHandler(this.NextButton_Click);
+    this.Load += new System.EventHandler(this.Page_Load);
+
+  }
+	#endregion
+
+  private void SaveButton_Click(object sender, System.EventArgs e)
+  {
+    bool isValid = FormGridManager.Validate();
+    if (isValid)
+    {
+      reflectionBusinessObjectDataSource.SaveValues (false);
+      Person person = (Person) reflectionBusinessObjectDataSource.BusinessObject;
+      person.SaveObject();
+      if (person.Partner != null)
+        person.Partner.SaveObject();
+    }
+  }
+
+  private void NextButton_Click(object sender, System.EventArgs e)
+  {
+    CurrentStep.ExecuteNextStep();
+  }
+}
+
+}
