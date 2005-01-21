2701bff9765eb1c48298ecc2d66912a3401e1492
Michael Ketting <michael.ketting@rubicon.eu>
2005-01-21 15:42:00 +0000

    Label: "1.1.18.0" User: Michael.ketting     Date: 29.12.04   Time:  9:26 Labeled

---
+++ ObjectBinding/Web/UI/Design/BusinessObjectDataSourceControlConverter.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.ComponentModel;
+using Rubicon.ObjectBinding.Web.Controls;
+using Rubicon.Utilities;
+
+namespace Rubicon.ObjectBinding.Web.Design
+{
+public class BusinessObjectDataSourceControlConverter : StringConverter
+{
+  public BusinessObjectDataSourceControlConverter()
+  {}
+
+  private object[] GetDataSourceControls (IContainer container)
+  {
+    ComponentCollection components = container.Components;
+    ArrayList dataSources = new ArrayList();
+
+    foreach (IComponent component in components)
+    {
+      IBusinessObjectDataSourceControl dataSource = component as IBusinessObjectDataSourceControl;
+      if (   dataSource != null 
+          && ! StringUtility.IsNullOrEmpty (dataSource.ID))
+      {
+        dataSources.Add (dataSource.ID);
+      }
+    }
+
+    dataSources.Sort(Comparer.Default);
+    return dataSources.ToArray();
+  }
+
+  public override TypeConverter.StandardValuesCollection GetStandardValues (ITypeDescriptorContext context)
+  {
+    if ((context != null) && (context.Container != null))
+    {
+      object[] dataSources = this.GetDataSourceControls (context.Container);
+      if (dataSources != null)
+        return new TypeConverter.StandardValuesCollection (dataSources);
+    }
+    return null;
+  }
+
+  public override bool GetStandardValuesExclusive (ITypeDescriptorContext context)
+  {
+    return false;
+  }
+
+  public override bool GetStandardValuesSupported (ITypeDescriptorContext context)
+  {
+    return true;
+  }
+}
+}
