6b7bbe136708f10da57ee8e82495c1a6a65952aa
Michael Ketting <michael.ketting@rubicon.eu>
2005-01-27 11:22:00 +0000

    Label: "1.1.23" User: Build        Date: 24.01.05   Time: 17:18 Labeled

--- Web/Core/UI/Controls/ValidationError.cs
+++ Web/Core/UI/Controls/ValidationError.cs
@@ -9,7 +9,7 @@ using Rubicon.Utilities;
 namespace Rubicon.Web.UI.Controls
 {
 /// <summary>
-///   Encapsulats an validation error: the affected control, the message and the validator used.
+///   Encapsulats an validation error: the effected control, the message and the validator used.
 /// </summary>
 public class ValidationError
 {
@@ -21,29 +21,25 @@ public class ValidationError
 
   // member fields
 
-  /// <summary>
-  ///   The control with an invalid state.
-  /// </summary>
+  /// <summary> The control with an invalid state. </summary>
   private Control _validatedControl;
 
-  /// <summary>
-  ///   The message to be displayed to the user.
-  /// </summary>
+  /// <summary> The message to be displayed to the user. </summary>
   private string _validationMessage;
 
-  /// <summary>
-  ///   The validator used to validate the <see cref="_validatedControl"/>.
-  /// </summary>
+  /// <summary> The validator used to validate the <see cref="_validatedControl"/>. </summary>
   private IValidator _validator;
 
   // construction and disposing
 
   /// <summary>
-  ///   Simple constructor
+  ///   Initializes a new instance of the <see cref="ValidationError"/> class with the
+  ///   <see cref="Control"/> containing invalid data and the <see cref=" IValidator"/>
+  ///   used to identify the error.
   /// </summary>
-  /// <overload>Overloaded</overload>
-  /// <param name="validatedControl">The control with an invalid state.</param>
-  /// <param name="validator">The validator used to validate the <paramref name="validatedControl"/>.</param>
+  /// <overload> Overloaded. </overload>
+  /// <param name="validatedControl"> The control with an invalid state. </param>
+  /// <param name="validator"> The validator used to validate the <paramref name="validatedControl"/>. </param>
 	public ValidationError (Control validatedControl, IValidator validator)
 	{
     ArgumentUtility.CheckNotNull ("validatedControl", validatedControl);
@@ -55,11 +51,12 @@ public class ValidationError
 	}
 
   /// <summary>
-  ///   Simple constructor
+  ///   Initializes a new instance of the <see cref="ValidationError"/> class with the
+  ///   <see cref="Control"/> containing invalid data and the message describing the error.
   /// </summary>
-  /// <overload>Overloaded</overload>
-  /// <param name="validatedControl">The control with an invalid state.</param>
-  /// <param name="validationMessage">The message to be displayed to the user.</param>
+  /// <overload> Overloaded. </overload>
+  /// <param name="validatedControl"> The control with an invalid state. </param>
+  /// <param name="validationMessage"> The message to be displayed to the user. </param>
   public ValidationError (Control validatedControl, string validationMessage)
 	{
     ArgumentUtility.CheckNotNull ("validatedControl", validatedControl);
@@ -72,19 +69,15 @@ public class ValidationError
 
   // methods and properties
 
-  /// <summary>
-  ///   The control with an invalid state.
-  /// </summary>
-  /// <value>The validated <see cref="Control"/>.</value>
+  /// <summary> Gets the control with an invalid state. </summary>
+  /// <value> The validated <see cref="Control"/>. </value>
   public Control ValidatedControl
   {
     get { return _validatedControl; }
   }
 
-  /// <summary>
-  ///   The message to be displayed to the user.
-  /// </summary>
-  /// <value>A string containing the message</value>
+  /// <summary> The message to be displayed to the user. </summary>
+  /// <value> A string containing the message. </value>
   public string ValidationMessage
   {
     get 
@@ -96,10 +89,8 @@ public class ValidationError
     }
   }
 
-  /// <summary>
-  ///   The validator used to validate the <see cref="ValidatedControl"/>.
-  /// </summary>
-  /// <value>A <see cref="IValidator"/> instance or <see langname="null" /></value>
+  /// <summary> Gets the validator used to validate the <see cref="ValidatedControl"/>. </summary>
+  /// <value> A <see cref="IValidator"/> instance or <see langname="null" />. </value>
   public IValidator Validator
   {
     get { return _validator; }
@@ -113,8 +104,8 @@ public class ValidationError
   ///   Be aware that the Internet Explorer resets DropDown controls
   ///   when jumped at through a label and does not create a postback event.
   /// </remarks>
-  /// <param name="cssClass">The name of the CSS-class used to format the label</param>
-  /// <returns>A <see cref="Label"/></returns>
+  /// <param name="cssClass"> The name of the CSS-class used to format the label. </param>
+  /// <returns> A <see cref="Label"/>. </returns>
   public Label ToLabel (string cssClass)
   {
     Label label = new Label();
@@ -132,8 +123,8 @@ public class ValidationError
   ///   Formats the <c>ValidationError</c> as a <see cref="HyperLink"/>
   ///   and references the <see cref="ValidatedControl"/> through an in-page link.
   /// </summary>
-  /// <param name="cssClass">The name of the CSS-class used to format the hyperlink.</param>
-  /// <returns>A <see cref="HyperLink"/></returns>
+  /// <param name="cssClass"> The name of the CSS-class used to format the hyperlink. </param>
+  /// <returns> A <see cref="HyperLink"/>. </returns>
   public HyperLink ToHyperLink (string cssClass)
   {
     HyperLink hyperLink = new HyperLink();
@@ -151,8 +142,8 @@ public class ValidationError
   ///   Places the <c>ValidationError</c>'s message into a <c>div</c>
   ///   and returns this construct as a <see cref="LiteralControl"/>.
   /// </summary>
-  /// <param name="cssClass">The name of the CSS-class used to format the <c>div</c>-tag.</param>
-  /// <returns>A <see cref="LiteralControl"/></returns>
+  /// <param name="cssClass"> The name of the CSS-class used to format the <c>div</c>-tag. </param>
+  /// <returns> A <see cref="LiteralControl"/>. </returns>
   public LiteralControl ToDiv (string cssClass)
   {
     return ToLiteralControl (cssClass, "div");
@@ -162,8 +153,8 @@ public class ValidationError
   ///   Places the <c>ValidationError</c>'s message into a <c>span</c>
   ///   and returns this construct as a <see cref="LiteralControl"/>.
   /// </summary>
-  /// <param name="cssClass">The name of the CSS-class used to format the <c>span</c>-tag.</param>
-  /// <returns>A <see cref="LiteralControl"/></returns>
+  /// <param name="cssClass"> The name of the CSS-class used to format the <c>span</c>-tag. </param>
+  /// <returns> A <see cref="LiteralControl"/>. </returns>
   public LiteralControl ToSpan (string cssClass)
   {
     return ToLiteralControl (cssClass, "span");
@@ -173,26 +164,19 @@ public class ValidationError
   ///   Places the <c>ValidationError</c>'s message into a which ever HTML tag is provided
   ///   and returns this construct as a <see cref="LiteralControl"/>.
   /// </summary>
-  /// <param name="cssClass">The name of the CSS-class used to format the HTML tag.</param>
-  /// <param name="tag">The HTML tag to be used</param>
-  /// <returns>A <see cref="LiteralControl"/></returns>
+  /// <param name="cssClass"> The name of the CSS-class used to format the HTML tag. </param>
+  /// <param name="tag"> The HTML tag to be used. </param>
+  /// <returns> A <see cref="LiteralControl"/>. </returns>
   private LiteralControl ToLiteralControl (string cssClass, string tag)
   {
-    LiteralControl literalControl = new LiteralControl();
-
-    StringBuilder validationMessage = new StringBuilder (100);
-
     //  Opening tag for validation message
-    validationMessage.AppendFormat ("<{0} class=\"", tag);
-    validationMessage.Append (cssClass);
-    validationMessage.Append ("\">");
-
+    string validationMessage = "<" + tag + " class=\"" + cssClass + "\">";
     //  Message
-    validationMessage.Append (this.ValidationMessage);
-
+    validationMessage += ValidationMessage;
     //  Closing tag for validation message
-    validationMessage.AppendFormat ("</{0}>", tag);
+    validationMessage += "</" + tag + ">";
 
+    LiteralControl literalControl = new LiteralControl();
     return new LiteralControl (validationMessage.ToString());
   }
 }
