70b150bf6c17cd3c7dc2bb7635fa8aaf4f8df927
Stefan Wenig <stefan.wenig@rubicon.eu>
2005-02-03 13:15:00 +0000

    Label: "1.1.23" User: Build        Date: 24.01.05   Time: 17:18 Labeled

---
+++ ObjectBinding/Core/BooleanToEnumPropertyConverter.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Rubicon.ObjectBinding
+{
+	/// <summary>
+	/// Summary description for BooleanToEnumPropertyConverter.
+	/// </summary>
+	public class BooleanToEnumPropertyConverter
+	{
+		public BooleanToEnumPropertyConverter()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+	}
+}
