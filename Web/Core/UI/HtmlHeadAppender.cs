62d05b1537be3d294b3a5a3b2e069b64b1560ef5
Stefan Wenig <stefan.wenig@rubicon.eu>
2005-02-07 15:13:00 +0000

    Label: "1.1.23" User: Build        Date: 24.01.05   Time: 17:18 Labeled

---
+++ Web/Core/UI/HtmlHeadAppender.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Web.UI;
+using System.Runtime.Remoting.Messaging;
+
+namespace Rubicon.Web.UI
+{
+
+/// <summary>
+///   Provides a mechanism to register HTML header elements (e.g., stylesheet or script links).
+/// </summary>
+/// <example>
+///   &lt;head&gt;
+///     &lt;% Rubicon.Web.UI.HeaderFactory.Current.WriteHeaders (Context); %&gt;
+///   &lt;/head&gt;
+/// </example>
+public class HeaderFactory
+{
+  public static HeaderFactory Current
+  {
+    get 
+    { 
+      const string contextKey = "Rubicon.Web.HeaderFactory.Curremt";
+      HeaderFactory current = (HeaderFactory) CallContext.GetData (contextKey);
+      if (current == null)
+      {
+        lock (typeof (HeaderFactory))
+        {
+          current = (HeaderFactory) CallContext.GetData (contextKey);
+          if (current == null)
+          {
+            current = new HeaderFactory();
+            CallContext.SetData (contextKey, current);
+          }
+        }
+      }
+      return current;
+    }
+  }
+
+  public void WriteHeaders (HttpContext context)
+  {
+    foreach (string header in _registeredHeaders.Values)
+    {
+      context.Response.Write ("...");
+    }
+  }
+
+  public void RegisterHeaderStylesheetLink (string key, string url)
+  {
+    RegisterHeader (key, "<link type=\"css/stylesheet\" ...");
+  }
+
+  public void RegisterHeader (string key, string header)
+  {
+    if (! _registeredHeaders.Contains (key))
+      _registeredHeaders.Add (key, header);
+  }
+}
+
+}
