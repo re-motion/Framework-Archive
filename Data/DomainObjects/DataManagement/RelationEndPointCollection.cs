a0a8221eb1ff0832a210676318fd4ff957a0ce63
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-02-11 11:03:00 +0000

    Label: "1.1.25.0" User: Admin        Date:  8.02.05   Time: 14:39 Labeled

---
+++ Data/DomainObjects/DataManagement/RelationEndPointCollection.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Collections;
+
+namespace Rubicon.Data.DomainObjects.Relations
+{
+public class RelationEndPointList : IEnumerable
+{
+  // types
+
+  // static members and constants
+
+  // member fields
+
+  private ArrayList _relationEndPoints;
+
+  // construction and disposing
+
+  public RelationEndPointList ()
+  {
+    _relationEndPoints = new ArrayList ();
+  }
+
+  // methods and properties
+
+  public RelationEndPoint this [int index]
+  {
+    get { return (RelationEndPoint) _relationEndPoints[index]; }
+  }
+
+  public bool Contains (RelationEndPoint relationEndPoint)
+  {
+    return _relationEndPoints.Contains (relationEndPoint);
+  }
+
+  public int Count
+  {
+    get { return _relationEndPoints.Count; }
+  }
+
+  public void Add (RelationEndPoint relationEndPoint)
+  {
+    _relationEndPoints.Add (relationEndPoint);
+  }
+
+  public void Remove (RelationEndPoint relationEndPoint)
+  {
+    _relationEndPoints.Remove (relationEndPoint);
+  }
+
+  public void RemoveAt (int index)
+  {
+    _relationEndPoints.RemoveAt (index); 
+  }
+
+  public void Clear ()
+  {
+    _relationEndPoints.Clear ();
+  }
+
+  public int IndexOf (RelationEndPoint relationEndPoint)
+  {
+    return _relationEndPoints.IndexOf (relationEndPoint);
+  }
+
+  #region IEnumerable Members
+
+  public virtual IEnumerator GetEnumerator ()
+  {
+    return _relationEndPoints.GetEnumerator ();
+  }
+
+  #endregion
+}
+}
