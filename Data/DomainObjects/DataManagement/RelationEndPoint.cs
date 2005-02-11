b2e4338fc20210f407b4e54b61986519241513ff
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-02-11 10:30:00 +0000

    Label: "1.1.25.0" User: Admin        Date:  8.02.05   Time: 14:39 Labeled

---
+++ Data/DomainObjects/DataManagement/RelationEndPoint.cs
@@ -0,0 +1,84 @@
+using System;
+
+using Rubicon.Data.DomainObjects.Configuration.Mapping;
+
+namespace Rubicon.Data.DomainObjects.Relations
+{
+public class RelationEndPoint
+{
+  // types
+
+  // static members and constants
+
+  // member fields
+
+  private IRelationEndPointDefinition _definition;
+
+  // construction and disposing
+
+  protected RelationEndPoint (IRelationEndPointDefinition definition)
+  {
+    Initialize (definition);
+  }
+
+  protected RelationEndPoint ()
+  {
+  }
+
+  // methods and properties
+
+  protected void Initialize (IRelationEndPointDefinition definition)
+  {
+    ArgumentUtility.CheckNotNull ("definition", definition);
+
+    _definition = definition;
+  }
+
+  public IRelationEndPointDefinition Definition 
+  {
+    get { return _definition; }
+  }
+
+  public virtual RelationDefinition RelationDefinition
+  {
+    get 
+    {
+      CheckDefinition ();
+      return _definition.ClassDefinition.GetRelationDefinition (PropertyName); 
+    }
+  }
+
+  public virtual IRelationEndPointDefinition OppositeEndPointDefinition
+  {
+    get 
+    {
+      CheckDefinition ();
+      return _definition.ClassDefinition.GetOppositeEndPointDefinition (PropertyName); 
+    }
+  }
+
+  public virtual string PropertyName
+  {
+    get 
+    { 
+      CheckDefinition ();
+      return _definition.PropertyName; 
+    }
+  }
+
+  public virtual bool IsVirtual
+  {
+    get 
+    {
+      CheckDefinition ();
+      return _definition.IsVirtual; 
+    }
+  }
+
+  private void CheckDefinition ()
+  {
+    if (_definition == null)
+      throw new InvalidOperationException ("Initialize must be called first.");
+  }
+}
+}
