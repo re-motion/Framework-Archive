62a01dd88e09666ec1d2d0d19ebe4e2e23a446e0
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-02-13 18:30:00 +0000

    Label: "AFter Removement of LinkMaps" User: Markus.leimhofer     Date: 13.02.05   Time: 17:21 Labeled Label comment:

---
+++ Data/DomainObjects/DataManagement/RelationEndPointMap.cs
@@ -0,0 +1,59 @@
+using System;
+
+using Rubicon.Data.DomainObjects.Configuration.Mapping;
+
+namespace Rubicon.Data.DomainObjects.DataManagement
+{
+public class RelationEndPointMap : RelationEndPointCollection
+{
+  // types
+
+  // static members and constants
+
+  // member fields
+
+  // construction and disposing
+
+  public RelationEndPointMap ()
+  {
+  }
+
+  public RelationEndPointMap (RelationEndPointCollection collection, bool isCollectionReadOnly) 
+      : base (collection, isCollectionReadOnly)
+  {
+  }
+
+  // methods and properties
+
+  public void Register (DataContainer dataContainer)
+  {
+    ArgumentUtility.CheckNotNull ("dataContainer", dataContainer);
+
+    ClassDefinition classDefinition = dataContainer.ClassDefinition;
+
+    foreach (RelationDefinition relationDefinition in classDefinition.GetAllRelationDefinitions ())
+    {
+      foreach (IRelationEndPointDefinition endPointDefinition in relationDefinition.EndPointDefinitions)
+      {
+        if (!endPointDefinition.IsVirtual)
+        {
+          if (classDefinition.IsRelationEndPoint (endPointDefinition))
+          {
+            ObjectID oppositeObjectID = dataContainer.GetObjectID (endPointDefinition.PropertyName);
+            ObjectEndPoint endPoint = new ObjectEndPoint (dataContainer, endPointDefinition, oppositeObjectID);
+            Add (endPoint);
+
+            if (endPoint.OppositeEndPointDefinition.Cardinality == CardinalityType.One && endPoint.OppositeObjectID != null)
+            {
+              ObjectEndPoint oppositeEndPoint = new ObjectEndPoint (
+                  endPoint.OppositeObjectID, endPoint.OppositeEndPointDefinition, endPoint.ObjectID);
+
+              Add (oppositeEndPoint);
+            }
+          }
+        }
+      }
+    }
+  }
+}
+}
