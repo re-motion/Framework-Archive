0688282c33adc48fef437659ec1d5e12f5ee5fe3
Michael Ketting <michael.ketting@rubicon.eu>
2005-02-21 16:25:00 +0000

    Label: "1.1.29.0" User: Stefan.wenig     Date: 17.02.05   Time: 18:10 Labeled

--- ObjectBinding/Web/UI/Design/BocListViewCollectionEditor.cs
+++ ObjectBinding/Web/UI/Design/BocListViewCollectionEditor.cs
@@ -2,6 +2,7 @@ using System;
 using System.ComponentModel;
 using System.ComponentModel.Design;
 using Rubicon.ObjectBinding.Web.Controls;
+using Rubicon.Web.UI.Design;
 
 namespace Rubicon.ObjectBinding.Web.Design
 {
