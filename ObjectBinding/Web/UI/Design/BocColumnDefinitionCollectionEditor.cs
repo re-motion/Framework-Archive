c257f5f83ce82dea328b2258d466987699964096
Michael Ketting <michael.ketting@rubicon.eu>
2005-02-21 16:25:00 +0000

    Label: "1.1.29.0" User: Stefan.wenig     Date: 17.02.05   Time: 18:10 Labeled

--- ObjectBinding/Web/UI/Design/BocColumnDefinitionCollectionEditor.cs
+++ ObjectBinding/Web/UI/Design/BocColumnDefinitionCollectionEditor.cs
@@ -2,9 +2,24 @@ using System;
 using System.ComponentModel;
 using System.ComponentModel.Design;
 using Rubicon.ObjectBinding.Web.Controls;
+using Rubicon.Web.UI.Design;
 
 namespace Rubicon.ObjectBinding.Web.Design
 {
+
+public class BocMenuItemCollectionEditor: MenuItemCollectionEditor
+{
+  public BocMenuItemCollectionEditor (Type type)
+    : base (type)
+  {
+  }
+
+  protected override Type[] CreateNewItemTypes()
+  {
+    return new Type[] {typeof (BocMenuItem)};
+  }
+}
+
 public class BocColumnDefinitionCollectionEditor: CollectionEditor
 {
   public BocColumnDefinitionCollectionEditor (Type type)
