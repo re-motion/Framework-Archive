a2e668ac8086d4d529fa6f4dbe4c3b676e5aea16
Stefan Wenig <stefan.wenig@rubicon.eu>
2005-02-24 15:30:00 +0000

    Label: "1.1.29.0" User: Stefan.wenig     Date: 17.02.05   Time: 18:10 Labeled

--- Core/ObjectBinding/IBusinessObject.cs
+++ Core/ObjectBinding/IBusinessObject.cs
@@ -8,10 +8,13 @@ public interface IBusinessObject
   object GetProperty (IBusinessObjectProperty property);
   void SetProperty (IBusinessObjectProperty property, object value);
   object this [IBusinessObjectProperty property] { get; set; }
+  string GetPropertyString (IBusinessObjectProperty property);
+  string GetPropertyString (IBusinessObjectProperty property, string format);
 
   object GetProperty (string property);
   void SetProperty (string property, object value);
   object this[string property] { get; set; }
+  string GetPropertyString (string property);
 
   IBusinessObjectClass BusinessObjectClass { get; }
 }
