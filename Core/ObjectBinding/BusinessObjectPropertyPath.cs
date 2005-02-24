112a87d91da6296b0f3044a3325f76870b53dcce
Stefan Wenig <stefan.wenig@rubicon.eu>
2005-02-24 15:30:00 +0000

    Label: "1.1.29.0" User: Stefan.wenig     Date: 17.02.05   Time: 18:10 Labeled

--- Core/ObjectBinding/BusinessObjectPropertyPath.cs
+++ Core/ObjectBinding/BusinessObjectPropertyPath.cs
@@ -11,6 +11,31 @@ namespace Rubicon.ObjectBinding
 /// </summary>
 public class BusinessObjectPropertyPath
 {
+  /// <summary>
+  ///   Property path formatters can be passed to <see cref="String.Format"/> for full <see cref="IFormattable"/> support.
+  /// </summary>
+  public class Formatter: IFormattable
+  {
+    private IBusinessObject _object;
+    private BusinessObjectPropertyPath _path;
+
+    public Formatter (IBusinessObject obj, BusinessObjectPropertyPath path)
+    {
+      _object = obj;
+      _path = path;
+    }
+
+    public string ToString (string format, IFormatProvider formatProvider)
+    {
+      return _path.GetString (_object, format);
+    }
+
+    public override string ToString()
+    {
+      return _path.GetString (_object, null);
+    }
+  }
+
   private const string c_multipleValuesFormatString = "{0}, ... [{1}]";
 
   private IBusinessObjectProperty[] _properties; 
@@ -74,45 +99,79 @@ public class BusinessObjectPropertyPath
     get { return _properties[_properties.Length - 1]; }
   }
 
-  public object GetValue (IBusinessObject obj)
+  /// <summary>
+  ///   Gets the value of this property path for the specified object. 
+  /// </summary>
+  /// <param name="obj"> The object that has the first property in the path. </param>
+  /// <param name="throwExceptionIfNotReachable"> 
+  ///   If <c>true</c>, an InvalidOperationException is thrown if any but the last property in the path is null. If <c>false</c>,
+  ///   <see langword="null"/> is returned instead. </param>
+  /// <param name="getFirstListEntry">
+  ///   If <c>true</c>, the first value of each list property is processed. If <c>false</c>, evaluation of list properties 
+  ///   causes an InvalidOperationException.
+  ///   (This does not apply to the last property in the path. If the last property is a list property, the return value is always a list.) </param>
+  /// <exception cref="InvalidOperationException"> 
+  ///   Thrown if any but the last property in the path is <see langword="null"/>, or is not a single-value reference property. </exception>
+  public object GetValue (IBusinessObject obj, bool throwExceptionIfNotReachable, bool getFirstListEntry)
   {
-    object value = obj;
-    foreach (IBusinessObjectProperty property in _properties)
+    IBusinessObject obj2 = GetValueWithoutLast (obj, throwExceptionIfNotReachable, getFirstListEntry);
+    if (obj2 == null)
+      return null;
+
+    return obj2.GetProperty (LastProperty);
+  }
+  
+  public string GetString (IBusinessObject obj, string format)
+  {
+    IBusinessObject obj2 = GetValueWithoutLast (obj, false, true);
+    if (obj2 == null)
+      return null;
+
+    return obj2.GetPropertyString (LastProperty, format);
+  }
+  
+  private IBusinessObject GetValueWithoutLast (IBusinessObject obj, bool throwExceptionIfNotReachable, bool getFirstListEntry)
+  {
+    for (int i = 0; i < (_properties.Length - 1); ++i)
     {
-      obj = value as IBusinessObject;
+      IBusinessObjectProperty property = _properties[i];
+      if (! (property is IBusinessObjectReferenceProperty))
+        throw new InvalidOperationException (string.Format ("Element {0} of property path {1} is not reference property.", i, this));
+      if (property.IsList)
+      {
+        if (getFirstListEntry)
+        {
+          IList list = (IList) obj.GetProperty (property);
+          if (list.Count > 0)
+            obj = (IBusinessObject) list[0];
+          else
+            obj = null;
+        }
+        else
+        {
+          throw new InvalidOperationException (string.Format ("Element {0} of property path {1} is a not a single-value property.", i, this));
+        }
+      }
+      else
+      {
+        obj = (IBusinessObject) obj.GetProperty (property);
+      }
+            
       if (obj == null)
-        throw new Exception ("Cannot get value"); // TODO: throw useful exception
-      value = obj.GetProperty (property);
-      if (value == null)
-        return null;
+      {
+        if (throwExceptionIfNotReachable)
+          throw new InvalidOperationException (string.Format ("A null value was detected in element {0} of property path {1}. Cannot evaluate rest of path.", i, this));
+        else
+          return null;
+      }
     }
-    return value;
+    return obj;
   }
 
   public object SetValue (IBusinessObject obj)
   {
     // TODO: implement
     throw new NotImplementedException();
-  }
-
-  public string GetStringValue (IBusinessObject obj)
-  {
-    object value = GetValue (obj);
-    if (value == null)
-    {
-      return string.Empty;
-    }
-    else if (value is IList)
-    {
-      IList list = (IList) value;
-      if (list[0] == null)
-        return null;
-      return string.Format (c_multipleValuesFormatString, list[0], list.Count);
-    }
-    else
-    {
-      return value.ToString();
-    }        
   }
 
   public string Identifier
