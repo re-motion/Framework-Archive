a10a6e06bf1fc96ceeed3902e2fb1b3a95b12bc2
Ernst Scheithauer <ernst.scheithauer@rubicon.eu>
2005-02-28 10:31:00 +0000

    Label: "1.1.29.0" User: Stefan.wenig     Date: 17.02.05   Time: 18:10 Labeled

---
+++ Data/DomainObjects.UnitTests/IntegrationTests/DomainObjectTest.cs
@@ -0,0 +1,27 @@
+using System;
+using NUnit.Framework;
+
+using Rubicon.Data.DomainObjects.UnitTests.TestDomain;
+using Rubicon.Data.DomainObjects.UnitTests.Factories;
+
+namespace Rubicon.Data.DomainObjects.UnitTests.DomainObjects
+{
+[TestFixture]
+public class IntegrationTest: ClientTransactionBaseTest
+{
+  // types
+
+  // static members and constants
+
+  // member fields
+
+  // construction and disposing
+
+  public IntegrationTest ()
+  {
+  }
+
+  // methods and properties
+
+}
+}
