81e6ed6b363625357b2f18c4e46ffa60a4e54c19
Stefan Wenig <stefan.wenig@rubicon.eu>
2005-02-28 11:40:00 +0000

    Label: "1.1.29.0" User: Stefan.wenig     Date: 17.02.05   Time: 18:10 Labeled

---
+++ Web/Core/ExecutionEngine/WxeFunctionState.cs
@@ -0,0 +1,93 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace Rubicon.Web.ExecutionEngine
+{
+
+/// <summary>
+///   Stores the session state for a single page token.
+/// </summary>
+[Serializable]
+public class WxePageSession: ISerializable, IDisposable
+{
+  private WxeFunction _function;
+  private DateTime _lastAccess;
+  private int _lifetime;
+  private string _pageToken;
+
+  public WxePageSession (WxeFunction function, int lifetime)
+  {
+    _function = function;
+    _lastAccess = DateTime.Now;
+    _lifetime = lifetime;
+    _pageToken = Guid.NewGuid().ToString();
+  }
+
+  protected WxePageSession (SerializationInfo info, StreamingContext context)
+  {
+    _function = (WxeFunction) info.GetValue ("_function", typeof (WxeFunction));
+    _lastAccess = info.GetDateTime ("_lastAccess");
+    _lifetime = info.GetInt32 ("_lifetime");
+    _pageToken = info.GetString ("_pageToken");
+  }
+
+  void ISerializable.GetObjectData (SerializationInfo info, StreamingContext context)
+  {
+    info.AddValue ("_function", _function, typeof (WxeFunction));
+    info.AddValue ("_lastAccess", _lastAccess);
+    info.AddValue ("_lifetime", _lifetime);
+    info.AddValue ("_pageToken", _pageToken);
+  }
+
+  public WxeFunction Function
+  {
+    get { return _function; }
+  }
+
+  public DateTime LastAccess
+  {
+    get { return _lastAccess; }
+  }
+
+  public int Lifetime
+  {
+    get { return _lifetime; }
+  }
+
+  public string PageToken
+  {
+    get { return _pageToken; }
+  }
+
+  public void Touch()
+  {
+    _lastAccess = DateTime.Now;
+  }
+
+  public bool IsExpired
+  {
+    get { return _lastAccess + new TimeSpan (0, _lifetime, 0) < DateTime.Now; }
+  }
+
+  public void Dispose()
+  {
+    Dispose (true);
+    GC.SuppressFinalize (this);
+  }
+
+  protected virtual void Dispose (bool disposing)
+  {
+    if (disposing)
+    {
+      if (_function != null)
+        _function.Dispose();
+    }
+  }
+
+  ~WxePageSession()
+  {
+    Dispose (false);
+  }
+}
+
+}
