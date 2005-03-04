17c47ac6cc4f97ff25726d367d284f91b429910d
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-03-04 11:11:00 +0000

    Label: "1.1.35.0" User: Stefan.wenig     Date:  3.03.05   Time: 11:48 Labeled

--- Data/DomainObjects/DataManagement/ICollectionChangeDelegate.cs
+++ Data/DomainObjects/DataManagement/ICollectionChangeDelegate.cs
@@ -5,6 +5,7 @@ namespace Rubicon.Data.DomainObjects.DataManagement
 internal interface ICollectionChangeDelegate
 {
   void PerformAdd (DomainObjectCollection collection, DomainObject domainObject);
+  void PerformInsert (DomainObjectCollection collection, DomainObject domainObject, int index);
   void PerformRemove (DomainObjectCollection collection, DomainObject domainObject);
 }
 }
