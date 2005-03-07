2b2c9ce7c670455f56fb6e9256cbfc664f3e2804
Stefan Wenig <stefan.wenig@rubicon.eu>
2005-03-07 11:55:00 +0000

    Label: "1.1.40.0" User: Build        Date:  7.03.05   Time: 12:19 Labeled

--- Web/Core/ExecutionEngine/WxePageStep.cs
+++ Web/Core/ExecutionEngine/WxePageStep.cs
@@ -10,6 +10,10 @@ namespace Rubicon.Web.ExecutionEngine
 
 public class WxeExecuteNextStepException: Exception
 {
+  public WxeExecuteNextStepException()
+    : base ("This exception does not indicate an error. It is used to roll back the call stack. It is recommended to disable breaking on this exeption type while debugging.")
+  {
+  }
 }
 
 public class WxePageStep: WxeStep
