fa0ff7ba3ebafd977e0c5571d458235299434863
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-03-07 10:34:00 +0000

    Label: "1.1.39.0" User: Build        Date:  4.03.05   Time: 19:09 Labeled

---
+++ Data/DomainObjects.UnitTests/Configuration/Mapping/TypeInfoTest.cs
@@ -0,0 +1,73 @@
+using System;
+using NUnit.Framework;
+
+using Rubicon.Data.DomainObjects.Configuration.Mapping;
+using Rubicon.Data.DomainObjects.UnitTests.TestDomain;
+
+namespace Rubicon.Data.DomainObjects.UnitTests.Configuration.Mapping
+{
+[TestFixture]
+public class TypeMappingTest
+{
+  // types
+
+  // static members and constants
+
+  // member fields
+
+  // construction and disposing
+
+  public TypeMappingTest ()
+  {
+  }
+
+  // methods and properties
+
+  [Test]
+  public void TypeMapping ()
+  {
+    string[] mappingTypes = new string[]
+    {
+      "byte",
+      "boolean",
+      "date",
+      "dateTime", 
+      "single",
+      "decimal", 
+      "double", 
+      "guid", 
+      "int16", 
+      "int32", 
+      "int64", 
+      "string", 
+      "char", 
+      "Rubicon.Data.DomainObjects.UnitTests.TestDomain.Customer+CustomerType, Rubicon.Data.DomainObjects.UnitTests",
+      "objectID"
+    };
+
+    Type[] expectedTypes = new Type[]
+    {
+      typeof (byte),
+      typeof (bool),
+      typeof (DateTime),
+      typeof (DateTime),
+      typeof (float),
+      typeof (decimal),
+      typeof (double),
+      typeof (Guid),
+      typeof (short),
+      typeof (int),
+      typeof (long),
+      typeof (string),
+      typeof (char),
+      typeof (Customer.CustomerType),
+      typeof (ObjectID)
+    };
+
+    for (int i = 0; i < mappingTypes.Length; i++)
+    {
+      Assert.AreEqual (expectedTypes[i], MappingUtility.MapType (mappingTypes[i]));
+    }
+  }
+}
+}
