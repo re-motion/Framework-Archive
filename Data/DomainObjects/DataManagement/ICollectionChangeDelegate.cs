b4e524d71796a8034881b098b8ae3650aa43b40b
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-03-08 10:09:00 +0000

    Label: "1.1.43.0" User: Build        Date:  7.03.05   Time: 14:48 Labeled

--- Data/DomainObjects/DataManagement/ICollectionChangeDelegate.cs
+++ Data/DomainObjects/DataManagement/ICollectionChangeDelegate.cs
@@ -4,7 +4,6 @@ namespace Rubicon.Data.DomainObjects.DataManagement
 {
 internal interface ICollectionChangeDelegate
 {
-  void PerformAdd (DomainObjectCollection collection, DomainObject domainObject);
   void PerformInsert (DomainObjectCollection collection, DomainObject domainObject, int index);
   void PerformReplace (DomainObjectCollection collection, DomainObject domainObject, int index);
   void PerformRemove (DomainObjectCollection collection, DomainObject domainObject);
--- Data/DomainObjects/DataManagement/ICollectionEndPointChangeDelegate.cs
+++ Data/DomainObjects/DataManagement/ICollectionEndPointChangeDelegate.cs
@@ -4,7 +4,6 @@ namespace Rubicon.Data.DomainObjects.DataManagement
 {
 public interface ICollectionEndPointChangeDelegate
 {
-  void PerformAdd (CollectionEndPoint endPoint, DomainObject domainObject);
   void PerformInsert (CollectionEndPoint endPoint, DomainObject domainObject, int index);
   void PerformReplace (CollectionEndPoint endPoint, DomainObject domainObject, int index);
   void PerformRemove (CollectionEndPoint endPoint, DomainObject domainObject);
