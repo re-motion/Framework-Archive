b67e19afb1f5510effc33f995dea0d67e2a4a469
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-03-10 14:28:00 +0000

    Label: "Version 0.9.0.0" User: Markus.leimhofer     Date:  8.03.05   Time: 17:24 Labeled Label comment:

---
+++ Data/DomainObjects.UnitTests/Configuration/Mapping/MappingConfigurationTest.cs
@@ -0,0 +1,38 @@
+using System;
+using System.IO;
+using NUnit.Framework;
+
+using Rubicon.Data.DomainObjects.Configuration.Mapping;
+
+namespace Rubicon.Data.DomainObjects.UnitTests.Configuration.Mapping
+{
+[TestFixture]
+public class MappingConfigurationTest
+{
+  // types
+
+  // static members and constants
+
+  // member fields
+
+  // construction and disposing
+
+  public MappingConfigurationTest ()
+  {
+  }
+
+  // methods and properties
+
+  [Test]
+  public void InitializeWithFileNames ()
+  {
+    MappingConfiguration.SetCurrent (new MappingConfiguration (@"..\..\mapping.xml", @"..\..\..\mapping.xsd"));
+
+    string configurationFile = Path.GetFullPath (@"..\..\mapping.xml");
+    string schemaFile = Path.GetFullPath (@"..\..\..\mapping.xsd");
+
+    Assert.AreEqual (configurationFile, MappingConfiguration.Current.ConfigurationFile);
+    Assert.AreEqual (schemaFile, MappingConfiguration.Current.SchemaFile);
+  }
+}
+}
