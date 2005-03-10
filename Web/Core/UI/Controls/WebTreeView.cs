5c0dd3cf09f6a33601287d3855782c922e64254c
Michael Ketting <michael.ketting@rubicon.eu>
2005-03-10 17:00:00 +0000

    Label: "1.1.43.0" User: Build        Date:  7.03.05   Time: 14:48 Labeled

---
+++ Web/Core/UI/Controls/WebTreeView.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Rubicon.Web.UI.Controls
+{
+
+[ToolboxData("<{0}:WebTreeView runat=server></{0}:WebTreeView>")]
+public class WebTreeView : WebControl
+{
+}
+
+}
