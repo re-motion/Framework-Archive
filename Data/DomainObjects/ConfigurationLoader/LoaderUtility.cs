fead11dfd79d459ea3d4d60ecffe437bb985b726
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-03-10 13:13:00 +0000

    Label: "Version 0.9.0.0" User: Markus.leimhofer     Date:  8.03.05   Time: 17:24 Labeled Label comment:

--- Data/DomainObjects/ConfigurationLoader/LoaderUtility.cs
+++ Data/DomainObjects/ConfigurationLoader/LoaderUtility.cs
@@ -23,23 +23,23 @@ public sealed class LoaderUtility
     return Path.GetDirectoryName (codeBaseUri.LocalPath);
   }
 
-  public static Type GetTypeFromNode (XmlNode node)
+  public static Type MapType (XmlNode node)
   {
     ArgumentUtility.CheckNotNull ("node", node);
 
     return MappingUtility.MapType (node.InnerText.Trim ());    
   }
 
-  public static Type GetTypeFromNode (XmlNode node, string xPath, XmlNamespaceManager namespaceManager)
+  public static Type MapType (XmlNode node, string xPath, XmlNamespaceManager namespaceManager)
   {
     ArgumentUtility.CheckNotNull ("node", node);
     ArgumentUtility.CheckNotNullOrEmpty ("xPath", xPath);
     ArgumentUtility.CheckNotNull ("namespaceManager", namespaceManager);
 
-    return GetTypeFromNode (node.SelectSingleNode (xPath, namespaceManager));
+    return MapType (node.SelectSingleNode (xPath, namespaceManager));
   }
 
-  public static Type GetTypeFromOptionalNode (XmlNode selectionNode, string xPath, XmlNamespaceManager namespaceManager)
+  public static Type MapOptionalType (XmlNode selectionNode, string xPath, XmlNamespaceManager namespaceManager)
   {
     ArgumentUtility.CheckNotNull ("selectionNode", selectionNode);
     ArgumentUtility.CheckNotNullOrEmpty ("xPath", xPath);
@@ -48,7 +48,37 @@ public sealed class LoaderUtility
     XmlNode typeNode = selectionNode.SelectSingleNode (xPath, namespaceManager);
 
     if (typeNode != null)
-      return GetTypeFromNode (typeNode);
+      return MapType (typeNode);
+    else
+      return null;
+  }
+
+  public static Type GetType (XmlNode node)
+  {
+    ArgumentUtility.CheckNotNull ("node", node);
+
+    return Type.GetType (node.InnerText.Trim (), true);    
+  }
+
+  public static Type GetType (XmlNode node, string xPath, XmlNamespaceManager namespaceManager)
+  {
+    ArgumentUtility.CheckNotNull ("node", node);
+    ArgumentUtility.CheckNotNullOrEmpty ("xPath", xPath);
+    ArgumentUtility.CheckNotNull ("namespaceManager", namespaceManager);
+
+    return GetType (node.SelectSingleNode (xPath, namespaceManager));
+  }
+
+  public static Type GetOptionalType (XmlNode selectionNode, string xPath, XmlNamespaceManager namespaceManager)
+  {
+    ArgumentUtility.CheckNotNull ("selectionNode", selectionNode);
+    ArgumentUtility.CheckNotNullOrEmpty ("xPath", xPath);
+    ArgumentUtility.CheckNotNull ("namespaceManager", namespaceManager);
+    
+    XmlNode typeNode = selectionNode.SelectSingleNode (xPath, namespaceManager);
+
+    if (typeNode != null)
+      return GetType (typeNode);
     else
       return null;
   }
