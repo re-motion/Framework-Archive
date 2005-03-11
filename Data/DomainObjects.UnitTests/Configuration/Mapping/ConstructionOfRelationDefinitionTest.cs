c92aa04e6b8ba82c2af80601131143f25610e897
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-03-11 06:56:00 +0000

    Label: "Version 0.9.0.0" User: Markus.leimhofer     Date:  8.03.05   Time: 17:24 Labeled Label comment:

--- Data/DomainObjects.UnitTests/Configuration/Mapping/ConstructionOfRelationDefinitionTest.cs
+++ Data/DomainObjects.UnitTests/Configuration/Mapping/ConstructionOfRelationDefinitionTest.cs
@@ -1,7 +1,7 @@
 using System;
 using NUnit.Framework;
 
-using Rubicon.Data.DomainObjects.Configuration.Mapping;
+using Rubicon.Data.DomainObjects.Mapping;
 using Rubicon.Data.DomainObjects.UnitTests.TestDomain;
 
 namespace Rubicon.Data.DomainObjects.UnitTests.Configuration.Mapping
