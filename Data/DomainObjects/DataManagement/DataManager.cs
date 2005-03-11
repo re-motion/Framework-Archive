37f45a72598b140b9a2e5a791ca010b78c5de4a0
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-03-11 06:56:00 +0000

    Label: "Version 0.9.0.0" User: Markus.leimhofer     Date:  8.03.05   Time: 17:24 Labeled Label comment:

--- Data/DomainObjects/DataManagement/DataManager.cs
+++ Data/DomainObjects/DataManagement/DataManager.cs
@@ -1,6 +1,6 @@
 using System;
 
-using Rubicon.Data.DomainObjects.Configuration.Mapping;
+using Rubicon.Data.DomainObjects.Mapping;
 using Rubicon.Utilities;
 
 namespace Rubicon.Data.DomainObjects.DataManagement
