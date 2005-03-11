961000d99a2e4425d0a469fef4728e99ae5cbf23
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-03-11 06:56:00 +0000

    Label: "Version 0.9.0.0" User: Markus.leimhofer     Date:  8.03.05   Time: 17:24 Labeled Label comment:

--- Data/DomainObjects/DataManagement/RelationEndPointCollection.cs
+++ Data/DomainObjects/DataManagement/RelationEndPointCollection.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections;
 
-using Rubicon.Data.DomainObjects.Configuration.Mapping;
+using Rubicon.Data.DomainObjects.Mapping;
 using Rubicon.Utilities;
 
 namespace Rubicon.Data.DomainObjects.DataManagement
