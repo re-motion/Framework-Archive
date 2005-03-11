63db154d2dedcd8d106e76391ad1765b16d228ac
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-03-11 06:56:00 +0000

    Label: "Version 0.9.0.0" User: Markus.leimhofer     Date:  8.03.05   Time: 17:24 Labeled Label comment:

--- Data/DomainObjects.UnitTests/DataManagement/DataContainerTest.cs
+++ Data/DomainObjects.UnitTests/DataManagement/DataContainerTest.cs
@@ -2,7 +2,7 @@ using System;
 using NUnit.Framework;
 
 using Rubicon.NullableValueTypes;
-using Rubicon.Data.DomainObjects.Configuration.Mapping;
+using Rubicon.Data.DomainObjects.Mapping;
 using Rubicon.Data.DomainObjects.DataManagement;
 using Rubicon.Data.DomainObjects.UnitTests.Factories;
 using Rubicon.Data.DomainObjects.UnitTests.TestDomain;
