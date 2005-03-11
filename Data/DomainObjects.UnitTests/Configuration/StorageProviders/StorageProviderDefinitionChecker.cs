4c1399b95ae1355fd07d2e150ddc8a1f00ccaeef
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-03-11 07:05:00 +0000

    Label: "Version 0.9.0.0" User: Markus.leimhofer     Date:  8.03.05   Time: 17:24 Labeled Label comment:

--- Data/DomainObjects.UnitTests/Configuration/StorageProviders/StorageProviderDefinitionChecker.cs
+++ Data/DomainObjects.UnitTests/Configuration/StorageProviders/StorageProviderDefinitionChecker.cs
@@ -1,7 +1,7 @@
 using System;
 using NUnit.Framework;
 
-using Rubicon.Data.DomainObjects.Configuration.StorageProviders;
+using Rubicon.Data.DomainObjects.Persistence.Configuration;
 
 namespace Rubicon.Data.DomainObjects.UnitTests.Configuration.StorageProviders
 {
