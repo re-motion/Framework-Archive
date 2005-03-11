c7121652b9511c0a179eda1a57bc6f2bd33329d4
Andreas Schlapsi <andreas.schlapsi@rubicon.eu>
2005-03-11 09:32:00 +0000

    Label: "Version 0.9.0.0" User: Markus.leimhofer     Date:  8.03.05   Time: 17:24 Labeled Label comment:

--- Data/DomainObjects.ObjectBinding.Web/DomainObjectDataSourceControl.cs
+++ Data/DomainObjects.ObjectBinding.Web/DomainObjectDataSourceControl.cs
@@ -1,21 +1,79 @@
 using System;
+using System.Collections;
+using System.Reflection;
 using System.ComponentModel;
 using System.Drawing.Design;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using Rubicon.Web.Utilities;
 using Rubicon.ObjectBinding;
 using Rubicon.ObjectBinding.Web.Controls;
 
+using Rubicon.Data.DomainObjects.Configuration.Mapping;
+using Rubicon.Data.DomainObjects.Configuration.Loader;
 using Rubicon.Data.DomainObjects.ObjectBinding.Design;
 
 namespace Rubicon.Data.DomainObjects.ObjectBinding.Web
 {
-
-public class DomainObjectDataSourceControl: BusinessObjectDataSourceControl
+public class DomainObjectDataSourceControl : BusinessObjectDataSourceControl
 {
+  public static object GetDesignTimePropertyValue (Control control, string propertyName)
+  {
+    if (! ControlHelper.IsDesignMode (control))
+      return null;
+
+    try
+    {
+      ISite site = control.Site;
+
+      //EnvDTE._DTE environment = (EnvDTE._DTE) site.GetService (typeof (EnvDTE._DTE));
+      MethodInfo getServiceMethod = site.GetType().GetMethod ("GetService");
+      Type _DTEType = Type.GetType ("EnvDTE._DTE, EnvDTE");
+      object[] arguments = new object[] { _DTEType };
+      object environment = getServiceMethod.Invoke (site, arguments);
+
+      if (environment != null)
+      {
+        //EnvDTE.Project project = environment.ActiveDocument.ProjectItem.ContainingProject;
+        object activeDocument =_DTEType.InvokeMember ("ActiveDocument", BindingFlags.GetProperty, null, environment, null);
+        object projectItem = activeDocument.GetType().InvokeMember ("ProjectItem", BindingFlags.GetProperty, null, activeDocument, null);
+        object project = projectItem.GetType().InvokeMember ("ContainingProject", BindingFlags.GetProperty, null, projectItem, null);
+
+        ////project.Properties uses a 1-based index
+        //foreach (EnvDTE.Property property in project.Properties)
+        object properties = project.GetType().InvokeMember ("Properties", BindingFlags.GetProperty, null, project, null);
+        IEnumerator propertiesEnumerator = (IEnumerator) properties.GetType().InvokeMember ("GetEnumerator", BindingFlags.InvokeMethod, null, properties, null);
+        while (propertiesEnumerator.MoveNext())
+        {
+          object property = propertiesEnumerator.Current;
+
+          //if (property.Name == propertyName)
+          string projectPropertyName = (string) property.GetType().InvokeMember ("Name", BindingFlags.GetProperty, null, property, null);
+          if (projectPropertyName == propertyName)
+          {
+            //return property.Value;
+            return property.GetType().InvokeMember ("Value", BindingFlags.GetProperty, null, property, null);
+          }
+        }
+      }
+    }
+    catch
+    {
+      return null;
+    }
+
+    return null;
+  }
+
   private DomainObjectDataSource _dataSource = new DomainObjectDataSource();
+
+  protected override void OnInit(EventArgs e)
+  {
+    base.OnInit (e);
+    SetDesignTimeMapping ();
+  }
 
   [PersistenceMode (PersistenceMode.Attribute)]
   [Category ("Data")]
@@ -36,6 +94,16 @@ public class DomainObjectDataSourceControl: BusinessObjectDataSourceControl
   protected override IBusinessObjectDataSource GetDataSource()
   {
     return _dataSource;
+  }
+
+  private void SetDesignTimeMapping ()
+  {
+    string projectPath = (string) GetDesignTimePropertyValue (this, "ActiveFileSharePath");
+    if (projectPath == null)
+      return;
+
+    MappingLoader mappingLoader = new MappingLoader (projectPath + @"\bin\mapping.xml", projectPath + @"\bin\mapping.xsd");
+    MappingConfiguration.SetCurrent (new MappingConfiguration (mappingLoader));
   }
 }
 }
