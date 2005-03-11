4c6989f7929c609281f9bae579f8148dd600b1c6
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-03-11 07:05:00 +0000

    Label: "Version 0.9.0.0" User: Markus.leimhofer     Date:  8.03.05   Time: 17:24 Labeled Label comment:

--- Data/DomainObjects/Persistence/Configuration/StorageProviderConfigurationException.cs
+++ Data/DomainObjects/Persistence/Configuration/StorageProviderConfigurationException.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Runtime.Serialization;
 
-namespace Rubicon.Data.DomainObjects.Configuration.StorageProviders
+namespace Rubicon.Data.DomainObjects.Persistence.Configuration
 {
 [Serializable]
 public class StorageProviderConfigurationException : ConfigurationException
