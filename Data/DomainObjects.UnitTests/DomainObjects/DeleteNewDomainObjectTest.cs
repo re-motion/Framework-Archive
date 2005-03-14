2685310f32073b38da1d4172e8b757e9cc81beca
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-03-14 09:07:00 +0000

    Label: "1.1.44.0" User: Build        Date: 14.03.05   Time:  9:47 Labeled

--- Data/DomainObjects.UnitTests/DomainObjects/DeleteNewDomainObjectTest.cs
+++ Data/DomainObjects.UnitTests/DomainObjects/DeleteNewDomainObjectTest.cs
@@ -4,7 +4,7 @@ using NUnit.Framework;
 
 using Rubicon.Data.DomainObjects.Mapping;
 using Rubicon.Data.DomainObjects.DataManagement;
-using Rubicon.Data.DomainObjects.UnitTests.EventSequence;
+using Rubicon.Data.DomainObjects.UnitTests.EventReceiver;
 using Rubicon.Data.DomainObjects.UnitTests.Factories;
 using Rubicon.Data.DomainObjects.UnitTests.TestDomain;
 using Rubicon.NullableValueTypes;
