417dce60cd689dc38a7451ec51c38c27e7b0f955
Michael Ketting <michael.ketting@rubicon.eu>
2005-03-16 14:49:00 +0000

    Label: "1.1.45.0" User: Build        Date: 14.03.05   Time: 10:27 Labeled

--- ObjectBinding/Web/IBusinessObjectWebUIService.cs
+++ ObjectBinding/Web/IBusinessObjectWebUIService.cs
@@ -1,7 +1,6 @@
 using System;
-using System.Drawing;
 using Rubicon.ObjectBinding;
-using System.Web.UI.WebControls;
+using Rubicon.Web.UI.Controls;
 
 namespace Rubicon.ObjectBinding.Web
 {
@@ -13,38 +12,6 @@ public interface IBusinessObjectWebUIService: IBusinessObjectService
 {
   IconInfo GetIcon (IBusinessObject obj);
   IconInfo GetNullValueIcon ();
-}
-
-public sealed class IconInfo
-{
-  private string _url;
-  private Unit _width;
-  private Unit _height;
-
-  public IconInfo (string url, Unit width, Unit height)
-  {
-    _url = url;
-    _width = width;
-    _height = height;
-  }
-
-  public string Url
-  {
-    get { return _url; }
-    set { _url = value; }
-  }
-
-  public Unit Width
-  {
-    get { return _width; }
-    set { _width = value; }
-  }
-
-  public Unit Height
-  {
-    get { return _height; }
-    set { _height = value; }
-  }
 }
 
 }
