d5488d5c5b0a9c2e096dc68f4fb6282054fe787c
Ernst Scheithauer <ernst.scheithauer@rubicon.eu>
2005-03-17 08:54:00 +0000

    Label: "1.1.46.0" User: Build        Date: 17.03.05   Time:  8:19 Labeled

--- Data/DomainObjects/PropertyValueCollection.cs
+++ Data/DomainObjects/PropertyValueCollection.cs
@@ -220,8 +220,8 @@ public class PropertyValueCollection : CollectionBase
   /// <exception cref="System.ArgumentNullException"><i>array</i> is a null reference.</exception>
   /// <exception cref="System.ArgumentOutOfRangeException"><i>index</i> is smaller than 0.</exception>
   /// <exception cref="System.ArgumentException">
-  ///   <i>array</i> is not a one-dimensional array.
-  ///   <i>index</i> is greater than the current length of the array.
+  ///   <i>array</i> is not a one-dimensional array.<br />
+  ///   <i>index</i> is greater than the current length of the array.<br />
   ///   The number of items is greater than the available space from <i>index</i> to the end of <i>array</i>.
   /// </exception>
   /// <exception cref="DataManagement.ObjectDiscardedException">Methods were called after a newly instantiated (uncommitted) <see cref="DomainObject"/> was deleted.</exception>
