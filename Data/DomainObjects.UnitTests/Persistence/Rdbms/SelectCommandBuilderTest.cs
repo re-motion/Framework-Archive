2efc9afc85cf3d4cb25a6a4445fc223db0a37591
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-03-17 14:16:00 +0000

    Label: "1.1.46.0" User: Build        Date: 17.03.05   Time:  8:19 Labeled

---
+++ Data/DomainObjects.UnitTests/Persistence/Rdbms/SelectCommandBuilderTest.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Data;
+using NUnit.Framework;
+
+using Rubicon.Data.DomainObjects.Mapping;
+using Rubicon.Data.DomainObjects.Persistence;
+using Rubicon.Data.DomainObjects.UnitTests.Factories;
+
+namespace Rubicon.Data.DomainObjects.UnitTests.Persistence
+{
+[TestFixture]
+public class SelectCommandBuilderTest : SqlProviderBaseTest
+{
+  // types
+
+  // static members and constants
+
+  // member fields
+
+  // construction and disposing
+
+  public SelectCommandBuilderTest ()
+  {
+  }
+
+  // methods and properties
+
+  [Test]
+  public void CreateWithOrderClause ()
+  {
+    ClassDefinition orderDefinition = TestMappingConfiguration.Current.ClassDefinitions.GetByClassID ("Order");
+    
+    SelectCommandBuilder builder = new SelectCommandBuilder (
+        Provider, "*", orderDefinition, "CustomerID", DomainObjectIDs.Customer1, "OrderNumber desc");
+    
+    Provider.Connect ();
+    using (IDbCommand command = builder.Create ())
+    {
+      Assert.AreEqual ("SELECT * FROM [Order] WHERE [CustomerID] = @CustomerID ORDER BY OrderNumber desc;", command.CommandText);
+    }
+  }
+}
+}
