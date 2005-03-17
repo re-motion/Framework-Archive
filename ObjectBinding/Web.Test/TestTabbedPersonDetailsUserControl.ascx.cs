75b36ec5bce26a686b8c9c983bf82082ef028e8c
Michael Ketting <michael.ketting@rubicon.eu>
2005-03-17 16:08:00 +0000

    Label: "1.1.46.0" User: Build        Date: 17.03.05   Time:  8:19 Labeled

--- ObjectBinding/Web.Test/TestTabbedPersonDetailsUserControl.ascx.cs
+++ ObjectBinding/Web.Test/TestTabbedPersonDetailsUserControl.ascx.cs
@@ -1,12 +1,17 @@
 using System;
+using System.Collections.Specialized;
+using System.Web.UI.HtmlControls;
 using Rubicon.ObjectBinding.Web.Controls;
 using Rubicon.ObjectBinding.Reflection;
 using Rubicon.ObjectBinding;
+using Rubicon.Web.UI.Controls;
+using Rubicon.Web.UI;
+using Rubicon.Collections;
 
 namespace OBWTest
 {
 public class TestTabbedPersonDetailsUserControl : 
-    DataEditUserControl
+    DataEditUserControl, IControl, IFormGridRowProvider
 {
   protected Rubicon.ObjectBinding.Web.Controls.BocTextValue FirstNameField;
   protected Rubicon.ObjectBinding.Web.Controls.BocTextValue LastNameField;
@@ -18,16 +23,33 @@ public class TestTabbedPersonDetailsUserControl :
   protected Rubicon.ObjectBinding.Web.Controls.BocDateTimeValue DateOfDeathField;
   protected Rubicon.ObjectBinding.Web.Controls.BocEnumValue MarriageStatusField;
   protected System.Web.UI.HtmlControls.HtmlTable FormGrid;
+  private AutoInitHashtable _listOfFormGridRowInfos =
+      new AutoInitHashtable (typeof (FormGridRowInfoCollection));
+  private AutoInitHashtable _listOfHiddenRows = 
+      new AutoInitHashtable (typeof (StringCollection));
 
   public override IBusinessObjectDataSourceControl DataSource
   {
     get { return ReflectionBusinessObjectDataSourceControl; }
   }
 
+  public virtual StringCollection GetHiddenRows (HtmlTable table)
+  {
+    return (StringCollection) _listOfHiddenRows[table];
+  }
+
+  public virtual FormGridRowInfoCollection GetAdditionalRows (HtmlTable table)
+  {
+    return (FormGridRowInfoCollection) _listOfFormGridRowInfos[table];
+  }
+
 	#region Web Form Designer generated code
 
 	override protected void OnInit(EventArgs e)
 	{
+    StringCollection hiddenRows = (StringCollection)_listOfHiddenRows[FormGrid];
+    FormGridRowInfoCollection newRows = (FormGridRowInfoCollection)_listOfFormGridRowInfos[FormGrid];
+
 		//
 		// CODEGEN: This call is required by the ASP.NET Web Form Designer.
 		//
--- ObjectBinding/Web.Test/TestTabbedPersonJobsUserControl.ascx.cs
+++ ObjectBinding/Web.Test/TestTabbedPersonJobsUserControl.ascx.cs
@@ -1,12 +1,17 @@
 using System;
+using System.Collections.Specialized;
+using System.Web.UI.HtmlControls;
 using Rubicon.ObjectBinding.Web.Controls;
 using Rubicon.ObjectBinding.Reflection;
 using Rubicon.ObjectBinding;
+using Rubicon.Web.UI.Controls;
+using Rubicon.Web.UI;
+using Rubicon.Collections;
 
 namespace OBWTest
 {
 public class TestTabbedPersonJobsUserControl : 
-    DataEditUserControl
+    DataEditUserControl, IControl, IFormGridRowProvider
 {
   protected Rubicon.ObjectBinding.Web.Controls.BocList ListField;
   protected Rubicon.Web.UI.Controls.FormGridManager FormGridManager;
@@ -16,16 +21,37 @@ public class TestTabbedPersonJobsUserControl :
   protected Rubicon.ObjectBinding.Web.Controls.BocMultilineTextValue MultilineTextField;
 
   protected Rubicon.ObjectBinding.Reflection.ReflectionBusinessObjectDataSourceControl ReflectionBusinessObjectDataSourceControl;
+  private AutoInitHashtable _listOfFormGridRowInfos =
+      new AutoInitHashtable (typeof (FormGridRowInfoCollection));
+  private AutoInitHashtable _listOfHiddenRows = 
+      new AutoInitHashtable (typeof (StringCollection));
 
   public override IBusinessObjectDataSourceControl DataSource
   {
     get { return ReflectionBusinessObjectDataSourceControl; }
   }
 
+  public virtual StringCollection GetHiddenRows (HtmlTable table)
+  {
+    return (StringCollection) _listOfHiddenRows[table];
+  }
+
+  public virtual FormGridRowInfoCollection GetAdditionalRows (HtmlTable table)
+  {
+    return (FormGridRowInfoCollection) _listOfFormGridRowInfos[table];
+  }
+
 	#region Web Form Designer generated code
 
 	override protected void OnInit(EventArgs e)
 	{
+//    StringCollection hiddenRows = (StringCollection)_listOfHiddenRows[FormGrid];
+//    FormGridRowInfoCollection newRows = (FormGridRowInfoCollection)_listOfFormGridRowInfos[FormGrid];
+//    BocList childrenList = new BocList();
+//    childrenList.ID = "ChildrenList";
+//    childrenList.DataSource = DataSource;
+//    childrenList.PropertyIdentifier = "Children";
+//    newRows.Add (new FormGridRowInfo (childrenList, FormGridRowInfo.RowType.ControlInRowWithLabel, null, FormGridRowInfo.RowPosition.AfterRowWithID));
 		//
 		// CODEGEN: This call is required by the ASP.NET Web Form Designer.
 		//
