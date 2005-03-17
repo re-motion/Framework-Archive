d3b8409c28d28d491d4e9255697d6dfb06da3af7
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-03-17 16:50:00 +0000

    Label: "1.1.46.0" User: Build        Date: 17.03.05   Time:  8:19 Labeled

--- Data/DomainObjects/Persistence/Rdbms/DataContainerFactory.cs
+++ Data/DomainObjects/Persistence/Rdbms/DataContainerFactory.cs
@@ -228,6 +228,7 @@ public class DataContainerFactory
       case "System.Char":
         return GetCharValue (propertyDefinition, _dataReader.GetString (columnOrdinal));
 
+      // TODO: Move this code to MappingUtility. Review when adding new Na* types.
       case "Rubicon.NullableValueTypes.NaBoolean":
         return new NaBoolean (_dataReader.GetBoolean (columnOrdinal));
 
@@ -288,6 +289,7 @@ public class DataContainerFactory
 
   protected virtual object GetNullValue (PropertyDefinition propertyDefinition)
   {
+    // TODO: Move this code to MappingUtility. Review when adding new Na* types.
     switch (propertyDefinition.PropertyType.FullName)
     {
       case "Rubicon.NullableValueTypes.NaBoolean":
