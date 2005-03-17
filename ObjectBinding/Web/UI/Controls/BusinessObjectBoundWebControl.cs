84903dc2e495225b0589e1247da7db25b7814ae8
Stefan Wenig <stefan.wenig@rubicon.eu>
2005-03-17 12:04:00 +0000

    Label: "1.1.46.0" User: Build        Date: 17.03.05   Time:  8:19 Labeled

--- ObjectBinding/Web/UI/Controls/BusinessObjectBoundWebControl.cs
+++ ObjectBinding/Web/UI/Controls/BusinessObjectBoundWebControl.cs
@@ -105,18 +105,14 @@ public abstract class BusinessObjectBoundWebControl: WebControl, IBusinessObject
     ArgumentUtility.CheckNotNull ("businessObject", businessObject);
     ArgumentUtility.CheckNotNull ("businessObjectProvider", businessObjectProvider);
 
-    IBusinessObjectService service = businessObjectProvider.GetService (typeof (IBusinessObjectWebUIService));
-
-    IBusinessObjectWebUIService webUIService = service as IBusinessObjectWebUIService;
-
     IconInfo iconInfo = null;
+
+    IBusinessObjectWebUIService webUIService = 
+        (IBusinessObjectWebUIService) businessObjectProvider.GetService (typeof (IBusinessObjectWebUIService));
+
     if (webUIService != null)
-    {
-      if (businessObject != null)
-        iconInfo = webUIService.GetIcon (businessObject);
-      else
-        iconInfo = webUIService.GetNullValueIcon ();
-    }
+      iconInfo = webUIService.GetIcon (businessObject);
+
     return iconInfo;
   }
 
