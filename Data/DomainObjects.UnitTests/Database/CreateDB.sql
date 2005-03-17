69ebe8b4e47cba27e42167fc6b7eb213394a1ab5
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-03-17 09:22:00 +0000

    Label: "1.1.46.0" User: Build        Date: 17.03.05   Time:  8:19 Labeled

--- Data/DomainObjects.UnitTests/Database/CreateDB.sql
+++ Data/DomainObjects.UnitTests/Database/CreateDB.sql
@@ -2,12 +2,12 @@
 CREATE DATABASE TestDomain
 ON PRIMARY (
 	Name = 'TestDomain_Data',
-	Filename = 'C:\Development\Libraries\Commons\Data\DomainObjects\UnitTests\Database\TestDomain.mdf',
+	Filename = 'C:\Databases\TestDomain.mdf',
 	Size = 10MB
 )
 LOG ON (
 	Name = 'TestDomain_Log',
-	Filename = 'C:\Development\Libraries\Commons\Data\DomainObjects\UnitTests\Database\TestDomain.ldf',
+	Filename = 'C:\Databases\TestDomain.ldf',
 	Size = 10MB	
 )
 GO
