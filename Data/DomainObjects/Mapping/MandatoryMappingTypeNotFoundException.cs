37a0a9e5282f81f01c94fcb1424f72aeb92be581
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-03-21 13:51:00 +0000

    Label: "1.2.2.0" User: Build        Date: 18.03.05   Time: 19:30 Labeled

---
+++ Data/DomainObjects/Mapping/MandatoryMappingTypeNotFoundException.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Runtime.Serialization;
+
+using Rubicon.Utilities;
+
+namespace Rubicon.Data.DomainObjects
+{
+// TODO documentation:
+[Serializable]
+public class MandatoryMappingTypeNotFoundException : DomainObjectException
+{
+  // types
+
+  // static members and constants
+
+  // member fields
+
+  private string _mappingType;
+  private bool _isNullable;
+
+  // construction and disposing
+
+  public MandatoryMappingTypeNotFoundException () : this ("A mandatory mapping type could not be found.") 
+  {
+  }
+
+  public MandatoryMappingTypeNotFoundException (string message) : base (message) 
+  {
+  }
+  
+  public MandatoryMappingTypeNotFoundException (string message, Exception inner) : base (message, inner) 
+  {
+  }
+
+  protected MandatoryMappingTypeNotFoundException (SerializationInfo info, StreamingContext context) : base (info, context) 
+  {
+    _mappingType = info.GetString ("MappingType");
+    _isNullable = info.GetBoolean ("IsNullable");
+  }
+
+  public MandatoryMappingTypeNotFoundException (string message, string mappingType, bool isNullable) : base (message) 
+  {
+    ArgumentUtility.CheckNotNullOrEmpty ("mappingType", mappingType);
+
+    _mappingType = mappingType;
+    _isNullable = isNullable;
+  }
+
+  // methods and properties
+
+  public string MappingType
+  {
+    get { return _mappingType; }
+  }
+
+  public bool IsNullable
+  {
+    get { return _isNullable; }
+  }
+
+  /// <summary>
+  /// Sets the SerializationInfo object with the parameter name and additional exception information.
+  /// </summary>
+  /// <param name="info">The object that holds the serialized object data.</param>
+  /// <param name="context">The contextual information about the source or destination.</param>
+  public override void GetObjectData (SerializationInfo info, StreamingContext context)
+  {
+    base.GetObjectData (info, context);
+
+    info.AddValue ("MappingType", _mappingType);
+    info.AddValue ("IsNullable", _isNullable);
+  }
+}
+}
---
+++ Data/DomainObjects/Persistence/Rdbms/ValueConverter.cs
@@ -0,0 +1,221 @@
+using System;
+using System.Data;
+
+using Rubicon.Data.DomainObjects.Mapping;
+using Rubicon.NullableValueTypes;
+using Rubicon.Utilities;
+
+namespace Rubicon.Data.DomainObjects.Persistence
+{
+public sealed class DBValueConverter
+{
+  // types
+
+  // static members and constants
+
+  public static object GetDBValue (object value)
+  {
+    if (value == null)
+      return DBNull.Value;
+
+    Type type = value.GetType ();
+
+    INaNullable naValueType = value as INaNullable;
+    if (naValueType != null)
+    {
+      if (!naValueType.IsNull)
+        return naValueType.Value;
+      else
+        return DBNull.Value;
+    }
+
+    if (type.IsEnum)
+      return (int) value;
+
+    // TODO: Remove this line when char is removed 
+    if (type == typeof (char))
+      return value.ToString ();
+    
+    return value;
+  }
+
+  public static ObjectID GetObjectID (
+      ClassDefinition classDefinition, 
+      object value)
+  {
+    if (value == null)
+      return null;
+
+    if (value.GetType () == typeof (string))
+    {
+      ObjectID id = null;
+      try
+      {
+        id = ObjectID.Parse ((string) value);
+      }
+      catch (ArgumentException)
+      {
+      }
+      catch (FormatException)
+      {
+      }
+
+      if (id != null)
+        return id;
+    }
+
+    return new ObjectID (classDefinition.StorageProviderID, classDefinition.ID, value);
+  }
+
+  public static object GetValue (ClassDefinition classDefinition, PropertyDefinition propertyDefinition, IDataReader dataReader)
+  {
+    ArgumentUtility.CheckNotNull ("classDefinition", classDefinition);
+    ArgumentUtility.CheckNotNull ("propertyDefinition", propertyDefinition);
+    ArgumentUtility.CheckNotNull ("dataReader", dataReader);
+
+    int columnOrdinal = dataReader.GetOrdinal (propertyDefinition.ColumnName);
+
+    if (dataReader.IsDBNull (columnOrdinal))
+      return propertyDefinition.DefaultValue;
+
+    if (propertyDefinition.PropertyType == typeof (ObjectID))
+    {
+      ClassDefinition relatedClassDefinition = GetRelatedClassDefinition (propertyDefinition, dataReader);
+      if (relatedClassDefinition == null)
+      {
+        relatedClassDefinition = GetRelatedClassDefinition (classDefinition, propertyDefinition);
+
+        if (relatedClassDefinition.BaseClass != null)
+        {
+          throw CreateStorageProviderException (
+              "Incorrect database format encountered."
+              + " Class must have column '{0}' defined, because it points to derived class '{1}'.",
+              GetRelatedClassIDColumnName (propertyDefinition.ColumnName), 
+              relatedClassDefinition.ID);    
+        }
+
+        ClassDefinitionCollection derivedClasses = 
+            MappingConfiguration.Current.ClassDefinitions.GetDirectlyDerivedClassDefinitions (relatedClassDefinition);
+
+        if (derivedClasses.Count > 0)
+        {
+          throw CreateStorageProviderException (
+              "Incorrect database format encountered."
+              + " Class must have column '{0}' defined, because at least one class inherits from '{1}'.",
+              GetRelatedClassIDColumnName (propertyDefinition.ColumnName), 
+              relatedClassDefinition.ID);    
+        }
+      }
+
+      return GetObjectID (relatedClassDefinition, dataReader.GetValue (columnOrdinal));
+    }
+
+    if (propertyDefinition.PropertyType.IsEnum)
+      return GetEnumValue (propertyDefinition, dataReader.GetValue (columnOrdinal));
+
+    // TODO: Remove line below and method GetCharValue:
+    if (propertyDefinition.PropertyType == typeof (char))
+      return GetCharValue (propertyDefinition, dataReader.GetString (columnOrdinal));
+
+    if (propertyDefinition.PropertyType == typeof (NaBoolean))
+      return new NaBoolean (dataReader.GetBoolean (columnOrdinal));
+
+    if (propertyDefinition.PropertyType == typeof (NaDateTime))
+      return new NaDateTime (dataReader.GetDateTime (columnOrdinal));
+
+    if (propertyDefinition.PropertyType == typeof (NaDouble))
+      return new NaDouble (dataReader.GetDouble (columnOrdinal));
+
+    if (propertyDefinition.PropertyType == typeof (NaInt32))
+      return new NaInt32 (dataReader.GetInt32 (columnOrdinal));
+
+    return dataReader.GetValue (columnOrdinal);
+  }
+
+  private static ClassDefinition GetRelatedClassDefinition (PropertyDefinition propertyDefinition, IDataReader dataReader)
+  {
+    string relatedClassIDColumnName = GetRelatedClassIDColumnName (propertyDefinition.ColumnName);
+    try
+    {
+      string classID = dataReader.GetString (dataReader.GetOrdinal (relatedClassIDColumnName));
+      return MappingConfiguration.Current.ClassDefinitions.GetMandatory (classID);
+    }
+    catch (IndexOutOfRangeException)
+    {
+    }
+
+    return null;
+  }
+
+  private static ClassDefinition GetRelatedClassDefinition (
+      ClassDefinition classDefinition,
+      PropertyDefinition propertyDefinition)
+  {
+    ClassDefinition relatedClassDefinition = classDefinition.GetRelatedClassDefinition (
+        propertyDefinition.PropertyName);
+
+    if (relatedClassDefinition == null)
+    {
+      throw new StorageProviderException (string.Format (
+          "Property '{0}' of class '{1}' has no relations assigned.", 
+          propertyDefinition.PropertyName, classDefinition.ID));  
+    }
+
+    return relatedClassDefinition;
+  }
+
+  private static char GetCharValue (PropertyDefinition propertyDefinition, string stringValue)
+  {
+    if (stringValue.Length == 1)
+    {
+      return stringValue[0];
+    }
+    else 
+    {
+      throw CreateStorageProviderException (
+          "Value '{0}' is not supported for System.Char datatype, property: {1}.",
+          stringValue, propertyDefinition.PropertyName);
+    }
+  }
+
+  private static object GetEnumValue (PropertyDefinition propertyDefinition, object dataValue)
+  {
+    if (Enum.IsDefined (propertyDefinition.PropertyType, dataValue))
+      return Enum.ToObject (propertyDefinition.PropertyType, dataValue);
+
+    throw CreateStorageProviderException (
+        "Enumeration '{0}' does not define the value '{1}', property '{2}'.",
+        propertyDefinition.PropertyType.FullName, dataValue, propertyDefinition.PropertyName);
+  }
+
+  private static string GetRelatedClassIDColumnName (string columnName)
+  {
+    return columnName + "ClassID";
+  }
+
+  private static StorageProviderException CreateStorageProviderException (
+      string formatString,
+      params object[] args)
+  {
+    return CreateStorageProviderException (null, formatString, args);
+  }
+
+  private static StorageProviderException CreateStorageProviderException (
+      Exception innerException,
+      string formatString,
+      params object[] args)
+  {
+    return new StorageProviderException (string.Format (formatString, args), innerException);
+  }
+
+  // member fields
+
+  // construction and disposing
+
+  private DBValueConverter ()
+  {
+  }
+
+  // methods and properties
+}
+}
