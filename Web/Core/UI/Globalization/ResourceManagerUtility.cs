2c204ae64df383e619ee1b87814873327a5a817c
Stefan Wenig <stefan.wenig@rubicon.eu>
2005-03-21 13:34:00 +0000

    Label: "1.2.2.0" User: Build        Date: 18.03.05   Time: 19:30 Labeled

--- Web/Core/UI/Globalization/ResourceManagerUtility.cs
+++ Web/Core/UI/Globalization/ResourceManagerUtility.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Web.UI;
 using Rubicon.Globalization;
+using Rubicon.Collections;
 
 namespace Rubicon.Web.Utilities
 {
@@ -10,26 +11,44 @@ namespace Rubicon.Web.Utilities
 public class ResourceManagerUtility
 {
   /// <summary>
-  ///   Find the closest parent <see cref="Control"/> impementing
-  ///   <see cref="IObjectWithResources"/>.
+  ///   Get resource managers of all controls impementing <see cref="IObjectWithResources"/> in the 
+  ///   current control's hierarchy (parents last).
   /// </summary>
   /// <param name="control">
   ///   The <see cref="Control"/> where to start searching for <see cref="IObjectWithResources"/>.
   /// </param>
   /// <returns>
-  ///   An <see cref="IResourceManager"/> or <see langname="null"/> if not implemented.
+  ///   An <see cref="IResourceManager"/> or <see langname="null"/> if not implemented. If more than
+  ///   one resource manager is found, an <see cref="ResourceManagerSet"/> is returned.
   /// </returns>
   public static IResourceManager GetResourceManager (Control control)
   {
     if (control == null)
       return null;
 
+    TypedArrayList resourceManagers = new TypedArrayList (typeof (IResourceManager));
+
+    GetResourceManagersRecursive (control, resourceManagers);
+
+    if (resourceManagers.Count == 0)
+      return null;
+    else if (resourceManagers.Count == 1)
+      return resourceManagers[0];
+    else
+      return new ResourceManagerSet ((IResourceManger[]) resourceManagers.ToArray());
+  }
+
+  private void GetResourceManagersRecursive (Control control, TypedArrayList resourceManagers)
+  {
+    if (control == null)
+      return;
+
     IObjectWithResources objectWithResources  = control as IObjectWithResources;
 
     if (objectWithResources != null)
-      return objectWithResources.GetResourceManager();
+      return resourceManagers.Add (objectWithResources.GetResourceManager());
 
-    return GetResourceManager (control.Parent);
+    GetResourceManagersRecursive (control.Parent, resourceManagers);
   }
 
   //  No construction for static only class
