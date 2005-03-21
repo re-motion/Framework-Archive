9b1c42a4d77a9035f49915e43eed6878784e300e
Michael Ketting <michael.ketting@rubicon.eu>
2005-03-21 15:09:00 +0000

    Label: "1.2.2.0" User: Build        Date: 18.03.05   Time: 19:30 Labeled

--- ObjectBinding/Web/UI/Controls/BocCommand.cs
+++ ObjectBinding/Web/UI/Controls/BocCommand.cs
@@ -5,6 +5,8 @@ using System.Drawing.Design;
 using Rubicon.Utilities;
 using Rubicon.Web.UI;
 using Rubicon.Web.UI.Controls;
+using Rubicon.Web.ExecutionEngine;
+using Rubicon.Collections;
 
 namespace Rubicon.ObjectBinding.Web.Controls
 {
@@ -12,6 +14,88 @@ namespace Rubicon.ObjectBinding.Web.Controls
 [TypeConverter (typeof (ExpandableObjectConverter))]
 public class BocCommand: Command
 {
+  /// <summary> Wraps the properties required for rendering a hyperlink. </summary>
+  [TypeConverter (typeof (ExpandableObjectConverter))]
+  public class BocHrefCommandInfo: Command.HrefCommandInfo
+  {
+    /// <summary> Initalizes a new instance </summary>
+    public BocHrefCommandInfo()
+    {
+    }
+
+    /// <summary> Gets or sets the URL to link to when the rendered command is clicked. </summary>
+    /// <value> 
+    ///   The URL to link to when the rendered command is clicked. The default value is <see cref="String.Empty"/>. 
+    /// </value>
+    [Description ("The hyperlink reference of the command. Use {0} to insert the Business Object's ID.")]
+    public override string Href 
+    {
+      get { return base.Href; }
+      set { base.Href = value; }
+    }
+  }
+
+  /// <summary> Wraps the properties required for calling a WxeFunction. </summary>
+  [TypeConverter (typeof (ExpandableObjectConverter))]
+  public class BocWxeFunctionCommandInfo: Command.WxeFunctionCommandInfo
+  {
+    /// <summary> Initalizes a new instance </summary>
+    public BocWxeFunctionCommandInfo()
+    {
+    }
+
+    /// <summary> 
+    ///   Gets or sets the comma separated list of parameters passed to the WxeFunction when the rendered 
+    ///   command is clicked.
+    /// </summary>
+    /// <remarks>
+    ///   The following reference parameters can be added to the list of parameters.
+    ///   <list type="table">
+    ///     <listheader>
+    ///       <term> Name </term>
+    ///       <description> Contents </description>
+    ///     </listheader>
+    ///     <item>
+    ///       <term> id </term>
+    ///       <description> The ID, if the object is of type <see cref="IBusinessObjectWithIdentity"/>. </description>
+    ///     </item>
+    ///     <item>
+    ///       <term> object </term>
+    ///       <description> The <see cref="IBusinessObject"/> itself. </description>
+    ///     </item>
+    ///     <item>
+    ///       <term> parent </term>
+    ///       <description> The containing <see cref="IBusinessObject"/>. </description>
+    ///     </item>
+    ///     <item>
+    ///       <term> parentproperty </term>
+    ///       <description> The <see cref="IBusinessObjectReferenceProperty"/> used to acess the object. </description>
+    ///     </item>
+    ///   </list>
+    /// </remarks>
+    /// <value> 
+    ///   The comma separated list of parameters passed to the WxeFunction when the rendered 
+    ///   command is clicked. The default value is <see cref="String.Empty"/>. 
+    /// </value>
+    [Description ("A comma separated list of parameters for the command. The following reference parameter are available: id, object, parent, parentproperty.")]
+    public override string Parameters
+    {
+      get { return base.Parameters; }
+      set { base.Parameters = value; }
+    }
+  }
+
+  /// <summary>
+  ///   The <see cref="BocHrefCommandInfo"/> used when rendering the command as a hyperlink.
+  /// </summary>
+  private BocHrefCommandInfo _hrefCommand = new BocHrefCommandInfo();
+  /// <summary>
+  ///   The <see cref="BocWxeFunctionCommandInfo"/> used when rendering the command as a <see cref="WxeFunction"/>.
+  /// </summary>
+  private BocWxeFunctionCommandInfo _wxeFunctionCommand = new BocWxeFunctionCommandInfo();
+
+  public BocCommandClickEventHandler Click;
+
   /// <summary> Initializes an instance. </summary>
   public BocCommand()
     : this (CommandType.None)
@@ -24,6 +108,89 @@ public class BocCommand: Command
   {
   }
 
+  /// <summary> Fires the <see cref="Click"/> event. </summary>
+  public virtual void OnClick (IBusinessObject businessObject)
+  {
+    if (Click != null)
+    {
+      BocCommandClickEventArgs e = new BocCommandClickEventArgs (businessObject);
+      Click (this, e);
+    }
+  }
+
+  /// <summary> Renders the opening tag for the command. </summary>
+  /// <param name="writer"> The <see cref="HtmlTextWriter"/> object to use. </param>
+  /// <param name="postBackLink">
+  ///   The string rendered in the <c>href</c> tag of the anchor element when the command type is
+  ///   <see cref="CommandType.Event"/> or <see cref="CommandType.WxeFunction"/>.
+  ///   This string is usually the call to the <c>__doPostBack</c> script function used by ASP.net
+  ///   to force a post back.
+  /// </param>
+  /// <param name="onClick"> 
+  ///   The string rendered in the <c>onClick</c> tag of the anchor element. 
+  /// </param>
+  /// <param name="businessObjectID">
+  ///   An identifier for the <see cref="IBusinessObject"/> to which the rendered command is 
+  ///   applied.
+  /// </param>
+  public void RenderBegin (
+      HtmlTextWriter writer,
+      string postBackLink,
+      string onClick, 
+      string businessObjectID)
+  {
+    base.RenderBegin (writer, postBackLink, onClick, businessObjectID);
+  }
+
+  /// <summary>
+  ///   Executes the <see cref="WxeFunction"/> defined by the <see cref="WxeFunctionCommand"/>.
+  /// </summary>
+  /// <param name="wxePage"> The <see cref="IWxePage"/> where this command is rendered on. </param>
+  /// <param name="listIndex"> 
+  ///   The index of the <see cref="IBusinessObject"/> in the row where the command was clicked.
+  /// </param>
+  /// <param name="businessObject">
+  ///   The <see cref="IBusinessObject"/> in the row where the command was clicked.
+  /// </param>
+  public void ExecuteWxeFunction (IWxePage wxePage, IBusinessObject businessObject)
+  {
+    if (! WxeContext.Current.IsReturningPostBack)
+    {
+      NameObjectCollection parameters = new NameObjectCollection();
+      parameters["object"] = businessObject;
+      if (businessObject is IBusinessObjectWithIdentity)
+        parameters["id"] = ((IBusinessObjectWithIdentity) businessObject).UniqueIdentifier;
+      if (OwnerControl != null)
+      {
+        if (OwnerControl.DataSource != null && OwnerControl.Value != null)
+          parameters["parent"] = OwnerControl.DataSource.BusinessObject;
+        if (OwnerControl.Property != null)
+          parameters["parentproperty"] = OwnerControl.Property;
+      }
+      ExecuteWxeFunction (wxePage, parameters);
+    }
+  }
+
+  /// <summary> The <see cref="BocValueHrefCommandInfo"/> used when rendering the command as a hyperlink. </summary>
+  /// <remarks> Only interpreted if <see cref="Type"/> is set to <see cref="CommandType.Href"/>. </remarks>
+  /// <value> A <see cref="BocHrefCommandInfo"/> object. </value>
+  public override HrefCommandInfo HrefCommand
+  {
+    get { return _hrefCommand;  }
+    set { _hrefCommand = (BocHrefCommandInfo) value; }
+  }
+
+  /// <summary>
+  ///   The <see cref="BocWxeFunctionCommandInfo"/> used when rendering the command as a <see cref="WxeFunction"/>.
+  /// </summary>
+  /// <remarks> Only interpreted if <see cref="Type"/> is set to <see cref="CommandType.WxeFunction"/>. </remarks>
+  /// <value> A <see cref="BocWxeFunctionCommandInfo"/> object. </value>
+  public override WxeFunctionCommandInfo WxeFunctionCommand
+  {
+    get { return _wxeFunctionCommand; }
+    set { _wxeFunctionCommand = (BocWxeFunctionCommandInfo) value; }
+  }
+  
   /// <summary> Gets or sets the <see cref="IBusinessObjectBoundWebControl"/> to which this object belongs. </summary>
   [DesignerSerializationVisibility (DesignerSerializationVisibility.Hidden)]
   [Browsable (false)]
@@ -37,6 +204,33 @@ public class BocCommand: Command
   {
     get { return (Control) OwnerControl; }
     set { OwnerControl = (IBusinessObjectBoundWebControl) value; }
+  }
+}
+
+/// <summary>
+///   Represents the method that handles the <see cref="BocCommand.Click"/> event
+///   raised when clicking on a <see cref="Command"/> of type <see cref="CommandType.Event"/>.
+/// </summary>
+public delegate void BocCommandClickEventHandler (object sender, BocCommandClickEventArgs e);
+
+/// <summary> Provides data for the <see cref="BocCommand.Click"/> event. </summary>
+public class BocCommandClickEventArgs: EventArgs
+{
+  private IBusinessObject _businessObject;
+
+  /// <summary> Initializes a new instance. </summary>
+  public BocCommandClickEventArgs (
+      IBusinessObject businessObject)
+  {
+    _businessObject = businessObject;
+  }
+
+  /// <summary>
+  ///   The <see cref="IBusinessObject"/> on which the rendered command is applied on.
+  /// </summary>
+  public IBusinessObject BusinessObject
+  {
+    get { return _businessObject; }
   }
 }
 
