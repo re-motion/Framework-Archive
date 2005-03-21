f3f7915082a8ac985ca306fea1df1a738a78a127
Stefan Wenig <stefan.wenig@rubicon.eu>
2005-03-21 17:39:00 +0000

    Label: "1.2.2.0" User: Build        Date: 18.03.05   Time: 19:30 Labeled

--- localbuild.cmd
+++ localbuild.cmd
@@ -1,1 +1,3 @@
-nant local clean resources && nant local debug && nant local cleantemp 
+@set target=%1
+@if "%1"=="" set target=all
+nant local clean resources && nant local setdebug %target% && nant local cleantemp 
