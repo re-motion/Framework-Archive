0e1aa25ed1c4a07ea91aaace86e261942b771076
Michael Ketting <michael.ketting@rubicon.eu>
2005-03-22 10:35:00 +0000

    Label: "1.2.3.0" User: Build        Date: 21.03.05   Time: 20:46 Labeled

---
+++ Web/Core/UI/Controls/WebButton.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.ComponentModel;
+
+namespace Rubicon.Web.UI.Controls
+{
+
+  [ToolboxData("<{0}:WebButton runat=server></{0}:WebButton>")]
+public class WebButton : Button
+{
+}
+
+}
