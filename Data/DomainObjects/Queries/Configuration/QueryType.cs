5aa809920102a078575b5e3f7f9eae8e533112d3
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-03-22 15:20:00 +0000

    Label: "1.2.4.0" User: Build        Date: 22.03.05   Time: 11:40 Labeled

--- Data/DomainObjects/Queries/Configuration/QueryType.cs
+++ Data/DomainObjects/Queries/Configuration/QueryType.cs
@@ -2,9 +2,19 @@ using System;
 
 namespace Rubicon.Data.DomainObjects.Queries.Configuration
 {
+/// <summary>
+/// Indicates the type of a <see cref="QueryDefinition"/>.
+/// </summary>
 public enum QueryType
 {
+  /// <summary>
+  /// Instances of a <see cref="QueryDefinition"/> return a collection of <see cref="DomainObject"/>s.
+  /// </summary>
   Collection = 0,
+
+  /// <summary>
+  /// Instances of a <see cref="QueryDefinition"/> return only a single value.
+  /// </summary>
   Scalar = 1
 }
 }
