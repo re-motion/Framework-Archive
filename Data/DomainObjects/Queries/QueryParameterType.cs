dc4e292e203df355a7e73576ee9d72f1a85bdf85
Ernst Scheithauer <ernst.scheithauer@rubicon.eu>
2005-03-22 15:34:00 +0000

    Label: "1.2.4.0" User: Build        Date: 22.03.05   Time: 11:40 Labeled

--- Data/DomainObjects/Queries/QueryParameterType.cs
+++ Data/DomainObjects/Queries/QueryParameterType.cs
@@ -2,9 +2,19 @@ using System;
 
 namespace Rubicon.Data.DomainObjects.Queries
 {
+/// <summary>
+/// Indicates the type of a <see cref="QueryParameter"/>.
+/// </summary>
 public enum QueryParameterType
 {
+  /// <summary>
+  /// Instances of <see cref="QueryParameter"/> will be treated as parameters in the query.
+  /// </summary>
   Value = 0,
+
+  /// <summary>
+  /// Instances of <see cref="QueryParameter"/> will replaced inline into the query.
+  /// </summary>
   Text = 1
 }
 }
