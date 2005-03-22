3c69530e9ce0da985a9fce9434cafd969ac8414a
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-03-22 13:19:00 +0000

    Label: "1.2.4.0" User: Build        Date: 22.03.05   Time: 11:40 Labeled

--- Core.Test/NullableValueTypes/SerializationTests.cs
+++ Core.Test/NullableValueTypes/SerializationTests.cs
@@ -22,6 +22,72 @@ public class SerializationTests
     CheckSerialization (NaInt32.MaxValue);
   }
 
+  [Test]
+  public void SerializeNaByte ()
+  {
+    CheckSerialization (NaByte.Null);
+    CheckSerialization (new NaByte (32));
+    CheckSerialization (NaByte.Zero);
+    CheckSerialization (NaByte.MinValue);
+    CheckSerialization (NaByte.MaxValue);
+  }
+
+  [Test]
+  public void SerializeNaDecimal ()
+  {
+    CheckSerialization (NaDecimal.Null);
+    CheckSerialization (new NaDecimal (32));
+    CheckSerialization (new NaDecimal (-32));
+    CheckSerialization (new NaDecimal (new decimal (321.12345)));
+    CheckSerialization (new NaDecimal (new decimal (-321.12345)));
+    CheckSerialization (NaDecimal.Zero);
+    CheckSerialization (NaDecimal.MinValue);
+    CheckSerialization (NaDecimal.MaxValue);
+  }
+
+  [Test]
+  public void SerializeNaGuid ()
+  {
+    CheckSerialization (NaGuid.Null);
+    CheckSerialization (new NaGuid (Guid.NewGuid ()));
+    CheckSerialization (NaGuid.Empty);
+  }
+
+  [Test]
+  public void SerializeNaInt16()
+  {
+    CheckSerialization (NaInt16.Null);
+    CheckSerialization (new NaInt16 (32));
+    CheckSerialization (new NaInt16 (-32));
+    CheckSerialization (NaInt16.Zero);
+    CheckSerialization (NaInt16.MinValue);
+    CheckSerialization (NaInt16.MaxValue);
+  }
+
+  [Test]
+  public void SerializeNaInt64()
+  {
+    CheckSerialization (NaInt64.Null);
+    CheckSerialization (new NaInt64 (32));
+    CheckSerialization (new NaInt64 (-32));
+    CheckSerialization (NaInt64.Zero);
+    CheckSerialization (NaInt64.MinValue);
+    CheckSerialization (NaInt64.MaxValue);
+  }
+
+  [Test]
+  public void SerializeNaSingle ()
+  {
+    CheckSerialization (NaSingle.Null);
+    CheckSerialization (new NaSingle (32));
+    CheckSerialization (new NaSingle (-32));
+    CheckSerialization (new NaSingle (321.12345F));
+    CheckSerialization (new NaSingle (-321.12345F));
+    CheckSerialization (NaSingle.Zero);
+    CheckSerialization (NaSingle.MinValue);
+    CheckSerialization (NaSingle.MaxValue);
+  }
+
   private object SerializeAndDeserialize (object obj)
   {
     MemoryStream stream = new MemoryStream ();
