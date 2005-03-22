7e9f7a146c8a2157c59f7f0f0a580e011516c39a
Ernst Scheithauer <ernst.scheithauer@rubicon.eu>
2005-03-22 15:01:00 +0000

    Label: "1.2.4.0" User: Build        Date: 22.03.05   Time: 11:40 Labeled

--- Data/DomainObjects/Queries/QueryParameter.cs
+++ Data/DomainObjects/Queries/QueryParameter.cs
@@ -4,6 +4,9 @@ using Rubicon.Utilities;
 
 namespace Rubicon.Data.DomainObjects.Queries
 {
+/// <summary>
+/// Represents a parameter that is used in a query.
+/// </summary>
 public class QueryParameter
 {
   // types
@@ -18,10 +21,24 @@ public class QueryParameter
 
   // construction and disposing
 
+  /// <summary>
+  /// Initializes a new instance of the <see cref="QueryParameter"/> class with a <see cref="ParameterType"/> of <see cref="QueryParameterType.Value"/>.
+  /// </summary>
+  /// <param name="name">The name of the parameter.</param>
+  /// <param name="value">The value of the parameter.</param>
   public QueryParameter (string name, object value) : this (name, value, QueryParameterType.Value)
   {
   }
 
+  /// <summary>
+  /// Initializes a new instance of the <see cref="QueryParameter"/> class.
+  /// </summary>
+  /// <param name="name">The name of the parameter.</param>
+  /// <param name="value">The value of the parameter.</param>
+  /// <param name="parameterType">The <see cref="QueryParameterType"/> of the parameter.</param>
+  /// <exception cref="System.ArgumentNullException"><i>name</i> is a null reference.</exception>
+  /// <exception cref="Rubicon.Utilities.ArgumentEmptyException"><i>name</i> is an empty string.</exception>
+  /// <exception cref="System.ArgumentOutOfRangeException"><i>parameterType</i> is not a valid enum value.</exception>
   public QueryParameter (string name, object value, QueryParameterType parameterType)
   {
     ArgumentUtility.CheckNotNullOrEmpty ("name", name);
@@ -34,16 +51,25 @@ public class QueryParameter
 
   // methods and properties
 
+  /// <summary>
+  /// Gets the name of the <see cref="QueryParameter"/>.
+  /// </summary>
   public string Name
   {
     get { return _name; }
   }
 
+  /// <summary>
+  /// Gets the value of the <see cref="QueryParameter"/>.
+  /// </summary>
   public object Value
   {
     get { return _value; }
   }
 
+  /// <summary>
+  /// Gets the <see cref="QueryParameterType"/> of the <see cref="QueryParameter"/>.
+  /// </summary>
   public QueryParameterType ParameterType
   {
     get { return _parameterType; }
