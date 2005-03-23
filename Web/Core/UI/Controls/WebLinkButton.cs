c55bc29b938f17d82e333b5b6efc6851f3b06343
Michael Ketting <michael.ketting@rubicon.eu>
2005-03-23 14:42:00 +0000

    Label: "1.2.5.0" User: Build        Date: 22.03.05   Time: 20:06 Labeled

---
+++ Web/Core/UI/Controls/WebLinkButton.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.ComponentModel;
+using Rubicon.Utilities;
+
+namespace Rubicon.Web.UI.Controls
+{
+
+/// <summary> A <c>LinkButton</c> using <c>&</c> as access key prefix in <see cref="LinkButton.Text"/>. </summary>
+[ToolboxData("<{0}:WebLinkButton runat=server></{0}:WebLinkButton>")]
+public class WebLinkButton : LinkButton
+{
+  private string _text;
+
+  protected override void AddAttributesToRender(HtmlTextWriter writer)
+  {
+    string accessKey;
+    _text = StringUtility.NullToEmpty (Text);
+    _text = SmartLabel.FormatLabelText (_text, false, out accessKey);
+
+    if (StringUtility.IsNullOrEmpty (AccessKey))
+      writer.AddAttribute (HtmlTextWriterAttribute.Accesskey, accessKey);
+
+    base.AddAttributesToRender (writer);
+  }
+
+  protected override void RenderContents(HtmlTextWriter writer)
+  {
+    if (HasControls())
+      base.RenderContents (writer);
+    else
+      writer.Write (_text);
+  }
+}
+
+}
