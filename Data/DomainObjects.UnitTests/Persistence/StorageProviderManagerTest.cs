00f5edcef24995c3c81ae717fb9394898e8d0b3d
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-03-24 13:00:00 +0000

    Label: "1.2.6.0" User: Build        Date: 23.03.05   Time: 16:44 Labeled

--- Data/DomainObjects.UnitTests/Persistence/StorageProviderManagerTest.cs
+++ Data/DomainObjects.UnitTests/Persistence/StorageProviderManagerTest.cs
@@ -2,6 +2,7 @@ using System;
 using NUnit.Framework;
 
 using Rubicon.Data.DomainObjects.Persistence;
+using Rubicon.Data.DomainObjects.Persistence.Rdbms;
 using Rubicon.Data.DomainObjects.UnitTests.Factories;
 
 namespace Rubicon.Data.DomainObjects.UnitTests.Persistence
