1d6e90066de5f2df7f663074b3705f61993df22b
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-03-24 06:55:00 +0000

    Label: "1.2.6.0" User: Build        Date: 23.03.05   Time: 16:44 Labeled

--- Data/DomainObjects.UnitTests/AssemblyInfo.cs
+++ Data/DomainObjects.UnitTests/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyVersion("1.0.*")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
@@ -57,5 +57,5 @@ using System.Runtime.CompilerServices;
 //       documentation for more information on this.
 //
 [assembly: AssemblyDelaySign(false)]
-[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyKeyFile(@"\development\global\rubicon.snk")]
 [assembly: AssemblyKeyName("")]
