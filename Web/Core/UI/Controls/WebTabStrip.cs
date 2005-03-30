56c1e5e27ee1dfb3f6e9abc98cabab8ecb3dff3d
Michael Ketting <michael.ketting@rubicon.eu>
2005-03-30 10:25:00 +0000

    Label: "1.2.9.0" User: Build        Date: 29.03.05   Time:  8:51 Labeled

---
+++ Web/Core/UI/Controls/WebTabStrip.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.ComponentModel;
+
+namespace Rubicon.Web.UI.Controls
+{
+
+[ToolboxData("<{0}:WebTabStrip runat=server></{0}:WebTabStrip>")]
+public class WebTabStrip : WebControl
+{
+}
+
+}
