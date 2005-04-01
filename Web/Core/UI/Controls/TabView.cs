b2a298a01d30c3dfda892733fd16f7ac777d9874
Michael Ketting <michael.ketting@rubicon.eu>
2005-04-01 15:23:00 +0000

    Label: "1.2.11.0" User: Build        Date:  1.04.05   Time: 10:30 Labeled

---
+++ Web/Core/UI/Controls/TabView.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.ComponentModel;
+using Rubicon.NullableValueTypes;
+using Rubicon.Utilities;
+using Rubicon.Web.Utilities;
+using Rubicon.Web.UI.Design;
+using Rubicon.Web.UI.Globalization;
+
+namespace Rubicon.Web.UI.Controls
+{
+
+[ToolboxData("<{0}:TabView runat=server></{0}:TabView>")]
+public class TabView : WebControl, IControl
+{
+  //  constants
+
+  // statics
+
+  // types
+
+  // fields
+
+  // construction and destruction
+
+  // methods and properties
+}
+
+}
---
+++ Web/Core/UI/Controls/TabbedMultiView.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.ComponentModel;
+using Rubicon.NullableValueTypes;
+using Rubicon.Utilities;
+using Rubicon.Web.Utilities;
+using Rubicon.Web.UI.Design;
+using Rubicon.Web.UI.Globalization;
+
+namespace Rubicon.Web.UI.Controls
+{
+
+[ToolboxData("<{0}:TabbedMultiView runat=server></{0}:TabbedMultiView>")]
+public class TabbedMultiView : WebControl, IControl
+{
+  //  constants
+
+  // statics
+
+  // types
+
+  // fields
+
+  // construction and destruction
+
+  // methods and properties
+}
+
+}
