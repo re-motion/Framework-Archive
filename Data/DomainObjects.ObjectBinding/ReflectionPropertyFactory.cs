e030ba9abfe38b73266af69e1d8ba7c42c5df90c
Andreas Schlapsi <andreas.schlapsi@rubicon.eu>
2005-04-04 16:42:00 +0000

    Label: "1.2.13.0" User: Build        Date:  4.04.05   Time: 15:54 Labeled

---
+++ Data/DomainObjects.ObjectBinding/ReflectionPropertyFactory.cs
@@ -0,0 +1,149 @@
+using System;
+using System.Reflection;
+
+using Rubicon.Utilities;
+using Rubicon.NullableValueTypes;
+
+using Rubicon.ObjectBinding;
+using Rubicon.Data.DomainObjects.ObjectBinding.PropertyTypes;
+
+namespace Rubicon.Data.DomainObjects.ObjectBinding
+{
+public abstract class PropertyFactory
+{
+	public PropertyFactory()
+	{
+	}
+
+  public DomainObjectProperty CreateProperty (PropertyInfo propertyInfo)
+  {
+    ArgumentUtility.CheckNotNull ("propertyInfo", propertyInfo);
+
+    Type itemType = GetItemType (propertyInfo);
+
+    bool isNullableType = NaTypeUtility.IsNaNullableType (itemType);
+    if (isNullableType)
+      itemType = NaTypeUtility.GetBasicType (itemType);
+
+    return CreateProperty (propertyInfo, itemType, isNullableType);
+  }
+
+  protected virtual DomainObjectProperty CreateProperty (PropertyInfo propertyInfo, Type itemType, bool isNullableType)
+  {
+    bool isRequired = IsPropertyRequired (propertyInfo);
+    bool isList = IsList (propertyInfo);
+
+    if (itemType == typeof (string))
+    {
+      return new StringProperty (propertyInfo, isRequired, itemType, isList, GetMaxStringLength (propertyInfo));
+    }
+    else if (itemType == typeof (Guid))
+    {
+      return new GuidProperty (propertyInfo, isRequired, itemType, isList, isNullableType);
+    }
+    else if (itemType == typeof (byte))
+    {
+      return new ByteProperty (propertyInfo, isRequired, itemType, isList, isNullableType);
+    }
+    else if (itemType == typeof (int))
+    {
+      return new Int32Property (propertyInfo, isRequired, itemType, isList, isNullableType);
+    }
+    else if (itemType == typeof (Int16))
+    {
+      return new Int16Property (propertyInfo, isRequired, itemType, isList, isNullableType);
+    }
+    else if (itemType == typeof (Single))
+    {
+      return new SingleProperty (propertyInfo, isRequired, itemType, isList, isNullableType);
+    }
+    else if (itemType == typeof (double))
+    {
+      return new DoubleProperty (propertyInfo, isRequired, itemType, isList, isNullableType);
+    }
+    else if (itemType == typeof (decimal))
+    {
+      return new DecimalProperty (propertyInfo, isRequired, itemType, isList, isNullableType);
+    }
+    else if (itemType == typeof (bool))
+    {
+      return new BooleanProperty (propertyInfo, isRequired, itemType, isList, isNullableType);
+    }
+    else if (itemType == typeof (DateTime))
+    {
+      if (IsDateType (propertyInfo))
+        return new DateProperty (propertyInfo, isRequired, itemType, isList, isNullableType);
+      else
+        return new DateTimeProperty (propertyInfo, isRequired, itemType, isList, isNullableType);
+    }
+    else if (itemType.IsEnum)
+    {
+      return new EnumerationProperty (propertyInfo, isRequired, itemType, isList);
+    }
+    else if (typeof (IBusinessObjectWithIdentity).IsAssignableFrom (itemType))
+    {
+      return new ReferenceProperty (propertyInfo, IsRelationMandatory (propertyInfo), itemType, isList);
+    }
+    else
+    {
+      return new DomainObjectProperty (propertyInfo, isRequired, itemType, isList);
+    }
+  }
+
+  protected virtual Type GetItemType (PropertyInfo propertyInfo)
+  {
+    if (IsDomainObjectCollection (propertyInfo))
+      return GetItemTypeForDomainObjectCollection (propertyInfo);
+
+    return IsList (propertyInfo) ? propertyInfo.PropertyType.GetElementType() : propertyInfo.PropertyType;
+  }
+
+  protected virtual Type GetItemTypeForDomainObjectCollection (PropertyInfo propertyInfo)
+  {
+    Type itemType = typeof (BindableDomainObject);
+    object[] itemTypeAttributes = propertyInfo.GetCustomAttributes (typeof (ItemTypeAttribute), true);
+
+    if (itemTypeAttributes.Length > 0)
+    {
+      itemType = ((ItemTypeAttribute) itemTypeAttributes[0]).ItemType;
+      if (itemType != typeof (BindableDomainObject) && !itemType.IsSubclassOf (typeof (BindableDomainObject)))
+      {
+        throw new InvalidOperationException ("The ItemType defined for a property of type "
+          + "DomainObjectCollection or subclass of must be a DomainObject or a subclass of.");
+      }
+    }
+
+    return itemType;
+  }
+
+  protected virtual bool IsDomainObjectCollection (PropertyInfo propertyInfo)
+  {
+    return propertyInfo.PropertyType == typeof (DomainObjectCollection) || propertyInfo.PropertyType.IsSubclassOf (typeof (DomainObjectCollection));
+  }
+
+  protected virtual bool IsList (PropertyInfo propertyInfo)
+  {
+    return IsDomainObjectCollection (propertyInfo) || propertyInfo.PropertyType.IsArray;
+  }
+
+  protected virtual bool IsPropertyRequired (PropertyInfo propertyInfo)
+  {
+    return true;
+  }
+
+  protected virtual NaInt32 GetMaxStringLength (PropertyInfo propertyInfo)
+  {
+    return NaInt32.Null;
+  }
+
+  protected virtual bool IsDateType (PropertyInfo propertyInfo)
+  {
+    return false;
+  }
+
+  protected virtual bool IsRelationMandatory (PropertyInfo propertyInfo)
+  {
+    return IsPropertyRequired (propertyInfo);
+  }
+}
+}
