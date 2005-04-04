870e7d38069d9c679b5923b54a7182f430882d13
Andreas Schlapsi <andreas.schlapsi@rubicon.eu>
2005-04-04 16:42:00 +0000

    Label: "1.2.13.0" User: Build        Date:  4.04.05   Time: 15:54 Labeled

---
+++ Data/DomainObjects.ObjectBinding/BusinessObjectClassReflector.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using System.Reflection;
+using System.ComponentModel;
+
+using Rubicon.Utilities;
+using Rubicon.ObjectBinding;
+
+namespace Rubicon.Data.DomainObjects.ObjectBinding
+{
+public class BusinessObjectClassReflector
+{
+  private Type _businessObjectClassType;
+  private DomainObjectPropertyFactory _propertyFactory;
+
+	public BusinessObjectClassReflector (Type businessObjectClassType, DomainObjectPropertyFactory propertyFactory)
+	{
+    ArgumentUtility.CheckNotNull ("businessObjectClassType", businessObjectClassType);
+    ArgumentUtility.CheckNotNull ("propertyFactory", propertyFactory);
+
+    _businessObjectClassType = businessObjectClassType;
+    _propertyFactory = propertyFactory;
+  }
+
+  public Type BusinessObjectClassType
+  {
+    get { return _businessObjectClassType; }
+  }
+
+  public string Identifier
+  {
+    get { return _businessObjectClassType.FullName; }
+  }
+
+  public IBusinessObjectProperty GetPropertyDefinition (string propertyIdentifier)
+  {
+    PropertyInfo propertyInfo = _businessObjectClassType.GetProperty (propertyIdentifier);
+    return propertyInfo == null ? null : _propertyFactory.CreateProperty (propertyInfo);
+  }
+
+  public IBusinessObjectProperty[] GetPropertyDefinitions()
+  {
+    PropertyInfo[] propertyInfos = _businessObjectClassType.GetProperties ();
+    if (propertyInfos == null)
+      return new IBusinessObjectProperty[0];
+
+    ArrayList properties = new ArrayList();
+    foreach (PropertyInfo propertyInfo in propertyInfos)
+    {
+      if (UsePropertyInBusinessObject (propertyInfo))
+        properties.Add (_propertyFactory.CreateProperty (propertyInfo));
+    }
+
+    return (IBusinessObjectProperty[]) properties.ToArray (typeof (IBusinessObjectProperty));
+  }
+
+  private bool UsePropertyInBusinessObject (PropertyInfo propertyInfo)
+  {
+    EditorBrowsableAttribute[] editorBrowsableAttributes = (EditorBrowsableAttribute[]) propertyInfo.GetCustomAttributes (
+        typeof (EditorBrowsableAttribute), true);
+
+    if (editorBrowsableAttributes.Length == 1)
+    {
+      EditorBrowsableAttribute editorBrowsableAttribute = editorBrowsableAttributes[0];
+      if (editorBrowsableAttribute.State == EditorBrowsableState.Never)
+        return false;
+    }
+
+    //  Prevents the display of the indexers declared in BusinessObject.
+    //  Adding "EditorBrowsable (EditorBrowsableState.Never)" to BusinessObject 
+    //  might not be the best solution until the final way of hiding properties is established
+    if (propertyInfo.Name == "Item")
+      return false;
+
+    return true;
+  }
+}
+}
---
+++ Data/DomainObjects.ObjectBinding/BusinessObjectReflector.cs
@@ -0,0 +1,92 @@
+using System;
+using System.Reflection;
+using System.Collections;
+
+using Rubicon.Utilities;
+using Rubicon.ObjectBinding;
+using Rubicon.Data.DomainObjects.ObjectBinding.PropertyTypes;
+
+namespace Rubicon.Data.DomainObjects.ObjectBinding
+{
+public class BusinessObjectReflector
+{
+  private IBusinessObject _bindableObject;
+
+	public BusinessObjectReflector (IBusinessObject bindableObject)
+	{
+    ArgumentUtility.CheckNotNull ("bindableObject", bindableObject);
+    _bindableObject = bindableObject;
+	}
+
+  public string GetPropertyString (IBusinessObjectProperty property, string format)
+  {
+    object value;
+    int count;
+
+    if (property.IsList)
+    {
+      IList list = (IList) _bindableObject.GetProperty (property);
+      count = list.Count;
+      if (count > 0)
+        value = list[0];
+      else 
+        value = null;
+    }
+    else if (property.GetType () == typeof(EnumerationProperty))
+    {
+      object internalValue = _bindableObject.GetProperty (property);
+
+      value = ((EnumerationProperty)property).GetValueInfoByValue (internalValue).DisplayName;
+      count = 1;
+    }
+    else
+    {
+      value = _bindableObject.GetProperty (property);
+      count = 1;
+    }
+
+    if (value == null)
+      return string.Empty;
+
+    string strValue = null;
+    IBusinessObjectWithIdentity businessObject = value as IBusinessObjectWithIdentity;
+    if (businessObject != null)
+    {
+      strValue = businessObject.DisplayName;
+    }
+    else if (format != null)
+    {
+      IFormattable formattable = value as IFormattable;
+      if (formattable != null)
+        strValue = formattable.ToString (format, null);
+    }
+
+    if (strValue == null)
+      strValue = value.ToString();
+
+    if (count > 1)
+      strValue += " ... [" + count.ToString() + "]";
+    return strValue;
+  }
+
+  public object GetProperty (IBusinessObjectProperty property)
+  {
+    ArgumentUtility.CheckNotNullAndType ("property", property, typeof (DomainObjectProperty));
+    DomainObjectProperty reflectionProperty = (DomainObjectProperty) property;
+    PropertyInfo propertyInfo = reflectionProperty.PropertyInfo;
+
+    object internalValue = propertyInfo.GetValue (_bindableObject, new object[0]);
+    return reflectionProperty.FromInternalType (internalValue);
+  }
+
+  public void SetProperty (IBusinessObjectProperty property, object value)
+  {
+    ArgumentUtility.CheckNotNullAndType ("property", property, typeof (DomainObjectProperty));
+    DomainObjectProperty reflectionProperty = (DomainObjectProperty) property;
+    PropertyInfo propertyInfo = reflectionProperty.PropertyInfo;
+
+    object internalValue = reflectionProperty.ToInternalType (value);
+    propertyInfo.SetValue (_bindableObject, internalValue, new object[0]);
+  }
+}
+}
