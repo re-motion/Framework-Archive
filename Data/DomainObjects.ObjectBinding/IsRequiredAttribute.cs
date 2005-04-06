3cc2a831350b36c68ffa8a2115496fef441986a2
Andreas Schlapsi <andreas.schlapsi@rubicon.eu>
2005-04-06 13:01:00 +0000

    Label: "1.2.14.0" User: Build        Date:  5.04.05   Time: 14:42 Labeled

---
+++ Data/DomainObjects.ObjectBinding/IsRequiredAttribute.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace Rubicon.Data.DomainObjects.ObjectBinding
+{
+[AttributeUsage (AttributeTargets.Field | AttributeTargets.Property)]
+public class IsRequiredAttribute : Attribute
+{
+  private bool _isRequired;
+
+	public IsRequiredAttribute (bool isRequired)
+	{
+    _isRequired = isRequired;
+	}
+
+  public bool IsRequired
+  {
+    get { return _isRequired; }
+    set { _isRequired = value; }
+  }
+}
+}
