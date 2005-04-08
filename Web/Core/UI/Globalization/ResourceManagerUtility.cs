01ad9ff5f6757e5fafc6d0e06e700be678f61b9f
Stefan Wenig <stefan.wenig@rubicon.eu>
2005-04-08 11:02:00 +0000

    Label: "1.2.15.0" User: Build        Date:  8.04.05   Time: 12:21 Labeled

--- Web/Core/UI/Globalization/ResourceManagerUtility.cs
+++ Web/Core/UI/Globalization/ResourceManagerUtility.cs
@@ -17,18 +17,22 @@ public class ResourceManagerUtility
   /// <param name="control">
   ///   The <see cref="Control"/> where to start searching for <see cref="IObjectWithResources"/>.
   /// </param>
+  /// <param name="alwaysIncludeParents">
+  ///   If true, parent controls' resource managers are included even if a resource manager has already 
+  ///   been found in a child control.
+  /// </param>
   /// <returns>
   ///   An <see cref="IResourceManager"/> or <see langname="null"/> if not implemented. If more than
   ///   one resource manager is found, an <see cref="ResourceManagerSet"/> is returned.
   /// </returns>
-  public static IResourceManager GetResourceManager (Control control)
+  public static IResourceManager GetResourceManager (Control control, bool alwaysIncludeParents)
   {
     if (control == null)
       return null;
 
     TypedArrayList resourceManagers = new TypedArrayList (typeof (IResourceManager));
 
-    GetResourceManagersRecursive (control, resourceManagers);
+    GetResourceManagersRecursive (control, resourceManagers, alwaysIncludeParents);
 
     if (resourceManagers.Count == 0)
       return null;
@@ -38,7 +42,7 @@ public class ResourceManagerUtility
       return new ResourceManagerSet ((IResourceManager[]) resourceManagers.ToArray());
   }
 
-  private static void GetResourceManagersRecursive (Control control, TypedArrayList resourceManagers)
+  private static void GetResourceManagersRecursive (Control control, TypedArrayList resourceManagers, bool alwaysIncludeParents)
   {
     if (control == null)
       return;
@@ -48,7 +52,8 @@ public class ResourceManagerUtility
     if (objectWithResources != null)
       resourceManagers.Add (objectWithResources.GetResourceManager());
 
-    GetResourceManagersRecursive (control.Parent, resourceManagers);
+    if (objectWithResources == null || alwaysIncludeParents)
+      GetResourceManagersRecursive (control.Parent, resourceManagers, alwaysIncludeParents);
   }
 
   //  No construction for static only class
