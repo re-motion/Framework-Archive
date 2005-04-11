9c21c057076b915838adde57beaad29cb4fb4285
Stefan Wenig <stefan.wenig@rubicon.eu>
2005-04-11 13:09:00 +0000

    Label: "1.2.16.0" User: Build        Date:  8.04.05   Time: 21:28 Labeled

--- Core.Test/Utilities/ArrayUtilityTest.cs
+++ Core.Test/Utilities/ArrayUtilityTest.cs
@@ -20,6 +20,35 @@ public class ArrayUtilityTest
     //Assertion.AssertEquals (6, res.Length);
     Assertion.AssertEquals ("abcdef", string.Concat (res));
   }
+
+  [Test]
+  public void TestInsertFirst()
+  {
+    string[] s1 = { "a", "b", "c", "d" };
+    string[] res = (string[]) ArrayUtility.Insert (s1, 0, "X");
+    Assertion.AssertEquals ("Xabcd", string.Concat (res));
+  }
+  [Test]
+  public void TestInsertMiddle()
+  {
+    string[] s1 = { "a", "b", "c", "d" };
+    string[] res = (string[]) ArrayUtility.Insert (s1, 2, "X");
+    Assertion.AssertEquals ("abXcd", string.Concat (res));
+  }
+  [Test]
+  public void TestInsertEnd()
+  {
+    string[] s1 = { "a", "b", "c", "d" };
+    string[] res = (string[]) ArrayUtility.Insert (s1, 4, "X");
+    Assertion.AssertEquals ("abcdX", string.Concat (res));
+  }
+  [ExpectedException (typeof (IndexOutOfRangeException))]
+  [Test]
+  public void TestInsertPastEnd()
+  {
+    string[] s1 = { "a", "b", "c", "d" };
+    string[] res = (string[]) ArrayUtility.Insert (s1, 5, "X");
+  }
 }
 
 }
