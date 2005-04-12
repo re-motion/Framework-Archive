e3a9b0e1d13ed40f79acf579852d29a7a44f4eff
Michael Ketting <michael.ketting@rubicon.eu>
2005-04-12 12:58:00 +0000

    Label: "1.2.18.0" User: Build        Date: 12.04.05   Time:  9:35 Labeled

--- ObjectBinding/Reflection.Test/PersonCustomCell.cs
+++ ObjectBinding/Reflection.Test/PersonCustomCell.cs
@@ -28,18 +28,18 @@ public class PersonCustomCell: IBocCustomColumnDefinitionCell
       int listIndex,
       string onClick)
   {
-    writer.AddAttribute (
-        HtmlTextWriterAttribute.Href, 
-        list.GetCustomCellPostBackClientHyperlink (columnIndex, listIndex, "1"));
-    writer.AddAttribute (HtmlTextWriterAttribute.Onclick, onClick);
+    writer.AddAttribute (HtmlTextWriterAttribute.Href, "#");
+    string onClickEvent = list.GetCustomCellPostBackClientEvent (columnIndex, listIndex, "1");
+    onClickEvent += onClick;
+    writer.AddAttribute (HtmlTextWriterAttribute.Onclick, onClickEvent);
     writer.RenderBeginTag (HtmlTextWriterTag.A);
     writer.Write ("1 <br>");
     writer.RenderEndTag();
 
-    writer.AddAttribute (
-        HtmlTextWriterAttribute.Href, 
-        list.GetCustomCellPostBackClientHyperlink (columnIndex, listIndex, "2"));
-    writer.AddAttribute (HtmlTextWriterAttribute.Onclick, onClick);
+    writer.AddAttribute (HtmlTextWriterAttribute.Href, "#");
+    onClickEvent = list.GetCustomCellPostBackClientEvent (columnIndex, listIndex, "2");
+    onClickEvent += onClick;
+    writer.AddAttribute (HtmlTextWriterAttribute.Onclick, onClickEvent);
     writer.RenderBeginTag (HtmlTextWriterTag.A);
     writer.Write ("2 <br>");
     writer.RenderEndTag();
