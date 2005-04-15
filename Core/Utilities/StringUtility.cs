63390503116cfacecb17377337c481fe8da40caf
Stefan Wenig <stefan.wenig@rubicon.eu>
2005-04-15 16:16:00 +0000

    Label: "1.2.19.0" User: Build        Date: 13.04.05   Time:  9:52 Labeled

--- Core/Utilities/StringUtility.cs
+++ Core/Utilities/StringUtility.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Text;
 using System.Globalization;
+using System.Runtime.Serialization;
+using System.Reflection;
 using System.Collections;
+using Rubicon.Collections;
 
 namespace Rubicon.Utilities
 {
@@ -11,6 +14,154 @@ namespace Rubicon.Utilities
 /// </summary>
 public sealed class StringUtility
 {
+  /// <summary>
+  ///   The result of <see cref="ParseSeparatedList"/>.
+  /// </summary>
+  public class ParsedItem
+  {
+    public ParsedItem (string value, bool isQuoted)
+    {
+      Value = value;
+      IsQuoted = isQuoted;
+    }
+
+    /// <summary> The string value at this position. </summary>
+    public string Value;
+
+    /// <summary> An indicator that is <c>true</c> if the string at this position was quoted in the input string. </summary>
+    public bool IsQuoted;
+  }
+
+  public static ParsedItem[] ParseSeparatedList (string value, char delimiter)
+  {
+    return ParseSeparatedList (value, delimiter, '\"', '\"', '\\', " ", true);
+  }
+
+  public static ParsedItem[] ParseSeparatedList (string value)
+  {
+    return ParseSeparatedList (value, ',', '\"', '\"', '\\', " ", true);
+  }
+
+  /// <summary>
+  ///   Parses a delimiter-separated string into individual elements.
+  /// </summary>
+  /// <remarks>
+  ///   This method handles quotes and escaping. A quoted string may contain commas that
+  ///   will not be treated as separators. Commas prefixed with a backslash are treated
+  ///   like normal commas, double backslashes are treated as single backslashes.
+  /// </remarks>
+  /// <param name="value"> The string to be parsed. </param>
+  /// <param name="delimiter"> The character used for list separation. Default is comma (,). </param>
+  /// <param name="openingQuote"> The character used as opening quote. Default is double quote (&quot;). </param>
+  /// <param name="closingQuote"> The character used as closing quote. Default is double quote (&quot;). </param>
+  /// <param name="escapingChar"> The character used to escape quotes and itself. Default is backslash (\). </param>
+  /// <param name="whitespaceCharacters"> A string containing all characters to be considered whitespace. 
+  ///   Default is space character only. </param>
+  /// <param name="interpretSpecialCharacters"> If true, the escaping character can be followed by the letters
+  ///   r, n or t (case sensitive) for line feeds, new lines or tab characters, respectively. Default is true. </param>
+  public static ParsedItem[] ParseSeparatedList (
+      string value, 
+      char delimiter, char openingQuote, char closingQuote, char escapingChar, string whitespaceCharacters, 
+      bool interpretSpecialCharacters) 
+  {
+    string specialCharacters = "rnt";
+    string specialCharacterResults = "\r\n\t";
+
+    StringBuilder current = new StringBuilder();
+    TypedArrayList items = new TypedArrayList (typeof (ParsedItem));
+    // ArrayList argsArray = new ArrayList();
+
+    bool isQuoted = false;
+    // ArrayList isQuotedArray = new ArrayList();
+
+    int len = value.Length;
+    int state = 0; // 0 ... between arguments, 1 ... within argument, 2 ... within quotes
+
+    for (int i = 0; i < len; ++i)
+    {
+      char c = value[i];
+      if (state == 0)
+      {
+        if (c == openingQuote)
+        {
+          state = 2;
+          isQuoted = true;
+        }
+        else if (c != delimiter && whitespaceCharacters.IndexOf (c) < 0)
+        {
+          state = 1;
+          current.Append (c);
+        }
+      }
+      else if (state == 1)
+      {
+        if (c == openingQuote)
+        {
+          // the string started without quotes, but enters a quoted area now. isQuoted is still false!
+          state = 2;
+          current.Append (c);
+        }
+        else if (c == delimiter)
+        {
+          state = 0;
+          if (current.Length > 0)
+          {
+            items.Add (new ParsedItem (current.ToString(), isQuoted));
+            current.Length = 0;
+            isQuoted = false;
+          }
+        }
+        else 
+        {
+          current.Append (c);
+        }
+      }
+      else if (state == 2)
+      {
+        if (c == closingQuote)
+        {
+          if (! isQuoted)
+            current.Append (c);
+          state = 1;
+        }
+        else if (c == escapingChar)
+        {
+          if ((i + 1) < len)
+          {
+            char next = value[i+1];
+            if (next == escapingChar || next == openingQuote || next == closingQuote)
+            {
+              current.Append (next);
+              ++i;
+            }
+            else if (interpretSpecialCharacters && specialCharacters.IndexOf (next) >= 0)
+            {
+              current.Append (specialCharacterResults[specialCharacters.IndexOf (next)]);
+              ++i;
+            }
+            else
+            {
+              current.Append ('\\');
+            }
+          }
+          else
+          {
+            state = 1;
+          }
+        }
+        else 
+        {
+          current.Append (c);
+        }
+      }
+    }
+
+    if (current.Length > 0)
+    {
+      items.Add (new ParsedItem (current.ToString(), isQuoted));
+    }
+    return (ParsedItem[]) items.ToArray();
+  }
   // static members
 
   /// <summary>
@@ -50,23 +201,6 @@ public sealed class StringUtility
     return false;
   }
 
-//  public static string ConcatWithSeperator (string[] strings, string seperator)
-//  {
-//    StringBuilder sb = new StringBuilder ();
-//    foreach (string s in strings)
-//    {
-//      if (sb.Length > 0)
-//        sb.Append (seperator);
-//      sb.Append (s);
-//    }
-//    return sb.ToString();
-//  }
-//
-//  public static string ConcatWithSeperator (object[] objects, string seperator)
-//  {
-//    return ConcatWithSeperator (ObjectArrayToStringArray (objects), seperator);
-//  }
-
   public static string[] ListToStringArray (IList list)
   {
     if (list == null)
@@ -83,12 +217,12 @@ public sealed class StringUtility
 	{
 	}
 
-  public static string ConcatWithSeperator (IList list, string seperator)
+  public static string ConcatWithSeparator (IList list, string separator)
 	{
-		return ConcatWithSeperator (list, seperator, null, null);
+		return ConcatWithSeparator (list, separator, null, null);
 	}
 
-	public static string ConcatWithSeperator (IList list, string seperator, string format, IFormatProvider formatProvider)
+	public static string ConcatWithSeparator (IList list, string separator, string format, IFormatProvider formatProvider)
 	{
 		if (list == null)
 			throw new ArgumentNullException ("list");
@@ -100,7 +234,7 @@ public sealed class StringUtility
 		for (int i = 0; i < list.Count; ++i)
 		{
 			if (i > 0)
-				sb.Append (seperator);
+				sb.Append (separator);
 
 			object obj = list[i];
 			IFormattable formatable = obj as IFormattable;
@@ -112,7 +246,7 @@ public sealed class StringUtility
 		return sb.ToString();
 	}
 
-	public static string ConcatWithSeperator (string[] strings, string seperator)
+	public static string ConcatWithSeparator (string[] strings, string separator)
 	{
 		if (strings == null)
 			throw new ArgumentNullException ("strings");
@@ -123,7 +257,7 @@ public sealed class StringUtility
 		for (int i = 0; i < strings.Length; ++i)
 		{
 			if (i > 0)
-				sb.Append (seperator);
+				sb.Append (separator);
 			sb.Append (strings[i]);
 		}
 		return sb.ToString();
@@ -137,6 +271,97 @@ public sealed class StringUtility
 		return sb.ToString();
 	}
 
+  /// <summary>
+  ///   Parses the specified type from a string.
+  /// </summary>
+  /// <param name="type"> The type that should be created from the string. This type must have 
+  ///   a public static <c>Parse</c> method with either no arguments or a single 
+  ///   <c>IFormatProvider</c> argument. 
+  ///   If <c>type</c> is an array type, the values must be comma-separated. (Escaping is 
+  ///   handled as for <see cref="ParseSeparatedList"/>.) </param>
+  /// <param name="value"> The string value to be parsed. </param>
+  /// <param name="format"> The format provider to be passed to the type's <c>Parse</c> method
+  ///   (if present). </param>
+  /// <returns> An instance of the specified type. </returns>
+  /// <exception cref="ParseException"> The <c>Parse</c> method was not found, or failed. </exception>
+  public static object Parse (Type type, string value, IFormatProvider format)
+  {
+    if (type == typeof (string))
+      return value;
+
+    if (type.IsArray)
+    {
+      Type elementType = type.GetElementType();
+      ParsedItem[] items = ParseSeparatedList (value, ',');
+      Array results = Array.CreateInstance (elementType, items.Length);
+      for (int i = 0; i < items.Length; ++i)
+      {
+        try
+        {
+          results.SetValue (Parse (elementType, items[i].Value, format), i);
+        }
+        catch (ParseException e)
+        {
+          throw new ParseException (e.Message + " (Error accured at array index " + i.ToString() + ").", e);
+        }
+      }
+      return results;
+    }
+
+    try
+    {
+      MethodInfo parseMethod = null;
+      MethodInfo parseFormatMethod  = type.GetMethod (
+          "Parse", 
+          BindingFlags.Static | BindingFlags.Public | BindingFlags.FlattenHierarchy, 
+          null, 
+          new Type[] {typeof (string), typeof (IFormatProvider)}, 
+          null);
+
+      if (parseFormatMethod != null && type.IsAssignableFrom (parseFormatMethod.ReturnType))
+      {
+        return parseFormatMethod.Invoke (null, new object[] { value, format } );
+      }
+      else
+      {
+        parseMethod  = type.GetMethod (
+            "Parse", 
+            BindingFlags.Static | BindingFlags.Public | BindingFlags.FlattenHierarchy, 
+            null, 
+            new Type[] {typeof (string)}, 
+            null);
+
+        if (parseMethod == null || ! type.IsAssignableFrom (parseMethod.ReturnType))
+          throw new ParseException ("Type does not have method 'public static " + type.Name + " Parse (string s)'.");
+
+        return parseMethod.Invoke (null, new object[] { value } );
+      }
+    }
+    catch (TargetInvocationException e)
+    {
+      throw new ParseException ("Method '" + type.Name + ".Parse' failed.", e);
+    }
+  }
+
+}
+
+[Serializable]
+public class ParseException: Exception
+{
+  public ParseException (string message)
+    : base (message, null)
+  {
+  }
+
+  public ParseException (string message, Exception innerException)
+    : base (message, innerException)
+  {
+  }
+
+  public ParseException (SerializationInfo info, StreamingContext context)
+    : base (info, context)
+  {
+  }
 }
 
 }
