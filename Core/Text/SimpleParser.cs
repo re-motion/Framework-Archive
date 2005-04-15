7964cec891c9cca6fd4731980cf1641503c76aef
Stefan Wenig <stefan.wenig@rubicon.eu>
2005-04-15 10:31:00 +0000

    Label: "1.2.19.0" User: Build        Date: 13.04.05   Time:  9:52 Labeled

---
+++ Core/Text/SimpleParser.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Rubicon.Text
+{
+	/// <summary>
+	/// Summary description for SimpleParser.
+	/// </summary>
+	public class SimpleParser
+	{
+		public SimpleParser()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+	}
+}
