f7a20aabe796b88f00953abbc1e21a2ed499e3c1
Stefan Wenig <stefan.wenig@rubicon.eu>
2005-04-21 12:13:00 +0000

    Label: "1.2.23.0" User: Build        Date: 20.04.05   Time: 23:38 Labeled

---
+++ Web/Legacy/UI/Controls/PostBackHandler.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Rubicon.Web.UI.Controls
+{
+	/// <summary>
+	/// Summary description for PostBackHandler.
+	/// </summary>
+	public class PostBackHandler
+	{
+		public PostBackHandler()
+		{
+			//
+			// TODO: Add constructor logic here
+			//
+		}
+	}
+}
