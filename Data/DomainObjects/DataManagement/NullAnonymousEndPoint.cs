7d36a6b0a6f145982c9a1b1bbfdc1d9d65ace7cf
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-04-21 14:50:00 +0000

    Label: "1.2.23.0" User: Build        Date: 20.04.05   Time: 23:38 Labeled

---
+++ Data/DomainObjects/DataManagement/NullAnonymousEndPoint.cs
@@ -0,0 +1,50 @@
+using System;
+
+using Rubicon.Data.DomainObjects.Mapping;
+using Rubicon.Utilities;
+
+namespace Rubicon.Data.DomainObjects.DataManagement
+{
+public class NullAnonymousEndPoint : AnonymousEndPoint
+{
+  // types
+
+  // static members and constants
+
+  // member fields
+
+  // construction and disposing
+
+  public NullAnonymousEndPoint (RelationDefinition relationDefinition) : base (relationDefinition)
+  {
+  }
+
+  // methods and properties
+
+  public override ClientTransaction ClientTransaction
+  {
+    get { return null; }
+  }
+
+  public override DomainObject GetDomainObject ()
+  {
+    return null; 
+  }
+
+  public override DataContainer GetDataContainer ()
+  {
+    return null;
+  }
+
+  public override ObjectID ObjectID
+  {
+    get { return null; }
+  }
+
+
+  public override bool IsNull
+  {
+    get { return true; }
+  }
+}
+}
