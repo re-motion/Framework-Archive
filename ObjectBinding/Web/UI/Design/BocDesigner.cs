06b274989d915f1f3ab3154bc532086e17558f68
Michael Ketting <michael.ketting@rubicon.eu>
2005-05-04 08:34:00 +0000

    Label: "1.2.33.0" User: Build        Date:  3.05.05   Time: 17:22 Labeled

--- ObjectBinding/Web/UI/Design/BocDesigner.cs
+++ ObjectBinding/Web/UI/Design/BocDesigner.cs
@@ -1,6 +1,9 @@
 using System;
+using System.ComponentModel;
+using System.Web;
 using System.Web.UI.Design;
 using Rubicon.ObjectBinding.Web.Controls;
+using Rubicon.Utilities;
 
 namespace Rubicon.ObjectBinding.Web.Design
 {
@@ -15,9 +18,30 @@ public class BocDesigner: ControlDesigner
     get { return true; }
   }
 
+#if !net20
+  protected static readonly string ErrorDesignTimeHtmlTemplate = "<table cellpadding=\"4\" cellspacing=\"0\" style=\"font: messagebox; color: buttontext; background-color: buttonface; border: solid 1px; border-top-color: buttonhighlight; border-left-color: buttonhighlight; border-bottom-color: buttonshadow; border-right-color: buttonshadow\">\r\n                <tr><td nowrap><span style=\"font-weight: bold; color: red\">{0}</span> - {1}</td></tr>\r\n                <tr><td>{2}</td></tr>\r\n              </table>";
+  
+  protected static string CreateErrorDesignTimeHtml (string errorMessage, Exception e, IComponent component)
+  {
+    errorMessage = HttpUtility.HtmlEncode (StringUtility.NullToEmpty (errorMessage));
+    if (e != null)
+      errorMessage = errorMessage + "<br />" + HttpUtility.HtmlEncode(e.Message);
+    return string.Format (
+        BocDesigner.ErrorDesignTimeHtmlTemplate, 
+        "Error rendering Control", 
+        HttpUtility.HtmlEncode (component.Site.Name), 
+        errorMessage);
+  }
+ 
+  protected override string GetErrorDesignTimeHtml (Exception e)
+  {
+    return CreateErrorDesignTimeHtml ("An unhandled exception has occurred.", e, Component);
+  }
+#endif
+
   public override string GetDesignTimeHtml()
   {
-    BusinessObjectBoundWebControl control = (BusinessObjectBoundWebControl)Component;
+    BusinessObjectBoundWebControl control = (BusinessObjectBoundWebControl) Component;
     try
     {
       control.PreRenderForDesignMode();
@@ -26,6 +50,7 @@ public class BocDesigner: ControlDesigner
     {
       System.Diagnostics.Debug.WriteLine (e.Message);
     }
+
     return base.GetDesignTimeHtml ();
   }
 
