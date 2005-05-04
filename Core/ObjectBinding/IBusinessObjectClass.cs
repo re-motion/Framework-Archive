55fb05a80e7cdf49204a3097e5e90417e1111d4a
Michael Ketting <michael.ketting@rubicon.eu>
2005-05-04 10:27:00 +0000

    Label: "1.2.33.0" User: Build        Date:  3.05.05   Time: 17:22 Labeled

--- Core/ObjectBinding/IBusinessObjectClass.cs
+++ Core/ObjectBinding/IBusinessObjectClass.cs
@@ -3,35 +3,66 @@ using System;
 namespace Rubicon.ObjectBinding
 {
 
+/// <summary> The <b>Class</b> of an <see cref="IBusinessObject"/>. </summary>
+/// <remarks>
+///   <para>
+///     The <b>IBusinessObjectClass</b> interface provides the list of <see cref="IBusinessObjectProperty"/> instances
+///     available by an <see cref="IBusinessObject"/> of this <b>IBusinessObjectClass</b>'s type. 
+///   </para><para>
+///     It also provides services for accessing class specific meta data.
+///   </para>
+/// </remarks>
 public interface IBusinessObjectClass
 {
+  /// <summary> Returns the <see cref="IBusinessObjectProperty"/> for the passes <paramref name="propertyIdentifier"/>. </summary>
+  /// <param name="propertyIdentifier"> 
+  ///   A <see cref="String"/> uniquely identifying an <see cref="IBusinessObjectProperty"/> in this
+  ///   <see cref="IBusinessObjectClass"/>.
+  /// </param>
+  /// <returns> Returns the <see cref="IBusinessObjectProperty"/> or <see langword="null"/>. </returns>
+  /// <remarks> 
+  ///   It is not specified wheter an exception is thrown or <see langword="null"/> is returned if the 
+  ///   <see cref="IBusinessObjectProperty"/> could not be found.
+  /// </remarks>
   IBusinessObjectProperty GetPropertyDefinition (string propertyIdentifier);
+
+  /// <summary> 
+  ///   Returns the <see cref="IBusinessObjectProperty"/> instances defined for this <see cref="IBusinessObjectClass"/>.
+  /// </summary>
+  /// <returns> An array of <see cref="IBusinessObjectProperty"/> instances. Must not be <see langword="null"/>. </returns>
   IBusinessObjectProperty[] GetPropertyDefinitions ();
 
-  /// <remarks>
+  /// <summary> Gets the <see cref="IBusinessObjectProvider"/> for this <see cref="IBusinessObjectClass"/>. </summary>
+  /// <value> An instance of the <see cref="IBusinessObjectProvider"/> type.
   ///   <note type="inheritinfo">
   ///     Must not return <see langword="null"/>.
   ///   </note>
-  /// </remarks>
+  /// </value>
   IBusinessObjectProvider BusinessObjectProvider { get; }
 
   /// <summary>
-  ///   Specifies wheter a referenced object of this business object class needs to be written back to its container if some of its values have changed.
+  ///   Gets a flag that specifies whether a referenced object of this <see cref="IBusinessObjectClass"/> needs to be 
+  ///   written back to its container if some of its values have changed.
   /// </summary>
+  /// <value> <see langword="true"/> if the <see cref="IBusinessObject"/> must be reassigned to its container. </value>
   /// <example>
   ///   The following pseudo code shows how this value affects the binding behaviour.
   ///   <code><![CDATA[
   ///   Address address = person.Address;
   ///   address.City = "Vienna";
-  ///   // the RequiresWriteBack property of the 'Address' business object class specifies whether the following statement is required:
+  ///   // the RequiresWriteBack property of the 'Address' business object class specifies 
+  ///   // whether the following statement is required:
   ///   person.Address = address;
   ///   ]]></code>
   /// </example>
   bool RequiresWriteBack { get; }
 
+  /// <summary> Gets the identifier (i.e. the type name) for this <see cref="IBusinessObjectClass"/>. </summary>
+  /// <value> A string that uniquely identifies the <see cref="IBusinessObjectClass"/>. </value>
   string Identifier { get; }
 }
 
+/// <summary> The <b>Class</b> of an <see cref="IBusinessObjectWithIdentity"/>. </summary>
 public interface IBusinessObjectClassWithIdentity: IBusinessObjectClass
 {
   IBusinessObjectWithIdentity GetObject (string uniqueIdentifier);
