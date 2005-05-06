9389c067b00913ed5959675f9113b471c8095116
Michael Ketting <michael.ketting@rubicon.eu>
2005-05-06 14:11:00 +0000

    Label: "1.2.33.0" User: Build        Date:  3.05.05   Time: 17:22 Labeled

--- Core/ObjectBinding/IBusinessObjectProvider.cs
+++ Core/ObjectBinding/IBusinessObjectProvider.cs
@@ -4,38 +4,87 @@ using System.Collections;
 namespace Rubicon.ObjectBinding
 {
 
-/// <summary>
-///   Provides functionality for business object providers.
-/// </summary>
+/// <summary> Provides functionality for business object providers. </summary>
 /// <remarks>
-///   Note that if this interface is implemented using singletons, the singleton must be thread save.
+///   A business object provider is able to retrieve services (e.g. the
+///   <see cref="T:Rubicon.ObjectBinding.Web.IBusinessObjectWebUIService"/>) from the object model, as well as provide
+///   functionality required by more than one of the business object components (<b>Class</b>, <b>Property</b>, and
+///   <b>Object</b>).
+///   <note type="inheritinfo">
+///     If this interface is implemented using singletons, the singleton must be thread save.
+///   </note>
+///   <note type="inheritinfo">
+///     You can use the abstract default implemtation (<see cref="BusinessObjectProvider"/>) as a base for implementing
+///     the business object provider for your object model.
+///   </note>
 /// </remarks>
 public interface IBusinessObjectProvider
 {
+  /// <summary> Retrieves the requested <see cref="IBusinessObjectService"/>. </summary>
+  /// <param name="serviceType"> The type of <see cref="IBusinessObjectService"/> to get from the object model. </param>
+  /// <returns> 
+  ///   An instance if the <see cref="IBusinessObjectService"/> type or <see langword="null"/> if the sevice could not
+  ///   be found or instantiated.
+  ///  </returns>
+  ///  <remarks>
+  ///    <note type="inheritinfo">
+  ///     If your object model does not support services, this method may always return null.
+  ///    </note>
+  ///  </remarks>
   IBusinessObjectService GetService (Type serviceType);
-  char GetPropertyPathSeparator ();
+
+  /// <summary> 
+  ///   Returns the <see cref="Char"/> to be used as a serparator when formatting the property path's identifier.
+  /// </summary>
+  /// <returns> A <see cref="Char"/> that is not used by the property's identifier. </returns>
+  char GetPropertyPathSeparator();
+
+  /// <summary> 
+  ///   Creates a <see cref="BusinessObjectPropertyPath"/> from the passed <see cref="IBusinessObjectProperty"/> list.
+  /// </summary>
+  /// <param name="properties"> An array of <see cref="IBusinessObjectProperty"/> instances. </param>
+  /// <returns> A new instance of the <see cref="BusinessObjectPropertyPath"/> type. </returns>
   BusinessObjectPropertyPath CreatePropertyPath (IBusinessObjectProperty[] properties);
 }
 
+/// <summary> The abstract default implementation of the <see cref="IBusinessObjectProvider"/> interface. </summary>
 public abstract class BusinessObjectProvider: IBusinessObjectProvider
 {
+  /// <summary> The <see cref="IDictionary"/> used to store the references to the registered servies. </summary>
+  /// <remarks>
+  ///   <note type="inheritinfo">
+  ///    If your object model does not support services, this property may always return <see langword="null"/>.
+  ///    Do not forget to override the <see cref="GetService"/> method, having it return <see langword="null"/> as well, 
+  ///    instead of querying the <b>ServiceDictionary</b>.
+  ///   </note>
+  /// </remarks>
   protected abstract IDictionary ServiceDictionary { get; }
 
+  /// <summary> Retrieves the requested <see cref="IBusinessObjectService"/>. </summary>
   public IBusinessObjectService GetService (Type serviceType)
   {
     return (IBusinessObjectService) ServiceDictionary[serviceType];
   }
 
+  /// <summary> Registers a new <see cref="IBusinessObjectService"/> with this <see cref="BusinessObjectProvider"/>. </summary>
+  /// <param name="serviceType"> The type of the service to be registered. </param>
+  /// <param name="service"> The <see cref="IBusinessObjectService"/> to register. </param>
   public void AddService (Type serviceType, IBusinessObjectService service)
   {
     ServiceDictionary[serviceType] = service;
   }
 
-  public virtual char GetPropertyPathSeparator ()
+  /// <summary> 
+  ///   Returns the <see cref="Char"/> to be used as a serparator when formatting the property path's identifier.
+  /// </summary>
+  public virtual char GetPropertyPathSeparator()
   {
     return '.';
   }
 
+  /// <summary> 
+  ///   Creates a <see cref="BusinessObjectPropertyPath"/> from the passed <see cref="IBusinessObjectProperty"/> list.
+  /// </summary>
   public virtual BusinessObjectPropertyPath CreatePropertyPath (IBusinessObjectProperty[] properties)
   {
     return new BusinessObjectPropertyPath (properties);
