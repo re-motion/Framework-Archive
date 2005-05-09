ac6e9593e08a1b7d70301e0a7976bbc4530dbd9d
Michael Ketting <michael.ketting@rubicon.eu>
2005-05-09 12:39:00 +0000

    Label: "1.2.34.0" User: Build        Date:  8.05.05   Time: 21:53 Labeled

--- Core/ObjectBinding/IBusinessObjectService.cs
+++ Core/ObjectBinding/IBusinessObjectService.cs
@@ -3,9 +3,10 @@ using System;
 namespace Rubicon.ObjectBinding
 {
 
-/// <summary>
-///   Base type for interfaces that define specific functionality for business object providers.
-/// </summary>
+/// <summary> Base type for interfaces that define specific functionality for business object providers. </summary>
+/// <remarks> 
+///   See <see cref="Rubicon.ObjectBinding.Web.IBusinessObjectWebUIService"/> for a specialization of this interface.
+/// </remarks>
 public interface IBusinessObjectService
 {
 }
