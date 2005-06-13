97b304c749c2addd4722b5e027e7f34a1948599d
Michael Ketting <michael.ketting@rubicon.eu>
2005-06-13 10:08:00 +0000

    Label: "1.2.49.0" User: Build        Date: 31.05.05   Time: 11:35 Labeled

--- Web/Core/UI/Controls/TabViewCollection.cs
+++ Web/Core/UI/Controls/TabViewCollection.cs
@@ -37,7 +37,26 @@ public class TabViewCollection: System.Web.UI.WebControls.ViewCollection
     TabView view = (TabView) control;
     view.OnInsert (Owner);
     base.AddAt (index, view);
-    Owner.OnTabViewInserted ((TabView) view);
+    Owner.OnTabViewInserted (view);
+  }
+
+  public override void Remove (Control control)
+  {
+    ArgumentUtility.CheckNotNullAndType ("control", control, typeof (TabView));
+    TabView view = (TabView) control;
+    Owner.OnTabViewRemove (view);
+    base.Remove (control);
+    Owner.OnTabViewRemoved (view);
+  }
+
+  public override void RemoveAt (int index)
+  {
+    if (index < 0 || index > this.Count)
+      throw new ArgumentOutOfRangeException ("index");
+    TabView view = (TabView) this[index];
+    Owner.OnTabViewRemove (view);
+    base.RemoveAt (index);
+    Owner.OnTabViewRemoved (view);
   }
 
 #if net20
