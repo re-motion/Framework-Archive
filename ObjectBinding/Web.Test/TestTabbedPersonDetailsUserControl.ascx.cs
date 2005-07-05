712912d444fdf780596d5d55ee6f89fbc35495f3
Michael Ketting <michael.ketting@rubicon.eu>
2005-07-05 15:19:00 +0000

    Label: "1.2.54.0" User: Build        Date: 28.06.05   Time:  8:21 Labeled

--- ObjectBinding/Web.Test/TestTabbedPersonDetailsUserControl.ascx.cs
+++ ObjectBinding/Web.Test/TestTabbedPersonDetailsUserControl.ascx.cs
@@ -43,8 +43,6 @@ public class TestTabbedPersonDetailsUserControl :
     return (FormGridRowInfoCollection) _listOfFormGridRowInfos[table];
   }
 
-	#region Web Form Designer generated code
-
 	override protected void OnInit(EventArgs e)
 	{
     StringCollection hiddenRows = (StringCollection)_listOfHiddenRows[FormGrid];
@@ -55,8 +53,61 @@ public class TestTabbedPersonDetailsUserControl :
 		//
 		InitializeComponent();
 		base.OnInit(e);
+    InitalizePartnerFieldMenuItems();
   }
 	
+  private void InitalizePartnerFieldMenuItems()
+  {
+    BocMenuItem menuItem = null;
+
+    menuItem = new BocMenuItem();
+    menuItem.ItemID = "Open";
+    menuItem.Text = "Open";
+    menuItem.Category = "Object";
+    menuItem.RequiredSelection = RequiredSelection.OneOrMore;
+    menuItem.Command.Type = CommandType.WxeFunction;
+    menuItem.Command.WxeFunctionCommand.Parameters = "objects";
+    menuItem.Command.WxeFunctionCommand.TypeName = "OBWTest.ViewPersonsWxeFunction,OBWTest";
+    PartnerField.OptionsMenuItems.Add (menuItem);
+
+    menuItem = new BocMenuItem();
+    menuItem.ItemID = "Copy";
+    menuItem.Text = "Copy";
+    menuItem.Category = "Edit";
+    menuItem.Icon = "Images/CopyItem.gif";
+    menuItem.RequiredSelection = RequiredSelection.OneOrMore;
+    menuItem.Command.Type = CommandType.Event;
+    PartnerField.OptionsMenuItems.Add (menuItem);
+
+    menuItem = new BocMenuItem();
+    menuItem.ItemID = "Cut";
+    menuItem.Text = "Cut";
+    menuItem.Category = "Edit";
+    menuItem.RequiredSelection = RequiredSelection.OneOrMore;
+    menuItem.Command.Type = CommandType.Event;
+    PartnerField.OptionsMenuItems.Add (menuItem);
+
+    menuItem = new BocMenuItem();
+    menuItem.ItemID = "Paste";
+    menuItem.Text = "Paste";
+    menuItem.Category = "Edit";
+    menuItem.Command.Type = CommandType.Event;
+    PartnerField.OptionsMenuItems.Add (menuItem);
+
+    menuItem = new BocMenuItem();
+    menuItem.ItemID = "Delete";
+    menuItem.Text = "Delete";
+    menuItem.Category = "Edit";
+    menuItem.Icon = "Images/DeleteItem.gif";
+    menuItem.DisabledIcon = "Images/DeleteItemDisabled.gif";
+    menuItem.RequiredSelection = RequiredSelection.OneOrMore;
+    menuItem.Style = WebMenuItemStyle.Icon;
+    menuItem.Command.Type = CommandType.Event;
+    PartnerField.OptionsMenuItems.Add (menuItem);
+  }
+
+	#region Web Form Designer generated code
+
 	/// <summary>
 	///		Required method for Designer support - do not modify
 	///		the contents of this method with the code editor.
