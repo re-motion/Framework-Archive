ef144f3758d2acb3bba24e9a306ed598d5c5025b
Michael Ketting <michael.ketting@rubicon.eu>
2005-07-14 09:54:00 +0000

    Label: "1.2.54.0" User: Build        Date: 28.06.05   Time:  8:21 Labeled

--- Web/Core/UI/Controls/DatePickerPage.cs
+++ Web/Core/UI/Controls/DatePickerPage.cs
@@ -120,7 +120,7 @@ public class DatePickerPage : Page
     {
       string scriptUrl = ResourceUrlResolver.GetResourceUrl (
           this, Context, typeof (DatePickerPage), ResourceType.Html, c_datePickerScriptUrl);
-      HtmlHeadAppender.Current.RegisterJavaScriptInclude (key, scriptUrl);
+      HtmlHeadAppender.Current.RegisterJavaScriptInclude (key, scriptUrl, HtmlHeadAppender.Prioritiy.Library);
     }
 
     base.OnPreRender (e);
