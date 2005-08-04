33d7247e7bebc3c62ff17a3b4ddd03d7ca20d921
Michael Ketting <michael.ketting@rubicon.eu>
2005-08-04 12:34:00 +0000

    Label: "1.2.59.0" User: Build        Date:  2.08.05   Time: 16:56 Labeled

--- Web/Core/UI/WcagException.cs
+++ Web/Core/UI/WcagException.cs
@@ -4,29 +4,29 @@ using System.Web.UI;
 namespace Rubicon.Web.UI
 {
 
-public class WaiException: Exception
+public class WcagException: Exception
 {
-  public WaiException()
-    : this ("An element on the page is not WAI conform.", null)
+  public WcagException()
+    : this ("An element on the page is not WCAG conform.", null)
   {
   }
 
-  public WaiException (int priority)
+  public WcagException (int priority)
     : base (string.Format ("An element on the page does comply with a priority {0} checkpoint.", priority))
   {
   }
 
-  public WaiException (int priority, Control control)
+  public WcagException (int priority, Control control)
     : base (string.Format ("Control {1} does comply with a priority {0} checkpoint.", priority, control.ID))
   {
   }
 
-  public WaiException (int priority, Control control, string property)
+  public WcagException (int priority, Control control, string property)
     : base (string.Format ("Property {2} of Control {1} does comply with a priority {0} checkpoint.", priority, control.ID, property))
   {
   }
 
-  public WaiException (string message, Exception innerException)
+  public WcagException (string message, Exception innerException)
     : base (message, innerException)
   {
   }
