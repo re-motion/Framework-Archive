e5675f2b8469f3f2b4a79fa749a4e284e9c6d2eb
Michael Ketting <michael.ketting@rubicon.eu>
2005-08-09 10:49:00 +0000

    Label: "1.2.61.0" User: Build        Date:  8.08.05   Time:  9:37 Labeled

--- ObjectBinding/Web.Test/SingleBocTestWxeBasePage.cs
+++ ObjectBinding/Web.Test/SingleBocTestWxeBasePage.cs
@@ -21,32 +21,6 @@ namespace OBWTest
 [MultiLingualResources ("OBWTest.Globalization.SingleBocTestBasePage")]
 public class SingleBocTestWxeBasePage: TestWxeBasePage
 {
-  /// <summary> Hashtable&lt;type,IResourceManagers&gt; </summary>
-  private static Hashtable s_chachedResourceManagers = new Hashtable();
-
-  protected override void OnInit(EventArgs e)
-  {
-    base.OnInit (e);
-  }
-  public override IResourceManager GetResourceManager()
-  {
-    // cache the resource manager
-    Type type = this.GetType();
-    if (s_chachedResourceManagers[type] == null)
-    {
-      lock (typeof (SingleBocTestWxeBasePage))
-      {
-        if (s_chachedResourceManagers[type] == null)
-          s_chachedResourceManagers[type] = MultiLingualResourcesAttribute.GetResourceManager (type, true);
-      }  
-    }
-
-    //  TODO: Combine IResourceManager into a ResourceManagerSet
-    //    return ResourceManagerSet.Combine (
-    //      (IResourceManager) base.GetResourceManager(), 
-    // (IResourceManager) s_chachedResourceManagers[type]);
-    return base.GetResourceManager();
-  }
 }
 
 }
