7fbd288aad15386b798f1ec3bb53687b073af6f2
Michael Ketting <michael.ketting@rubicon.eu>
2005-08-19 14:44:00 +0000

    Label: "1.2.63.0" User: Build        Date: 17.08.05   Time:  9:56 Labeled

--- Web/Core/ExecutionEngine/WxeTemplateControl.cs
+++ Web/Core/ExecutionEngine/WxeTemplateControl.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Web;
 using System.Web.UI;
+using Rubicon.Globalization;
+using Rubicon.Utilities;
+using Rubicon.Web.UI.Globalization;
 using Rubicon.Collections;
 using Rubicon.Web.UI;
 using Rubicon.Web.UI.Controls;
@@ -20,9 +23,20 @@ public class WxeTemplateControlInfo
 {
   private WxePageStep _currentStep;
   private WxeFunction _currentFunction;
+  private IWxeTemplateControl _control;
+  /// <summary> Caches the <see cref="ResourceManagerSet"/> for this control. </summary>
+  private ResourceManagerSet _cachedResourceManager;
 
+  [Obsolete ("Use Initialize instead.")]
   public void OnInit (IWxeTemplateControl control, HttpContext context)
   {
+    Initialize (control, context);
+  }
+
+  public void Initialize (IWxeTemplateControl control, HttpContext context)
+  {
+    ArgumentUtility.CheckNotNullAndType ("control", control, typeof (Control));
+    _control = control;
     if (ControlHelper.IsDesignMode (control, context))
       return;
 
@@ -54,6 +68,34 @@ public class WxeTemplateControlInfo
   public NameObjectCollection Variables 
   {
     get { return (_currentStep == null) ? null : _currentStep.Variables; }
+  }
+
+  /// <summary> Find the <see cref="IResourceManager"/> for this control info. </summary>
+  /// <param name="localResourcesType"> 
+  ///   A type with the <see cref="MultiLingualResourcesAttribute"/> applied to it.
+  ///   Typically an <b>enum</b> or the derived class itself.
+  /// </param>
+  protected IResourceManager GetResourceManager (Type localResourcesType)
+  {
+    Rubicon.Utilities.ArgumentUtility.CheckNotNull ("localResourcesType", localResourcesType);
+
+    //  Provider has already been identified.
+    if (_cachedResourceManager != null)
+        return _cachedResourceManager;
+
+    //  Get the resource managers
+
+    IResourceManager localResourceManager = 
+        MultiLingualResourcesAttribute.GetResourceManager (localResourcesType, true);
+    IResourceManager namingContainerResourceManager = 
+        ResourceManagerUtility.GetResourceManager (_control.NamingContainer, true);
+
+    if (namingContainerResourceManager == null)
+      _cachedResourceManager = new ResourceManagerSet (localResourceManager);
+    else
+      _cachedResourceManager = new ResourceManagerSet (localResourceManager, namingContainerResourceManager);
+
+    return _cachedResourceManager;
   }
 }
 
