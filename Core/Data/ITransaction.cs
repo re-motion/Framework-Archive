9e609e089aaa33ed94d8c877cc22533146c501d0
Michael Ketting <michael.ketting@rubicon.eu>
2005-09-15 07:47:00 +0000

    Label: "1.3.1.0" User: Build        Date: 14.09.05   Time: 12:19 Labeled

---
+++ Core/Data/ITransaction.cs
@@ -0,0 +1,43 @@
+using System;
+
+namespace Rubicon.Data
+{
+
+/// <summary> The <b>ITransaction</b> interface provides functionality needed when working with a transaction. </summary>
+public interface ITransaction
+{
+  /// <summary> Commits the transaction. </summary>
+  void Commit();
+
+  /// <summary> Rolls the transaction back. </summary>
+  void Rollback();
+  
+  /// <summary> 
+  ///   Gets a flag that describes whether the transaction supports creating child transactions by invoking
+  ///   <see cref="CreateChild"/>.
+  /// </summary>
+  bool CanCreateChild { get; }
+  
+  /// <summary> Creates a new child transaction for the current transaction. </summary>
+  /// <returns> 
+  ///   A new instance of the of a type implementing <see cref="ITransaction"/> that has the creating transaction
+  ///   as a parent.
+  /// </returns>
+  /// <exception cref="NotSupportedException"> 
+  ///   Thrown if the method is invoked while <see cref="CanCreateChild"/> is <see langword="false"/>.
+  /// </exception>
+  ITransaction CreateChild();
+
+  /// <summary> Gets the transaction's parent transaction. </summary>
+  /// <value> 
+  ///   An instance of the of a type implementing <see cref="ITransaction"/> or <see langword="null"/> if the
+  ///   transaction is a root transaction.
+  /// </value>
+  ITransaction GetParent { get; }
+  
+  /// <summary> Allows the transaction to implement clean up logic. </summary>
+  /// <remarks> This method is called when the transaction is no longer needed. </remarks>
+  void Release();
+}
+
+}
