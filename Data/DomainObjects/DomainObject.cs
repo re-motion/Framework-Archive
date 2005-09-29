a6d737ca4e8eee59ae244f32b4e3f72bd2e7a94d
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-09-29 12:46:00 +0000

    Label: "1.3.8.0" User: Build        Date: 28.09.05   Time: 18:16 Labeled

--- Data/DomainObjects/DomainObject.cs
+++ Data/DomainObjects/DomainObject.cs
@@ -231,11 +231,11 @@ public class DomainObject
     }
   }
 
-  // TODO Doc: Write a remark that instead of overriding Delete the programmer should override OnDeleting or OnDeleted instead.
   /// <summary>
   /// Deletes the <b>DomainObject</b>.
   /// </summary>
   /// <exception cref="DataManagement.ObjectDiscardedException">The object is already discarded. See <see cref="DataManagement.ObjectDiscardedException"/> for further information.</exception>
+  /// <remarks>To perform custom actions when a <see cref="DomainObject"/> is deleted <see cref="OnDeleting"/> and <see cref="OnDeleted"/> should be overridden.</remarks>
   public void Delete ()
   {
     CheckDiscarded ();
