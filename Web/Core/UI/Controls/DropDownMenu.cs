b0011ad9c067ec5d7782cb08dfd779069ae22036
Michael Ketting <michael.ketting@rubicon.eu>
2005-09-30 14:03:00 +0000

    Label: "1.3.10.0" User: Build        Date: 30.09.05   Time: 10:59 Labeled

--- Web/Core/UI/Controls/DropDownMenu.cs
+++ Web/Core/UI/Controls/DropDownMenu.cs
@@ -264,7 +264,7 @@ public class DropDownMenu: WebControl, IControl, IPostBackEventHandler
   protected override void RenderContents (HtmlTextWriter writer)
   {
     if (WcagUtility.IsWcagDebuggingEnabled() && WcagUtility.IsWaiConformanceLevelARequired())
-      throw new WcagException (1, this);
+      WcagUtility.HandleError (1, this);
 
     //  Menu-Div filling the control's div is required to apply internal css attributes
     //  for position, width and height. This allows the Head and th popup-div to align themselves
