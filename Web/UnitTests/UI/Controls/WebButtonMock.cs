651cdb59143524611a87f4f21446fd2febef1db2
Michael Ketting <michael.ketting@rubicon.eu>
2005-10-03 07:34:00 +0000

    Label: "1.3.10.0" User: Build        Date: 30.09.05   Time: 10:59 Labeled

--- Web/UnitTests/UI/Controls/WebButtonMock.cs
+++ Web/UnitTests/UI/Controls/WebButtonMock.cs
@@ -1,4 +1,5 @@
 using System;
+using Rubicon.Web.UI;
 using Rubicon.Web.UI.Controls;
 
 namespace Rubicon.Web.UnitTests.UI.Controls
@@ -7,6 +8,8 @@ namespace Rubicon.Web.UnitTests.UI.Controls
 /// <summary> Exposes non-public members of the <see cref="WebButton"/> type. </summary>
 public class WebButtonMock: WebButton
 {
+  private WcagHelperMock _wcagHelper = new WcagHelperMock();
+
 	public new void EvaluateWaiConformity ()
   {
     base.EvaluateWaiConformity ();
@@ -15,6 +18,16 @@ public class WebButtonMock: WebButton
   public new bool IsLegacyButtonEnabled
   {
     get { return base.IsLegacyButtonEnabled; }
+  }
+
+  protected override WcagHelper WcagHelper
+  {
+    get { return _wcagHelper; }
+  }
+
+  public WcagHelperMock WcagHelperMock
+  {
+    get { return _wcagHelper; }
   }
 }
 
