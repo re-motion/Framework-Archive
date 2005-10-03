efc13a7619976e5083ce05dac7d2a19820810275
Michael Ketting <michael.ketting@rubicon.eu>
2005-10-03 07:34:00 +0000

    Label: "1.3.10.0" User: Build        Date: 30.09.05   Time: 10:59 Labeled

--- ObjectBinding/Web.UnitTests/UI/Controls/BocReferenceValueMock.cs
+++ ObjectBinding/Web.UnitTests/UI/Controls/BocReferenceValueMock.cs
@@ -1,5 +1,7 @@
 using System;
+using Rubicon.Web.UI;
 using Rubicon.ObjectBinding.Web.Controls;
+using Rubicon.Web.UnitTests.UI;
 
 namespace Rubicon.ObjectBinding.Web.UnitTests.UI.Controls
 {
@@ -7,9 +9,21 @@ namespace Rubicon.ObjectBinding.Web.UnitTests.UI.Controls
 /// <summary> Exposes non-public members of the <see cref="BocReferenceValue"/> type. </summary>
 public class BocReferenceValueMock: BocReferenceValue
 {
+  private WcagHelperMock _wcagHelper = new WcagHelperMock();
+
 	public new void EvaluateWaiConformity ()
   {
     base.EvaluateWaiConformity ();
+  }
+
+  protected override WcagHelper WcagHelper
+  {
+    get { return _wcagHelper; }
+  }
+
+  public WcagHelperMock WcagHelperMock
+  {
+    get { return _wcagHelper; }
   }
 
   public new string InternalValue
