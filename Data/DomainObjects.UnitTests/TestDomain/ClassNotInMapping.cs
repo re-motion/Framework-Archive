01018f36d496bcd2dfa405e08fabc83686f76fa3
Ernst Scheithauer <ernst.scheithauer@rubicon.eu>
2005-10-05 11:14:00 +0000

    Label: "1.3.10.0" User: Build        Date: 30.09.05   Time: 10:59 Labeled

---
+++ Data/DomainObjects.UnitTests/TestDomain/ClassNotInMapping.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Rubicon.Data.DomainObjects.UnitTests.TestDomain
+{
+public class ClassNotInMapping : TestDomainBase
+{
+  // types
+
+  // static members and constants
+
+  // member fields
+
+  // construction and disposing
+
+  public ClassNotInMapping ()
+  {
+  }
+
+  public ClassNotInMapping (ClientTransaction clientTransaction) : base (clientTransaction)
+  {
+  }
+
+  protected ClassNotInMapping (DataContainer dataContainer) : base (dataContainer)
+  {
+  }
+
+  // methods and properties
+
+}
+}
