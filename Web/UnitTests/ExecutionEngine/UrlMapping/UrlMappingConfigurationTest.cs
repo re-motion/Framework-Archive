211665f195de19592f4ae2ab39984d16f9ebf5ac
Michael Ketting <michael.ketting@rubicon.eu>
2005-10-12 08:00:00 +0000

    Label: "1.3.15.0" User: Build        Date: 10.10.05   Time: 16:56 Labeled

---
+++ Web/UnitTests/ExecutionEngine/UrlMapping/UrlMappingConfigurationTest.cs
@@ -0,0 +1,30 @@
+using System;
+using System.IO;
+using System.Web;
+using System.Web.SessionState;
+using System.Collections.Specialized;
+using System.Reflection;
+using System.Threading;
+using NUnit.Framework;
+using Rubicon.Development.UnitTesting;
+using Rubicon.Web.ExecutionEngine;
+using Rubicon.Utilities;
+using Rubicon.Web.UnitTests.AspNetFramework;
+
+namespace Rubicon.Web.UnitTests.ExecutionEngine
+{
+
+public class WxeMappingTest
+{
+  [SetUp]
+  public virtual void SetUp()
+  {
+  }
+
+  [TearDown]
+  public virtual void TearDown()
+  {
+  }
+}
+
+}
