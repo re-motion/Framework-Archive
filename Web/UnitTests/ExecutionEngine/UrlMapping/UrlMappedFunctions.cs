816d07a98da111c6809e2c4f6d533d017cce7370
Michael Ketting <michael.ketting@rubicon.eu>
2005-10-12 12:35:00 +0000

    Label: "1.3.15.0" User: Build        Date: 10.10.05   Time: 16:56 Labeled

---
+++ Web/UnitTests/ExecutionEngine/UrlMapping/UrlMappedFunctions.cs
@@ -0,0 +1,15 @@
+using System;
+using Rubicon.Web.ExecutionEngine;
+
+namespace Rubicon.Web.UnitTests.ExecutionEngine.Mapping
+{
+
+public class FirstMappedFunction: WxeFunction
+{
+}
+
+public class SecondMappedFunction: WxeFunction
+{
+}
+
+}
