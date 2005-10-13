13a694c3bdfbe265d5207eb708420df26fa05605
Michael Ketting <michael.ketting@rubicon.eu>
2005-10-13 06:41:00 +0000

    Label: "1.3.15.0" User: Build        Date: 10.10.05   Time: 16:56 Labeled

--- Web/Core/ExecutionEngine/WxeParameterDeclaration.cs
+++ Web/Core/ExecutionEngine/WxeParameterDeclaration.cs
@@ -11,28 +11,18 @@ using Rubicon.Utilities;
 namespace Rubicon.Web.ExecutionEngine
 {
 
-/// <summary>
-/// Specifies in which direction a parameter is passed.
-/// </summary>
+/// <summary> Specifies in which direction a parameter is passed. </summary>
 public enum WxeParameterDirection
 {
-  /// <summary>
-  /// The parameter is passed from the caller to the callee.
-  /// </summary>
+  /// <summary> The parameter is passed from the caller to the callee. </summary>
   In,
-  /// <summary>
-  /// The parameter is returned from the callee to the caller.
-  /// </summary>
+  /// <summary> The parameter is returned from the callee to the caller. </summary>
   Out,
-  /// <summary>
-  /// The parameter is passed from the caller to the callee and returned back to the caller.
-  /// </summary>
+  /// <summary> The parameter is passed from the caller to the callee and returned back to the caller. </summary>
   InOut
 }
 
-/// <summary>
-/// Declares a WXE parameter.
-/// </summary>
+/// <summary> Declares a WXE parameter. </summary>
 [Serializable]
 public class WxeParameterDeclaration
 {
@@ -69,18 +59,14 @@ public class WxeParameterDeclaration
     get { return _type; }
   }
 
-  /// <summary>
-  ///   Copy a single caller variable to a callee parameter.
-  /// </summary>
+  /// <summary> Copy a single caller variable to a callee parameter. </summary>
   public void CopyToCallee (string actualParameterName, NameObjectCollection callerVariables, NameObjectCollection calleeVariables)
   {
     if (_direction != WxeParameterDirection.Out)
       CopyParameter (actualParameterName, callerVariables, _name, calleeVariables, _required);
   }
 
-  /// <summary>
-  ///   Copy a value to a callee parameter.
-  /// </summary>
+  /// <summary> Copy a value to a callee parameter. </summary>
   public void CopyToCallee (object parameterValue, NameObjectCollection calleeVariables)
   {
     if (_direction == WxeParameterDirection.Out)
@@ -89,18 +75,14 @@ public class WxeParameterDeclaration
     SetParameter (_name, parameterValue, calleeVariables); 
   }
 
-  /// <summary>
-  ///   Copy a single callee parameter back to a caller variable.
-  /// </summary>
+  /// <summary> Copy a single callee parameter back to a caller variable. </summary>
   public void CopyToCaller (string actualParameterName, NameObjectCollection calleeVariables, NameObjectCollection callerVariables)
   {
     if (_direction != WxeParameterDirection.In)
       CopyParameter (_name, calleeVariables, actualParameterName, callerVariables, false);
   }
 
-  /// <summary>
-  ///   Copy fromVariables[fromName] to toVariables[toName].
-  /// </summary>
+  /// <summary> Copy fromVariables[fromName] to toVariables[toName]. </summary>
   private void CopyParameter (string fromName, NameObjectCollection fromVariables, string toName, NameObjectCollection toVariables, bool required)
   {
     object value = fromVariables[fromName];
@@ -109,9 +91,7 @@ public class WxeParameterDeclaration
     SetParameter (toName, value, toVariables);
   }
 
-  /// <summary>
-  ///   Set the parameter variables[parameterName] to the specified value.
-  /// </summary>
+  /// <summary> Set the parameter variables[parameterName] to the specified value. </summary>
   private void SetParameter (string parameterName, object value, NameObjectCollection variables)
   {
     if (value != null && _type != null && ! _type.IsAssignableFrom (value.GetType()))
