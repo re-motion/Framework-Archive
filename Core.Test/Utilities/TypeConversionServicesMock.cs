8e91c17ddb1964185f77a90af88dd8f12914b01e
Michael Ketting <michael.ketting@rubicon.eu>
2005-10-17 09:43:00 +0000

    Label: "1.3.17.0" User: Build        Date: 17.10.05   Time:  8:43 Labeled

---
+++ Core.Test/Utilities/TypeConversionServicesMock.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.ComponentModel;
+
+using Rubicon.Utilities;
+
+namespace Rubicon.Core.UnitTests.Utilities
+{
+
+/// <summary> Exposes non-public members of the <see cref="TypeConverterFactory"/> type. </summary>
+public class TypeConverterFactoryMock: TypeConverterFactory
+{
+  public new TypeConverter GetTypeConverterByAttribute (Type type)
+  {
+    return base.GetTypeConverterByAttribute (type);
+  }
+
+  public new TypeConverter GetCachedTypeConverterByAttribute (Type type)
+  {
+    return base.GetCachedTypeConverterByAttribute (type);
+  }
+
+  public new void AddTypeConverterToCache (Type key, TypeConverter converter)
+  {
+    base.AddTypeConverterToCache (key, converter);
+  }
+
+  public new TypeConverter GetCachedTypeConverter (Type key)
+  {
+    return base.GetCachedTypeConverter (key);
+  }
+
+}
+
+}
