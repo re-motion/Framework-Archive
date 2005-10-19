3979f9d43d53149214a96a3055d78d53541fba2c
Michael Ketting <michael.ketting@rubicon.eu>
2005-10-19 14:47:00 +0000

    Label: "1.3.17.0" User: Build        Date: 17.10.05   Time:  8:43 Labeled

---
+++ Web/UnitTests/Utilities/UrlUtilityTest.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Specialized;
+using System.Web;
+using NUnit.Framework;
+using Rubicon.Web.Utilities;
+
+namespace Rubicon.Web.UnitTests.Utilities
+{
+
+[TestFixture]
+public class UrlUtilityTest
+{
+  [Test]
+  public void FormatQueryString()
+  {
+    string parameter1 = "Parameter1";
+    string parameter2 = "Parameter2";
+    string value1 = "Value1";
+    string value2 = "Value2";
+
+    NameValueCollection queryString = new NameValueCollection();
+    queryString.Add (parameter1, value1);
+    queryString.Add (parameter2, value2);
+
+    string expectedQueryString = string.Format (
+        "?{0}={1}&{2}={3}", parameter1, HttpUtility.UrlEncode (value1), parameter2, HttpUtility.UrlEncode (value2));
+
+    Assert.AreEqual (expectedQueryString, UrlUtility.FormatQueryString (queryString));
+  }
+
+  [Test]
+  public void FormatQueryStringNoParameters()
+  {
+    NameValueCollection queryString = new NameValueCollection();
+    Assert.AreEqual ("", UrlUtility.FormatQueryString (queryString));
+  }
+}
+
+}
