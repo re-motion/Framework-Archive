6936a564ee2efcc27e734fb0d03c2cac42a4a1cb
Michael Ketting <michael.ketting@rubicon.eu>
2005-10-19 07:19:00 +0000

    Label: "1.3.17.0" User: Build        Date: 17.10.05   Time:  8:43 Labeled

---
+++ Core.Test/Utilities/BidirectionalStringConverterTest.cs
@@ -0,0 +1,103 @@
+using System;
+using System.ComponentModel;
+using NUnit.Framework;
+using Rubicon.Utilities;
+
+namespace Rubicon.Core.UnitTests.Utilities
+{
+
+[TestFixture]
+public class BidirectionalStringConverterTest
+{
+  private BidirectionalStringConverter _converter;
+  
+  [SetUp]
+  public void SetUp()
+  {
+    _converter = new BidirectionalStringConverter();
+  }
+
+  [Test]
+  public void CanConvertToInt32()
+  {
+    Assert.IsTrue (_converter.CanConvertTo (typeof (int)));
+  }
+
+  [Test]
+  public void CanConvertToObject()
+  {
+    Assert.IsFalse (_converter.CanConvertTo (typeof (object)));
+  }
+
+  [Test]
+  public void CanConvertFromInt32()
+  {
+    Assert.IsTrue (_converter.CanConvertFrom (typeof (int)));
+  }
+
+  [Test]
+  public void CanConvertFromObject2()
+  {
+    Assert.IsTrue (_converter.CanConvertFrom (typeof (object)));
+  }
+
+  [Test]
+  public void ConvertToInt32()
+  {
+    Type destinationType = typeof (int);
+
+    Assert.AreEqual (0, _converter.ConvertTo ("0", destinationType));
+    Assert.AreEqual (1, _converter.ConvertTo ("1", destinationType));
+  }
+
+  [Test]
+  public void ConvertFromInt32()
+  {
+    Assert.AreEqual ("0", _converter.ConvertFrom (0));
+    Assert.AreEqual ("1", _converter.ConvertFrom (1));
+  }
+
+  [Test]
+  [ExpectedException (typeof (ArgumentNullException))]
+  public void ConvertToInt32WithNull()
+  {
+    Type destinationType = typeof (int);
+
+    _converter.ConvertTo (null, destinationType);
+    Assert.Fail();
+  }
+
+  [Test]
+  public void ConvertFromNull()
+  {
+    Assert.AreEqual ("", _converter.ConvertFrom (null));
+  }
+
+  [Test]
+  public void CanConvertToDBNull()
+  {
+    Assert.IsFalse (_converter.CanConvertTo (typeof (DBNull)));
+  }
+
+  [Test]
+  public void CanConvertFromDBNull()
+  {
+    Assert.IsTrue (_converter.CanConvertFrom (typeof (DBNull)));
+  }
+
+  [Test]
+  [ExpectedException (typeof (NotSupportedException))]
+  public void ConvertToDBNull()
+  {
+    _converter.ConvertTo ("", typeof (DBNull));
+    Assert.Fail();
+  }
+
+  [Test]
+  public void ConvertFromDBNull()
+  {
+    Assert.AreEqual ("", _converter.ConvertFrom (DBNull.Value));
+  }
+}            
+
+}
