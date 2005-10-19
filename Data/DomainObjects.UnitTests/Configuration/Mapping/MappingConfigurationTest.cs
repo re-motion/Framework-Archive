e9b75dc127439c52f8804bbfb2d821e1cc060244
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-10-19 11:00:00 +0000

    Label: "1.3.17.0" User: Build        Date: 17.10.05   Time:  8:43 Labeled

--- Data/DomainObjects.UnitTests/Configuration/Mapping/MappingConfigurationTest.cs
+++ Data/DomainObjects.UnitTests/Configuration/Mapping/MappingConfigurationTest.cs
@@ -3,6 +3,8 @@ using System.IO;
 using NUnit.Framework;
 
 using Rubicon.Data.DomainObjects.Mapping;
+using Rubicon.Data.DomainObjects.UnitTests.Factories;
+using Rubicon.Data.DomainObjects.UnitTests.TestDomain;
 
 namespace Rubicon.Data.DomainObjects.UnitTests.Configuration.Mapping
 {
@@ -46,6 +48,66 @@ public class MappingConfigurationTest
   public void ApplicationName ()
   {
     Assert.AreEqual ("UnitTests", MappingConfiguration.Current.ApplicationName);
+  }
+
+  [Test]
+  public void ContainsClassDefinition ()
+  {
+    Assert.IsFalse (MappingConfiguration.Current.Contains (TestMappingConfiguration.Current.ClassDefinitions["Order"]));
+    Assert.IsTrue (MappingConfiguration.Current.Contains (MappingConfiguration.Current.ClassDefinitions["Order"]));
+  }
+
+  [Test]
+  [ExpectedException (typeof (ArgumentNullException))]
+  public void ContainsNull ()
+  {
+    MappingConfiguration.Current.Contains ((ClassDefinition) null);
+  }
+
+  [Test]
+  public void ContainsPropertyDefinition ()
+  {
+    Assert.IsFalse (MappingConfiguration.Current.Contains (TestMappingConfiguration.Current.ClassDefinitions["Order"]["OrderNumber"]));
+    Assert.IsTrue (MappingConfiguration.Current.Contains (MappingConfiguration.Current.ClassDefinitions["Order"]["OrderNumber"]));
+  }
+
+  [Test]
+  public void ContainsRelationDefinition ()
+  {
+
+    Assert.IsFalse (MappingConfiguration.Current.Contains (TestMappingConfiguration.Current.RelationDefinitions["OrderToOrderItem"]));
+    Assert.IsTrue (MappingConfiguration.Current.Contains (MappingConfiguration.Current.RelationDefinitions["OrderToOrderItem"]));
+  }
+
+  [Test]
+  public void ContainsRelationEndPointDefinition ()
+  {
+    Assert.IsFalse (MappingConfiguration.Current.Contains (TestMappingConfiguration.Current.RelationDefinitions["OrderToOrderItem"].EndPointDefinitions[0]));
+    Assert.IsFalse (MappingConfiguration.Current.Contains (TestMappingConfiguration.Current.RelationDefinitions["OrderToOrderItem"].EndPointDefinitions[1]));
+
+    Assert.IsTrue (MappingConfiguration.Current.Contains (MappingConfiguration.Current.RelationDefinitions["OrderToOrderItem"].EndPointDefinitions[0]));
+    Assert.IsTrue (MappingConfiguration.Current.Contains (MappingConfiguration.Current.RelationDefinitions["OrderToOrderItem"].EndPointDefinitions[1]));
+  }
+
+  [Test]
+  public void ContainsRelationEndPointDefinitionNotInMapping ()
+  {
+    ClassDefinition orderDefinition = new ClassDefinition ("Order", "Order", typeof (Order), "TestDomain");
+    ClassDefinition orderTicketDefinition = new ClassDefinition ("OrderTicket", "OrderTicket", typeof (OrderTicket), "TestDomain"); 
+    orderTicketDefinition.MyPropertyDefinitions.Add (new PropertyDefinition ("Order", "OrderID", "objectID", false));
+
+    VirtualRelationEndPointDefinition orderEndPointDefinition = new VirtualRelationEndPointDefinition (
+        orderDefinition, "OrderTicket", true, CardinalityType.One, typeof (OrderTicket));
+
+    RelationEndPointDefinition orderTicketEndPointdefinition = new RelationEndPointDefinition (orderTicketDefinition, "Order", true);
+
+    RelationDefinition relationDefinitionNotInMapping = new RelationDefinition (
+        "RelationIDNotInMapping", 
+        orderEndPointDefinition,
+        orderTicketEndPointdefinition);
+
+    Assert.IsFalse (MappingConfiguration.Current.Contains (orderEndPointDefinition));
+
   }
 }
 }
