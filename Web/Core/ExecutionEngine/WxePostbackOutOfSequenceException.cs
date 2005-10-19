48b98f5211af280ce958670116731ec36ae683b7
Michael Ketting <michael.ketting@rubicon.eu>
2005-10-19 14:47:00 +0000

    Label: "1.3.17.0" User: Build        Date: 17.10.05   Time:  8:43 Labeled

--- Web/Core/ExecutionEngine/WxePostbackOutOfSequenceException.cs
+++ Web/Core/ExecutionEngine/WxePostbackOutOfSequenceException.cs
@@ -6,15 +6,15 @@ namespace Rubicon.Web.ExecutionEngine
 
 /// <summary> This exception indicates an attempt to resubmit a page cached in the browser's history. </summary>
 [Serializable]
-public class WxePostBackOutOfSequenceException: WxeException
+public class WxePostbackOutOfSequenceException: WxeException
 {
-  public WxePostBackOutOfSequenceException()
+  public WxePostbackOutOfSequenceException()
     : base ("The server has received a post back from a page that has already been submitted. "
         + "The page's state is no longer valid. Please navigate to the start page to restart the web application.")
   {
   }
 
-  protected WxePostBackOutOfSequenceException (SerializationInfo info, StreamingContext context)
+  protected WxePostbackOutOfSequenceException (SerializationInfo info, StreamingContext context)
     : base (info, context)
   {
   }
