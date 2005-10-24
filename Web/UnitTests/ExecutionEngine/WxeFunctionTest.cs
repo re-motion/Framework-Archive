4fb90069ffd8fbd429657b39acf0211298362c41
Michael Ketting <michael.ketting@rubicon.eu>
2005-10-24 10:29:00 +0000

    Label: "1.3.18.0" User: Build        Date: 19.10.05   Time: 18:41 Labeled

--- Web/UnitTests/ExecutionEngine/WxeFunctionTest.cs
+++ Web/UnitTests/ExecutionEngine/WxeFunctionTest.cs
@@ -31,9 +31,82 @@ public class WxeFunctionTest: WxeTest
     TestFunctionWithSerializableParameters function = 
         new TestFunctionWithSerializableParameters ("Hello World", NaInt32.Null, 1);
     NameValueCollection parameters = function.SerializeParametersForQueryString();
-    Assert.AreEqual ("Hello World", parameters["StringValue"].ToString());
-    Assert.AreEqual (NaInt32.Null.ToString(), parameters["NaInt32Value"].ToString());
-    Assert.AreEqual ("1", parameters["IntValue"].ToString());
+    Assert.AreEqual (3, parameters.Count);
+    Assert.AreEqual ("Hello World", parameters["StringValue"]);
+    Assert.AreEqual ("", parameters["NaInt32Value"]);
+    Assert.AreEqual ("1", parameters["IntValue"]);
+  }
+
+  [Test]
+  public void SerializeParametersWithInt32Null()
+  {
+    TestFunctionWithSerializableParameters function = new TestFunctionWithSerializableParameters ();
+    function.Variables["StringValue"] = "Hello World";
+    function.Variables["NaInt32Value"] = new NaInt32 (1);
+    function.Variables["Int32Value"] = null;
+
+    NameValueCollection parameters = function.SerializeParametersForQueryString();
+    
+    Assert.AreEqual (2, parameters.Count);
+    Assert.AreEqual ("Hello World", parameters["StringValue"]);
+    Assert.AreEqual ("1", parameters["NaInt32Value"]);
+  }
+
+  [Test]
+  public void InitializeParametersThroughQueryStringWithEmptyString()
+  {
+    HttpContext context = HttpContextHelper.CreateHttpContext (
+        "GET", @"C:\index.html", "http://localhost/index.hmtl", "StringValue=&NaInt32Value=2&IntValue=1");
+
+    TestFunctionWithSerializableParameters function = new TestFunctionWithSerializableParameters ();
+    function.InitializeParameters (context.Request.Params);
+
+    Assert.AreEqual ("", function.StringValue);
+    Assert.AreEqual (new NaInt32 (2), function.NaInt32Value);
+    Assert.AreEqual (1, function.IntValue);
+  }
+
+  [Test]
+  public void InitializeParametersThroughQueryStringWithNaInt32BeingEmpty()
+  {
+    HttpContext context = HttpContextHelper.CreateHttpContext (
+        "GET", @"C:\index.html", "http://localhost/index.hmtl", "StringValue=Hello+World&NaInt32Value=&IntValue=1");
+
+    TestFunctionWithSerializableParameters function = new TestFunctionWithSerializableParameters ();
+    function.InitializeParameters (context.Request.Params);
+
+    Assert.AreEqual ("Hello World", function.StringValue);
+    Assert.AreEqual (NaInt32.Null, function.NaInt32Value);
+    Assert.AreEqual (1, function.IntValue);
+  }
+
+  [Test]
+  [ExpectedException (typeof (ApplicationException))]
+  public void InitializeParametersThroughQueryStringWitInt32BeingEmpty()
+  {
+    HttpContext context = HttpContextHelper.CreateHttpContext (
+        "GET", @"C:\index.html", "http://localhost/index.hmtl", "StringValue=Hello+World&NaInt32Value=1&IntValue=");
+
+    TestFunctionWithSerializableParameters function = new TestFunctionWithSerializableParameters ();
+    function.InitializeParameters (context.Request.Params);
+
+    Assert.Fail();
+  }
+
+  [Test]
+  public void InitializeParameters()
+  {
+    TestFunctionWithSerializableParameters function = new TestFunctionWithSerializableParameters ();
+    NameValueCollection parameters = new NameValueCollection();
+    parameters.Add ("StringValue", "Hello World");
+    parameters.Add ("NaInt32Value", "");
+    parameters.Add ("IntValue", "1");
+
+    function.InitializeParameters (parameters);
+
+    Assert.AreEqual ("Hello World", function.StringValue);
+    Assert.AreEqual (NaInt32.Null, function.NaInt32Value);
+    Assert.AreEqual (1, function.IntValue);
   }
 }
 
