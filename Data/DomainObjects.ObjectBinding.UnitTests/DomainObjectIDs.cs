992f0fc6650bd3d9750c32de4a64ab76b9d20843
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-11-03 11:13:00 +0000

    Label: "1.3.19.0" User: Build        Date: 26.10.05   Time: 14:52 Labeled

---
+++ Data/DomainObjects.ObjectBinding.UnitTests/DomainObjectIDs.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Rubicon.Data.DomainObjects.ObjectBinding.UnitTests
+{
+public class DomainObjectIDs
+{
+  // types
+
+  // static members and constants
+
+  public static ObjectID Order1 = new ObjectID ("Order", new Guid ("{5682F032-2F0B-494b-A31C-C97F02B89C36}"));
+
+  // member fields
+
+  // construction and disposing
+
+  public DomainObjectIDs ()
+  {
+  }
+
+  // methods and properties
+
+}
+}
