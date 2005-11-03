a15870ff301500eef4763588538919b4d453c30f
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-11-03 11:13:00 +0000

    Label: "1.3.19.0" User: Build        Date: 26.10.05   Time: 14:52 Labeled

---
+++ Data/DomainObjects.ObjectBinding.UnitTests/Serialization/SerializationTest.cs
@@ -0,0 +1,82 @@
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using NUnit.Framework;
+
+using Rubicon.Data.DomainObjects.ObjectBinding.UnitTests.TestDomain;
+
+namespace Rubicon.Data.DomainObjects.ObjectBinding.UnitTests.Serialization
+{
+[TestFixture]
+public class SerializationTest : DatabaseTest
+{
+  // types
+
+  // static members and constants
+
+  // member fields
+
+  // construction and disposing
+
+  public SerializationTest ()
+  {
+  }
+
+  // methods and properties
+
+  [Test]
+  public void BindableDomainObjectTest ()
+  {
+    Order order = Order.GetObject (DomainObjectIDs.Order1);
+
+    Order deserializedOrder = (Order) SerializeAndDeserialize (order);
+
+    Assert.IsNotNull (deserializedOrder);
+  }
+
+  [Test]
+  public void BindableDomainObjectWithReflector ()
+  {
+    Order order = Order.GetObject (DomainObjectIDs.Order1);
+
+    Order deserializedOrder = (Order) SerializeAndDeserialize (order);
+
+    Assert.AreEqual (order.GetProperty ("OrderNumber"), deserializedOrder.GetProperty ("OrderNumber"));
+  }
+
+  [Test]
+  public void BindableSearchObjectTest ()
+  {
+    TestSearchObject testSearchObject = new TestSearchObject ();
+
+    TestSearchObject deserializedTestSearchObject = (TestSearchObject) SerializeAndDeserialize (testSearchObject);
+
+    Assert.IsNotNull (deserializedTestSearchObject);
+  }
+
+  [Test]
+  public void BindableSearchObjectWithReflector ()
+  {
+    TestSearchObject testSearchObject = new TestSearchObject ();
+    testSearchObject.StringProperty = "test";
+
+    TestSearchObject deserializedTestSearchObject = (TestSearchObject) SerializeAndDeserialize (testSearchObject);
+
+    Assert.AreEqual (testSearchObject.GetProperty ("StringProperty"), deserializedTestSearchObject.GetProperty ("StringProperty"));
+  }
+
+  private object SerializeAndDeserialize  (object graph)
+  {
+    using (MemoryStream stream = new MemoryStream ())
+    {
+      BinaryFormatter serializationFormatter = new BinaryFormatter ();
+      serializationFormatter.Serialize (stream, graph);
+
+      stream.Position = 0;
+
+      BinaryFormatter deserializationFormatter = new BinaryFormatter ();
+      return deserializationFormatter.Deserialize (stream);
+    }
+  }
+}
+}
