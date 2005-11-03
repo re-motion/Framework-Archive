46f4750a827d13990cf04df2a035b488466e8824
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-11-03 11:13:00 +0000

    Label: "1.3.19.0" User: Build        Date: 26.10.05   Time: 14:52 Labeled

---
+++ Data/DomainObjects.Web.Test/Domain/DomainObjectIDs.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace Rubicon.Data.DomainObjects.Web.Test.Domain
+{
+public class DomainObjectIDs
+{
+  // types
+
+  // static members and constants
+
+  public static ObjectID ObjectWithAllDataTypes1 = new ObjectID ("ClassWithAllDataTypes", new Guid ("3F647D79-0CAF-4a53-BAA7-A56831F8CE2D"));
+
+  // member fields
+
+  // construction and disposing
+
+  public DomainObjectIDs ()
+  {
+  }
+
+  // methods and properties
+
+}
+}
