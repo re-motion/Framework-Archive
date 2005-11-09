487c4d69faf040c482c28493225ca3ae661e5906
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2005-11-09 16:41:00 +0000

    Label: "1.3.20.0" User: Build        Date:  3.11.05   Time: 18:23 Labeled

--- Data/DomainObjects.Web.Test/Domain/DomainObjectIDs.cs
+++ Data/DomainObjects.Web.Test/Domain/DomainObjectIDs.cs
@@ -8,7 +8,8 @@ public class DomainObjectIDs
 
   // static members and constants
 
-  public static ObjectID ObjectWithAllDataTypes1 = new ObjectID ("ClassWithAllDataTypes", new Guid ("3F647D79-0CAF-4a53-BAA7-A56831F8CE2D"));
+  public static ObjectID ObjectWithAllDataTypes1 = new ObjectID ("ClassWithAllDataTypes", new Guid ("{3F647D79-0CAF-4a53-BAA7-A56831F8CE2D}"));
+  public static ObjectID ObjectWithUndefinedEnum = new ObjectID ("ClassWithUndefinedEnum", new Guid ("{4F85CEE5-A53A-4bc5-B9D3-448C48946498}"));
 
   // member fields
 
