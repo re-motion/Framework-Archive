55cb1f038ebdd037dae3d4cc1975342b89fc9eab
Michael Ketting <michael.ketting@rubicon.eu>
2005-11-10 11:26:00 +0000

    Label: "1.3.21.0" User: Build        Date: 10.11.05   Time:  8:29 Labeled

---
+++ ObjectBinding/Web.UnitTests/UI/Controls/BocListSortingOrderEntryMock.cs
@@ -0,0 +1,40 @@
+using System;
+using Rubicon.ObjectBinding.Web.Controls;
+
+namespace Rubicon.ObjectBinding.Web.UnitTests.UI.Controls
+{
+
+public class BocListSortingOrderEntryMock : BocListSortingOrderEntry
+{
+  public BocListSortingOrderEntryMock (BocColumnDefinition column, SortingDirection direction)
+    : base (column, direction)
+  {
+  }
+
+  public BocListSortingOrderEntryMock (int columnIndex, SortingDirection direction)
+    : base (columnIndex, direction)
+  {
+  }
+
+  public new int ColumnIndex
+  {
+    get { return base.ColumnIndex; }
+  }
+
+  public new void SetColumnIndex (int columnIndex)
+  {
+    base.SetColumnIndex (columnIndex);
+  }
+
+  public new void SetColumn (BocColumnDefinition column)
+  {
+    base.SetColumn (column); 
+  }
+
+  public new void SetDirection (SortingDirection direction)
+  {
+    base.SetDirection  (direction);
+  }
+}
+
+}
