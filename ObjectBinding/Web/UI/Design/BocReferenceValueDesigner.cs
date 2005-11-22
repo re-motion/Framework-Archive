1106ccfcd36f52dddc4e8cf09e842b791ee171bd
Michael Ketting <michael.ketting@rubicon.eu>
2005-11-22 13:24:00 +0000

    Label: "1.3.27.0" User: Build        Date: 18.11.05   Time:  8:53 Labeled

--- ObjectBinding/Web/UI/Design/BocReferenceValueDesigner.cs
+++ ObjectBinding/Web/UI/Design/BocReferenceValueDesigner.cs
@@ -2,11 +2,12 @@ using System;
 using System.ComponentModel;
 using System.Web.UI.Design;
 using Rubicon.ObjectBinding.Web.Controls;
+using Rubicon.Web.UI.Design;
 
 namespace Rubicon.ObjectBinding.Web.Design
 {
 
-public class BocReferenceValueDesigner: Design.BocDesigner
+public class BocReferenceValueDesigner: WebControlDesigner
 {
   public override void OnComponentChanged(object sender, System.ComponentModel.Design.ComponentChangedEventArgs ce)
   {
