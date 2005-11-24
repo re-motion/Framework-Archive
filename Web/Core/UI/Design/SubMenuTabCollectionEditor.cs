36e142f089213e98382e69f870a882e253bede9a
Michael Ketting <michael.ketting@rubicon.eu>
2005-11-24 16:20:00 +0000

    Label: "1.3.27.0" User: Build        Date: 18.11.05   Time:  8:53 Labeled

---
+++ Web/Core/UI/Design/SubMenuTabCollectionEditor.cs
@@ -0,0 +1,22 @@
+using System;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using Rubicon.Web.UI.Controls;
+
+namespace Rubicon.Web.UI.Design
+{
+
+public class SubMenuTabCollectionEditor: AdvancedCollectionEditor
+{
+  public SubMenuTabCollectionEditor (Type type)
+    : base (type)
+  {
+  }
+
+  protected override Type[] CreateNewItemTypes()
+  {
+    return new Type[] {typeof (SubMenuTab)};
+  }
+}
+
+}
