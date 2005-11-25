04c2a30a3029113e8e9b93f094e43b7d3bd22072
Michael Ketting <michael.ketting@rubicon.eu>
2005-11-25 15:42:00 +0000

    Label: "1.3.27.0" User: Build        Date: 18.11.05   Time:  8:53 Labeled

---
+++ Web/Core/UI/Design/TabbedMenuDesigner.cs
@@ -0,0 +1,56 @@
+using System;
+using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Drawing.Design;
+using System.Web.UI.Design;
+using Rubicon.Utilities;
+using Rubicon.Web.UI.Design;
+using Rubicon.Web.UI.Controls;
+
+namespace Rubicon.Web.UI.Design
+{
+public class TabStripMenuDesigner: WebControlDesigner, IServiceProvider
+{
+  private DesignerVerbCollection _verbs = null;
+
+	public TabStripMenuDesigner()
+	{
+    _verbs = new DesignerVerbCollection();
+    _verbs.Add (new DesignerVerb ("Edit Menu Tabs", new EventHandler(OnVerbEditFixedColumns)));
+  }
+
+  private void OnVerbEditFixedColumns (object sender, EventArgs e) 
+  {
+    TabStripMenu tabStripMenu = Component as TabStripMenu;
+    if (tabStripMenu == null)
+      throw new InvalidOperationException ("Cannot use TabStripMenuDesigner for objects other than TabStripMenu.");
+
+    PropertyDescriptorCollection propertyDescriptors = TypeDescriptor.GetProperties (tabStripMenu);
+    PropertyDescriptor propertyDescriptor = propertyDescriptors["Tabs"];
+
+    TypeDescriptorContext context = new TypeDescriptorContext (this, this, propertyDescriptor);
+    object value = propertyDescriptor.GetValue (Component);
+    MainMenuTabCollectionEditor editor = null;
+    //Does not work because EditorAttribute is applied on property
+    //editor = (MainMenuTabCollectionEditor) TypeDescriptor.GetEditor (value, typeof(UITypeEditor));
+    editor = new MainMenuTabCollectionEditor (typeof (WebTabCollection));
+    editor.EditValue (context, this, value);
+  }
+
+  public override DesignerVerbCollection Verbs 
+  {
+    get { return _verbs; }
+  }
+
+  protected override object GetService (Type serviceType)
+  {
+    return base.GetService (serviceType);
+  }
+
+  object IServiceProvider.GetService (Type serviceType)
+  {
+    return GetService (serviceType);
+  }
+}
+
+}
