f8be70342b78db18ddca9c438ce5c794777d0915
Michael Ketting <michael.ketting@rubicon.eu>
2005-11-25 15:42:00 +0000

    Label: "1.3.27.0" User: Build        Date: 18.11.05   Time:  8:53 Labeled

--- Web/Core/UI/Controls/WebTreeView.cs
+++ Web/Core/UI/Controls/WebTreeView.cs
@@ -412,9 +412,9 @@ public class WebTreeView: WebControl, IControl, IPostBackEventHandler, IResource
   /// <summary> Overrides the parent control's <c>RenderContents</c> method. </summary>
   protected override void RenderContents (HtmlTextWriter writer)
   {
-    if (WcagHelper.IsWcagDebuggingEnabled() && WcagHelper.IsWaiConformanceLevelARequired())
+    if (WcagHelper.Instance.IsWcagDebuggingEnabled() && WcagHelper.Instance.IsWaiConformanceLevelARequired())
     {
-      WcagHelper.HandleError (1, this);
+      WcagHelper.Instance.HandleError (1, this);
     }
 
     ResolveNodeIcons();
@@ -864,17 +864,6 @@ public class WebTreeView: WebControl, IControl, IPostBackEventHandler, IResource
       if (_isLoadViewStateCompleted)
         EnsureTreeNodesCreated();
       return _selectedNode; 
-    }
-  }
- 
-  /// <summary> Gets an instance of the the <see cref="WcagHelper"/> type. </summary>
-  protected virtual WcagHelper WcagHelper
-  {
-    get 
-    {
-      if (_wcagHelper == null)
-        _wcagHelper = new WcagHelper();
-      return _wcagHelper; 
     }
   }
 
