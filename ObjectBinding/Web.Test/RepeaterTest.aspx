eed904eb5f6be0150a20b20ed6a7e33a1ea57e26
Michael Ketting <michael.ketting@rubicon.eu>
2006-01-13 11:32:00 +0000

    Label: "1.3.29.0" User: Build        Date: 12.01.06   Time: 12:03 Labeled

--- ObjectBinding/Web.Test/RepeaterTest.aspx
+++ ObjectBinding/Web.Test/RepeaterTest.aspx
@@ -1,21 +1,55 @@
+<%@ Register TagPrefix="uc1" TagName="TestTabbedPersonDetailsUserControl" Src="TestTabbedPersonDetailsUserControl.ascx" %>
 <%@ Page language="c#" Codebehind="RepeaterTest.aspx.cs" AutoEventWireup="false" Inherits="OBWTest.RepeaterTest" %>
+<%@ Register TagPrefix="uc1" TagName="TestTabbedPersonJobsUserControl" Src="TestTabbedPersonJobsUserControl.ascx" %>
+<%@ Register TagPrefix="rubicon" Namespace="Rubicon.Web.UI.Controls" Assembly="Rubicon.Web" %>
 <%@ Register TagPrefix="obw" Namespace="Rubicon.ObjectBinding.Web.Controls" Assembly="Rubicon.ObjectBinding.Web" %>
 <%@ Register TagPrefix="cc1" Namespace="Rubicon.ObjectBinding.Reflection" Assembly="Rubicon.ObjectBinding.Reflection" %>
+<%@ Register TagPrefix="obrt" Namespace="OBRTest" Assembly="OBRTest" %>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN" >
 <html>
   <head>
     <title>RepeaterTest</title>
-    <meta name="GENERATOR" Content="Microsoft Visual Studio .NET 7.1">
-    <meta name="CODE_LANGUAGE" Content="C#">
-    <meta name=vs_defaultClientScript content="JavaScript">
-    <meta name=vs_targetSchema content="http://schemas.microsoft.com/intellisense/ie5">
+<meta content="Microsoft Visual Studio .NET 7.1" name=GENERATOR>
+<meta content=C# name=CODE_LANGUAGE>
+<meta content=JavaScript name=vs_defaultClientScript>
+<meta content=http://schemas.microsoft.com/intellisense/ie5 name=vs_targetSchema>
+<rubicon:htmlheadcontents id="HtmlHeadContents" runat="server"></rubicon:htmlheadcontents>
   </head>
-  <body MS_POSITIONING="FlowLayout">
-	
-    <form id="Form1" method="post" runat="server"><asp:Repeater id="Repeater1" runat="server"><itemtemplate><div><obw:boctextvalue id="BocTextValue1" runat="server" DataSourceControl="ItemDataSourceControl" PropertyIdentifier="FirstName">
-<textboxstyle textmode="SingleLine">
-</TextBoxStyle></obw:boctextvalue><obw:BusinessObjectReferenceDataSourceControl id="ItemDataSourceControl" runat="server"></obw:BusinessObjectReferenceDataSourceControl></div></itemtemplate></asp:Repeater><cc1:ReflectionBusinessObjectDataSourceControl id="DataSource" runat="server" typename="OBRTest.Person,OBRTest"></cc1:ReflectionBusinessObjectDataSourceControl>
-     </form>
-	
+<body MS_POSITIONING="FlowLayout">
+<form id=Form1 method=post runat="server"><rubicon:WebButton id="SaveButton" runat="server" Text="Save"></rubicon:WebButton><asp:repeater id=Repeater1 runat="server">
+    <itemtemplate>
+    <div>
+    <obw:boctextvalue id="UnboundField" runat="server">
+</obw:boctextvalue>
+</div>
+</itemtemplate>
+</asp:repeater>
+<hr>
+<obrt:objectboundrepeater id=Repeater2 runat="server" datasourcecontrol="CurrentObject" propertyidentifier="Children">
+<itemtemplate>
+    <div>
+    <obw:boctextvalue id="FirstNameField" runat="server" ReadOnly="true" DataSourceControl="ItemDataSourceControl" PropertyIdentifier="FirstName">
+</obw:boctextvalue>
+<cc1:reflectionbusinessobjectdatasourcecontrol id="ItemDataSourceControl" runat="server" typename="OBRTest.Person,OBRTest"></cc1:reflectionbusinessobjectdatasourcecontrol>
+</div>
+</ItemTemplate>
+</obrt:objectboundrepeater>
+<hr>
+<obrt:objectboundrepeater id="Repeater3" runat="server" datasourcecontrol="CurrentObject" propertyidentifier="Children" readonly="True">
+<itemtemplate>
+<table style="width:100%">
+<tr>
+<td>
+<uc1:testtabbedpersondetailsusercontrol id="TestTabbedPersonDetailsUserControl1" runat="server"></uc1:testtabbedpersondetailsusercontrol>
+</td>
+<td>
+<uc1:testtabbedpersonjobsusercontrol id="TestTabbedPersonJobsUserControl1" runat="server"></uc1:testtabbedpersonjobsusercontrol>
+</td></tr>
+</table>
+</ItemTemplate>
+</obrt:objectboundrepeater>
+<hr>
+<cc1:reflectionbusinessobjectdatasourcecontrol id="CurrentObject" runat="server" typename="OBRTest.Person,OBRTest"></cc1:reflectionbusinessobjectdatasourcecontrol>
+	</form>
   </body>
 </html>
--- ObjectBinding/Web.Test/RepeaterTest.aspx.cs
+++ ObjectBinding/Web.Test/RepeaterTest.aspx.cs
@@ -8,60 +8,77 @@ using System.Web.SessionState;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.UI.HtmlControls;
+
+using Rubicon.ObjectBinding.Reflection;
+using Rubicon.ObjectBinding.Web.Controls;
+
 using OBRTest;
 
 namespace OBWTest
 {
-	/// <summary>
-	/// Summary description for RepeaterTest.
-	/// </summary>
-	public class RepeaterTest : System.Web.UI.Page
+/// <summary>
+/// Summary description for RepeaterTest.
+/// </summary>
+public class RepeaterTest : System.Web.UI.Page
+{
+  protected BocTextValue BocTextValue1;
+  protected BusinessObjectDataSourceControl ItemDataSourceControl;
+  protected OBRTest.ObjectBoundRepeater Repeater2;
+  protected ReflectionBusinessObjectDataSourceControl CurrentObject;
+  protected OBRTest.ObjectBoundRepeater Repeater3;
+  protected Rubicon.Web.UI.Controls.HtmlHeadContents HtmlHeadContents;
+  protected Rubicon.Web.UI.Controls.WebButton SaveButton;
+  protected System.Web.UI.WebControls.Repeater Repeater1;
+
+	private void Page_Load(object sender, System.EventArgs e)
 	{
-    protected Rubicon.ObjectBinding.Web.Controls.BocTextValue BocTextValue1;
-    protected Rubicon.ObjectBinding.Reflection.ReflectionBusinessObjectDataSourceControl DataSource;
-    protected Rubicon.ObjectBinding.Web.Controls.BusinessObjectReferenceDataSourceControl ItemDataSourceControl;
-    protected System.Web.UI.WebControls.Repeater Repeater1;
-  
-		private void Page_Load(object sender, System.EventArgs e)
-		{
-      Guid personID = new Guid(0,0,0,0,0,0,0,0,0,0,1);
-      Person person = Person.GetObject (personID);
+    Guid personID = new Guid(0,0,0,0,0,0,0,0,0,0,1);
+    Person person = Person.GetObject (personID);
 
-      Repeater1.DataSource = person.Children;
-      Repeater1.DataBind();
-		}
+    Repeater1.DataSource = person.Children;
+    Repeater1.DataBind();
 
-		#region Web Form Designer generated code
-		override protected void OnInit(EventArgs e)
-		{
-			//
-			// CODEGEN: This call is required by the ASP.NET Web Form Designer.
-			//
-			InitializeComponent();
-			base.OnInit(e);
-		}
-		
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{    
-      this.Repeater1.ItemCreated += new System.Web.UI.WebControls.RepeaterItemEventHandler(this.Repeater1_ItemCreated);
-      this.Repeater1.ItemDataBound += new System.Web.UI.WebControls.RepeaterItemEventHandler(this.Repeater1_ItemDataBound);
-      this.Load += new System.EventHandler(this.Page_Load);
-
-    }
-		#endregion
-
-    private void Repeater1_ItemCreated(object sender, System.Web.UI.WebControls.RepeaterItemEventArgs e)
-    {
-    
-    }
-
-    private void Repeater1_ItemDataBound(object sender, System.Web.UI.WebControls.RepeaterItemEventArgs e)
-    {
-    
-    }
+    CurrentObject.BusinessObject = person;
+    CurrentObject.LoadValues (IsPostBack);
 	}
+
+	#region Web Form Designer generated code
+	override protected void OnInit(EventArgs e)
+	{
+		//
+		// CODEGEN: This call is required by the ASP.NET Web Form Designer.
+		//
+		InitializeComponent();
+		base.OnInit(e);
+	}
+	
+	/// <summary>
+	/// Required method for Designer support - do not modify
+	/// the contents of this method with the code editor.
+	/// </summary>
+	private void InitializeComponent()
+	{    
+    this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+    this.Repeater1.ItemCreated += new System.Web.UI.WebControls.RepeaterItemEventHandler(this.Repeater1_ItemCreated);
+    this.Repeater1.ItemDataBound += new System.Web.UI.WebControls.RepeaterItemEventHandler(this.Repeater1_ItemDataBound);
+    this.Load += new System.EventHandler(this.Page_Load);
+
+  }
+	#endregion
+
+  private void Repeater1_ItemCreated(object sender, System.Web.UI.WebControls.RepeaterItemEventArgs e)
+  {
+  
+  }
+
+  private void Repeater1_ItemDataBound(object sender, System.Web.UI.WebControls.RepeaterItemEventArgs e)
+  {
+  
+  }
+
+  private void SaveButton_Click(object sender, System.EventArgs e)
+  {
+    CurrentObject.SaveValues (false);
+  }
+}
 }
