4993fb1ca7439eb72d35ae82680511fb0f31aa6c
Michael Ketting <michael.ketting@rubicon.eu>
2006-01-17 10:51:00 +0000

    Label: "1.3.30.0" User: Build        Date: 13.01.06   Time: 16:43 Labeled

---
+++ ObjectBinding/Web.UnitTests/UI/Controls/BocListTest.cs
@@ -0,0 +1,54 @@
+using System;
+using NUnit.Framework;
+using Rubicon.Development.UnitTesting;
+using Rubicon.Web.UI;
+using Rubicon.NullableValueTypes;
+using Rubicon.Web.Configuration;
+using Rubicon.Web.UnitTests.Configuration;
+using Rubicon.ObjectBinding.Web.Controls;
+using Rubicon.Web.UI.Controls;
+using Rubicon.Web.Utilities;
+
+namespace Rubicon.ObjectBinding.Web.UnitTests.UI.Controls
+{
+
+[TestFixture]
+public class BocListTest: BocTest
+{
+  private BocListMock _bocList;
+
+  public BocListTest()
+  {
+  }
+
+  
+  [SetUp]
+  public override void SetUp()
+  {
+    base.SetUp();
+    _bocList = new BocListMock();
+    _bocList.ID = "BocList";
+    NamingContainer.Controls.Add (_bocList);
+  }
+
+
+  [Test]
+  public void GetTrackedClientIDsInReadOnlyMode()
+  {
+    _bocList.ReadOnly = NaBoolean.True;
+    string[] actual = _bocList.GetTrackedClientIDs();
+    Assert.IsNotNull (actual);
+    Assert.AreEqual (0, actual.Length);
+  }
+
+  [Test]
+  public void GetTrackedClientIDsInEditMode()
+  {
+    _bocList.ReadOnly = NaBoolean.False;
+    string[] actual = _bocList.GetTrackedClientIDs();
+    Assert.IsNotNull (actual);
+    Assert.Ignore("Not implemented.");
+  }
+}
+
+}
