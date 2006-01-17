971a6dea787104225c04a03db8ea4346419f2b50
Michael Ketting <michael.ketting@rubicon.eu>
2006-01-17 10:51:00 +0000

    Label: "1.3.30.0" User: Build        Date: 13.01.06   Time: 16:43 Labeled

---
+++ ObjectBinding/Web.UnitTests/Domain/TypeWithEnum.cs
@@ -0,0 +1,27 @@
+using System;
+using Rubicon.NullableValueTypes;
+using Rubicon.ObjectBinding;
+using Rubicon.ObjectBinding.Reflection;
+
+namespace Rubicon.ObjectBinding.Web.UnitTests.Domain
+{
+
+public class TypeWithEnum: ReflectionBusinessObject
+{
+  private TestEnum _enumValue;
+
+  public TestEnum EnumValue
+  {
+    get { return _enumValue; }
+    set { _enumValue = value; }
+  }
+}
+
+public enum TestEnum
+{
+  First,
+  Second,
+  Third
+}
+
+}
