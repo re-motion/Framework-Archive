1c15b488adaa3d83d594034ea2a91264ba2ccbbe
Michael Ketting <michael.ketting@rubicon.eu>
2006-01-18 15:25:00 +0000

    Label: "1.3.30.0" User: Build        Date: 13.01.06   Time: 16:43 Labeled

--- ObjectBinding/Web.Test/Design/DesignTestBooleanValueForm.aspx.cs
+++ ObjectBinding/Web.Test/Design/DesignTestBooleanValueForm.aspx.cs
@@ -96,7 +96,7 @@ public class DesignTestBooleanValueForm : DesignTestWxeBasePage
 	private void InitializeComponent()
 	{    
     this.EnableAbort = Rubicon.NullableValueTypes.NaBooleanEnum.False;
-    this.EnableAbortConfirmation = Rubicon.NullableValueTypes.NaBooleanEnum.True;
+    this.ShowAbortConfirmation = Rubicon.Web.UI.ShowAbortConfirmation.Always;
     this.Load += new System.EventHandler(this.Page_Load);
 
   }
