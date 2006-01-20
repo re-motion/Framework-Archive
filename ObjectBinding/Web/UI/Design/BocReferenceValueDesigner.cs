a31b5bda40b2e1d6da251142c219129cd9608e30
Michael Ketting <michael.ketting@rubicon.eu>
2006-01-20 08:21:00 +0000

    Label: "1.3.31.0" User: Build        Date: 19.01.06   Time: 13:51 Labeled

--- ObjectBinding/Web/UI/Design/BocReferenceValueDesigner.cs
+++ ObjectBinding/Web/UI/Design/BocReferenceValueDesigner.cs
@@ -1,7 +1,7 @@
 using System;
 using System.ComponentModel;
 using System.Web.UI.Design;
-using Rubicon.ObjectBinding.Web.Controls;
+using Rubicon.ObjectBinding.Web.UI.Controls;
 using Rubicon.Web.UI.Design;
 
 namespace Rubicon.ObjectBinding.Web.Design
