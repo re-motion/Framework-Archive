a0cad8d8aeb2e9f87fd508db0122545e371b23be
Michael Ketting <michael.ketting@rubicon.eu>
2006-01-20 08:21:00 +0000

    Label: "1.3.31.0" User: Build        Date: 19.01.06   Time: 13:51 Labeled

--- ObjectBinding/Web/UI/Design/PropertyPathBindingCollectionEditor.cs
+++ ObjectBinding/Web/UI/Design/PropertyPathBindingCollectionEditor.cs
@@ -1,7 +1,7 @@
 using System;
 using System.ComponentModel;
 using System.ComponentModel.Design;
-using Rubicon.ObjectBinding.Web.Controls;
+using Rubicon.ObjectBinding.Web.UI.Controls;
 using Rubicon.Web.UI.Design;
 
 namespace Rubicon.ObjectBinding.Web.Design
