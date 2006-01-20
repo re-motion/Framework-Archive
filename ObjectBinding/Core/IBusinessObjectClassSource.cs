4be31801f908a9e98cdae165ad6344c9202af8ef
Michael Ketting <michael.ketting@rubicon.eu>
2006-01-20 08:21:00 +0000

    Label: "1.3.31.0" User: Build        Date: 19.01.06   Time: 13:51 Labeled

--- ObjectBinding/Core/IBusinessObjectClassSource.cs
+++ ObjectBinding/Core/IBusinessObjectClassSource.cs
@@ -14,8 +14,8 @@ namespace Rubicon.ObjectBinding
 ///     to query the <see cref="IBusinessObjectClass"/> of an <see cref="IBusinessObjectReferenceProperty"/>
 ///     or an <see cref="IBusinessObjectDataSource"/>, respectively.
 ///   </para><para>
-///     Implemented by <see cref="T:Rubicon.ObjectBinding.Web.Controls.BocSimpleColumnDefinition"/> 
-///     and <see cref="T:Rubicon.ObjectBinding.Web.Controls.PropertyPathBinding"/>.
+///     Implemented by <see cref="T:Rubicon.ObjectBinding.Web.UI.Controls.BocSimpleColumnDefinition"/> 
+///     and <see cref="T:Rubicon.ObjectBinding.Web.UI.Controls.PropertyPathBinding"/>.
 ///   </para>
 /// </remarks>
 public interface IBusinessObjectClassSource
