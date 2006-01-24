08a4c51ceb492e61d65d796d7718be91abd12e6b
Michael Ketting <michael.ketting@rubicon.eu>
2006-01-24 13:56:00 +0000

    Label: "1.3.32.0" User: Build        Date: 20.01.06   Time: 12:27 Labeled

--- ObjectBinding/Web.UnitTests/UI/Controls/BocListTest.cs
+++ ObjectBinding/Web.UnitTests/UI/Controls/BocListTest.cs
@@ -1,12 +1,14 @@
 using System;
 using NUnit.Framework;
 using Rubicon.Development.UnitTesting;
-using Rubicon.Web.UI;
 using Rubicon.NullableValueTypes;
-using Rubicon.Web.Configuration;
-using Rubicon.Web.UnitTests.Configuration;
+using Rubicon.ObjectBinding;
 using Rubicon.ObjectBinding.Web.UI.Controls;
+using Rubicon.ObjectBinding.Web.UnitTests.Domain;
+using Rubicon.Web.Configuration;
+using Rubicon.Web.UI;
 using Rubicon.Web.UI.Controls;
+using Rubicon.Web.UnitTests.Configuration;
 using Rubicon.Web.Utilities;
 
 namespace Rubicon.ObjectBinding.Web.UnitTests.UI.Controls
@@ -16,6 +18,9 @@ namespace Rubicon.ObjectBinding.Web.UnitTests.UI.Controls
 public class BocListTest: BocTest
 {
   private BocListMock _bocList;
+  private TypeWithReference _businessObject;
+  private BusinessObjectReferenceDataSource _dataSource;
+  private IBusinessObjectReferenceProperty _propertyReferenceList;
 
   public BocListTest()
   {
@@ -29,6 +34,13 @@ public class BocListTest: BocTest
     _bocList = new BocListMock();
     _bocList.ID = "BocList";
     NamingContainer.Controls.Add (_bocList);
+  
+    _businessObject = new TypeWithReference();
+    
+    _propertyReferenceList = (IBusinessObjectReferenceProperty) _businessObject.GetBusinessObjectProperty ("ReferenceList");
+    
+    _dataSource = new BusinessObjectReferenceDataSource();
+    _dataSource.BusinessObject = _businessObject;
   }
 
 
@@ -51,6 +63,7 @@ public class BocListTest: BocTest
     Assert.AreEqual (0, actual.Length);
   }
 
+  
   // EditDetailsMode currently not testable
   //  [Test]
   //  public void GetTrackedClientIDsInEditMode()
@@ -60,6 +73,82 @@ public class BocListTest: BocTest
   //    Assert.IsNotNull (actual);
   //    Assert.Ignore("Not implemented.");
   //  }
+
+  [Test]
+  public void SetValueToList()
+  {
+    IBusinessObject[] list = new IBusinessObject[] {new TypeWithString()};
+    _bocList.IsDirty = false;
+    _bocList.Value = list;
+    Assert.AreEqual (list, _bocList.Value);
+    Assert.IsTrue (_bocList.IsDirty);
+  }
+    
+  [Test]
+  public void SetValueToNull()
+  {
+    _bocList.IsDirty = false;
+    _bocList.Value = null;
+    Assert.AreEqual (null, _bocList.Value);
+    Assert.IsTrue (_bocList.IsDirty);
+  }
+ 
+
+  [Test]
+  public void IsDirtyAfterLoadValueBoundAndInterimTrueWithList()
+  {
+    _businessObject.ReferenceList = new TypeWithReference[] {new TypeWithReference(), new TypeWithReference()};
+    _bocList.DataSource = _dataSource;
+    _bocList.Property = _propertyReferenceList;
+    _bocList.Value = null;
+    _bocList.IsDirty = true;
+
+    _bocList.LoadValue (true);
+    Assert.AreEqual (_businessObject.ReferenceList, _bocList.Value);
+    Assert.IsFalse (_bocList.IsDirty);
+  }
+
+  [Test]
+  public void IsDirtyAfterLoadValueBoundAndInterimTrueWithNull()
+  {
+    _businessObject.ReferenceList = null;
+    _bocList.DataSource = _dataSource;
+    _bocList.Property = _propertyReferenceList;
+    _bocList.Value = new TypeWithReference[0];
+    _bocList.IsDirty = true;
+
+    _bocList.LoadValue (true);
+    Assert.AreEqual (_businessObject.ReferenceList, _bocList.Value);
+    Assert.IsFalse (_bocList.IsDirty);
+  }   
+
+  [Test]
+  public void IsDirtyAfterLoadValueBoundAndInterimFalseWithList()
+  {
+    _businessObject.ReferenceList = new TypeWithReference[] {new TypeWithReference(), new TypeWithReference()};
+    _bocList.DataSource = _dataSource;
+    _bocList.Property = _propertyReferenceList;
+    _bocList.Value = null;
+    _bocList.IsDirty = true;
+
+    _bocList.LoadValue (false);
+    Assert.AreEqual (_businessObject.ReferenceList, _bocList.Value);
+    Assert.IsFalse (_bocList.IsDirty);
+  }
+
+  [Test]
+  public void IsDirtyAfterLoadValueBoundAndInterimFalseWithNull()
+  {
+    _businessObject.ReferenceList = null;
+    _bocList.DataSource = _dataSource;
+    _bocList.Property = _propertyReferenceList;
+    _bocList.Value = new TypeWithReference[0];
+    _bocList.IsDirty = true;
+
+    _bocList.LoadValue (false);
+    Assert.AreEqual (_businessObject.ReferenceList, _bocList.Value);
+    Assert.IsFalse (_bocList.IsDirty);
+  }   
 }
 
 }
