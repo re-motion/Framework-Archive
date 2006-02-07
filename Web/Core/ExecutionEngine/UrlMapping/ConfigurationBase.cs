0d6c57cd0eb8913d314780cdd4f0bb5c625a39ba
Michael Ketting <michael.ketting@rubicon.eu>
2006-02-07 09:56:00 +0000

    Label: "1.4.8.0" User: Build        Date:  6.02.06   Time: 18:25 Labeled

--- Web/Core/ExecutionEngine/UrlMapping/ConfigurationBase.cs
+++ Web/Core/ExecutionEngine/UrlMapping/ConfigurationBase.cs
@@ -32,7 +32,7 @@ public class ConfigurationBase
   // methods and properties
 
   /// <summary> Gets the application name that is specified in the XML configuration file.  </summary>
-  [XmlAttribute ("applicationName")]
+  [XmlAttribute ("application")]
   public string ApplicationName
   {
     get { return _applicationName; }
