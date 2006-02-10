60624fbba66f81c166a4421c816270ab7507e1f6
Michael Ketting <michael.ketting@rubicon.eu>
2006-02-10 08:59:00 +0000

    Label: "1.4.9.0" User: Build        Date:  7.02.06   Time: 13:14 Labeled

--- ObjectBinding/Web.Test/StartForm.aspx.cs
+++ ObjectBinding/Web.Test/StartForm.aspx.cs
@@ -26,6 +26,7 @@ public class StartForm : System.Web.UI.Page
   protected Rubicon.Web.UI.Controls.WebButton Submit1Button;
   protected Rubicon.Web.UI.Controls.WebButton Button2Button;
   protected System.Web.UI.WebControls.TextBox TextBox1;
+  protected Rubicon.ObjectBinding.Web.UI.Controls.BocTextValue BocTextValue1;
   protected Rubicon.Web.UI.Controls.HtmlHeadContents HtmlHeadContents;
 
 	#region Web Form Designer generated code
