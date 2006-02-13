a40a719e4e39a3d9bedb9d5dd7e1229bf932bc54
Michael Ketting <michael.ketting@rubicon.eu>
2006-02-13 09:32:00 +0000

    Label: "1.4.11.0" User: Build        Date: 13.02.06   Time:  8:57 Labeled

--- Web/UnitTests/ExecutionEngine/UrlMapping/UrlMappedFunctions.cs
+++ Web/UnitTests/ExecutionEngine/UrlMapping/UrlMappedFunctions.cs
@@ -12,4 +12,8 @@ public class SecondMappedFunction: WxeFunction
 {
 }
 
+public class UnmappedFunction: WxeFunction
+{
+}
+
 }
--- Web/UnitTests/ExecutionEngine/UrlMapping/UrlMappingConfigurationTest.cs
+++ Web/UnitTests/ExecutionEngine/UrlMapping/UrlMappingConfigurationTest.cs
@@ -19,7 +19,7 @@ namespace Rubicon.Web.UnitTests.ExecutionEngine.UrlMapping
 {
 
 [TestFixture]
-public class UrlMappingTest
+public class UrlMappingConfigurationTest
 {
   [SetUp]
   public virtual void SetUp()
@@ -40,15 +40,23 @@ public class UrlMappingTest
     Assert.IsNotNull (mapping, "Mapping is null.");
     
     Assert.IsNotNull (mapping.Mappings, "Rules are null.");
-    Assert.AreEqual (2, mapping.Mappings.Count);
+    Assert.AreEqual (3, mapping.Mappings.Count);
 
     Assert.IsNotNull (mapping.Mappings[0], "First rule is null.");
     Assert.IsNotNull (mapping.Mappings[1], "Second rule is null.");
+    Assert.IsNotNull (mapping.Mappings[2], "Thrid rule is null.");
     
+    Assert.AreEqual ("First", mapping.Mappings[0].ID);
     Assert.AreEqual (typeof (FirstMappedFunction), mapping.Mappings[0].FunctionType);
     Assert.AreEqual ("~/First.wxe", mapping.Mappings[0].Resource);
+    
+    Assert.AreEqual ("Second", mapping.Mappings[1].ID);
     Assert.AreEqual (typeof (SecondMappedFunction), mapping.Mappings[1].FunctionType);
     Assert.AreEqual ("~/Second.wxe", mapping.Mappings[1].Resource);
+
+    Assert.IsNull (mapping.Mappings[2].ID);
+    Assert.AreEqual (typeof (FirstMappedFunction), mapping.Mappings[2].FunctionType);
+    Assert.AreEqual ("~/Primary.wxe", mapping.Mappings[2].Resource);
   }
 
   [Test]
@@ -76,15 +84,23 @@ public class UrlMappingTest
     Assert.IsNotNull (mapping, "Mapping is null.");
     
     Assert.IsNotNull (mapping.Mappings, "Rules are null.");
-    Assert.AreEqual (2, mapping.Mappings.Count);
+    Assert.AreEqual (3, mapping.Mappings.Count);
 
     Assert.IsNotNull (mapping.Mappings[0], "First rule is null.");
     Assert.IsNotNull (mapping.Mappings[1], "Second rule is null.");
+    Assert.IsNotNull (mapping.Mappings[2], "Thrid rule is null.");
     
+    Assert.AreEqual ("First", mapping.Mappings[0].ID);
     Assert.AreEqual (typeof (FirstMappedFunction), mapping.Mappings[0].FunctionType);
     Assert.AreEqual ("~/First.wxe", mapping.Mappings[0].Resource);
+
+    Assert.AreEqual ("Second", mapping.Mappings[1].ID);
     Assert.AreEqual (typeof (SecondMappedFunction), mapping.Mappings[1].FunctionType);
     Assert.AreEqual ("~/Second.wxe", mapping.Mappings[1].Resource);
+
+    Assert.IsNull (mapping.Mappings[2].ID);
+    Assert.AreEqual (typeof (FirstMappedFunction), mapping.Mappings[2].FunctionType);
+    Assert.AreEqual ("~/Primary.wxe", mapping.Mappings[2].Resource);
   }
 
   [Test]
@@ -97,6 +113,57 @@ public class UrlMappingTest
     
     Assert.IsNotNull (mapping.Mappings, "Rules are null.");
     Assert.AreEqual (0, mapping.Mappings.Count);
+  }
+
+  [Test]
+  public void FindByFunctionType()
+  {
+    WebConfigurationMock.Current = WebConfigurationFactory.GetExecutionEngineUrlMapping();
+    UrlMappingCollection mappings = UrlMappingConfiguration.Current.Mappings;
+
+    UrlMappingEntry entry = mappings[0];
+    Assert.AreSame (mappings[0], mappings.Find (entry.FunctionType), "Could not find {0}.", entry.FunctionType.FullName);
+    
+    entry = mappings[1];
+    Assert.AreSame (mappings[1], mappings.Find (entry.FunctionType), "Could not find {0}.", entry.FunctionType.FullName);
+
+    entry = mappings[2];
+    Assert.AreSame (mappings[0], mappings.Find (entry.FunctionType), "Could not find {0}.", entry.FunctionType.FullName);
+
+    Assert.IsNull (mappings.Find (typeof (UnmappedFunction)), "Found mapping for unmapped function.");
+  }
+
+  [Test]
+  public void FindByResource()
+  {
+    WebConfigurationMock.Current = WebConfigurationFactory.GetExecutionEngineUrlMapping();
+    UrlMappingCollection mappings = UrlMappingConfiguration.Current.Mappings;
+    
+    UrlMappingEntry entry = mappings[0];
+    Assert.AreSame (mappings[0], mappings.Find (entry.Resource), "Could not find {0}.", entry.Resource);
+    
+    entry = mappings[1];
+    Assert.AreSame (mappings[1], mappings.Find (entry.Resource), "Could not find {0}.", entry.Resource);
+
+    entry = mappings[2];
+    Assert.AreSame (mappings[2], mappings.Find (entry.Resource), "Could not find {0}.", entry.Resource);
+
+    Assert.IsNull (mappings.Find ("~/unmapped.wxe"), "Found mapping for unmapped resource.");
+  }
+
+  [Test]
+  public void FindByID()
+  {
+    WebConfigurationMock.Current = WebConfigurationFactory.GetExecutionEngineUrlMapping();
+    UrlMappingCollection mappings = UrlMappingConfiguration.Current.Mappings;
+    
+    UrlMappingEntry entry = mappings[0];
+    Assert.AreSame (mappings[0], mappings.FindByID (entry.ID), "Could not find {0}.", entry.ID);
+    
+    entry = mappings[1];
+    Assert.AreSame (mappings[1], mappings.FindByID (entry.ID), "Could not find {0}.", entry.Resource);
+
+    Assert.IsNull (mappings.FindByID ("unknown"), "Found mapping for unknown id.");
   }
 }
 
