812e9d591674773e5b6de3a8c9ab03e1932c6e3f
Michael Ketting <michael.ketting@rubicon.eu>
2006-02-13 12:24:00 +0000

    Label: "1.4.11.0" User: Build        Date: 13.02.06   Time:  8:57 Labeled

--- ObjectBinding/Web.Test/TestTabbedForm.aspx.cs
+++ ObjectBinding/Web.Test/TestTabbedForm.aspx.cs
@@ -160,7 +160,7 @@ public class TestTabbedForm : TestWxeBasePage
     base.OnInit(e);
 
     this.EnableAbort = Rubicon.NullableValueTypes.NaBooleanEnum.True;
-    this.ShowAbortConfirmation = Rubicon.Web.UI.ShowAbortConfirmation.Always;
+    this.ShowAbortConfirmation = Rubicon.Web.UI.ShowAbortConfirmation.OnlyIfDirty;
 	}
 	#region Web Form Designer generated code
 
