cc557ea3d3e43930aa98ff0cbb7b3aa0584230ba
Michael Ketting <michael.ketting@rubicon.eu>
2006-02-15 07:28:00 +0000

    Label: "1.4.15.0" User: Build        Date: 14.02.06   Time: 19:10 Labeled

---
+++ Web/Core/UI/Controls/ISupportsPostLoadControl.cs
@@ -0,0 +1,18 @@
+using System;
+using Rubicon.Web.UI.Controls;
+
+namespace Rubicon.Web.UI.Controls
+{
+
+/// <summary>
+///   Allows controls to receive a call after the <see cref="Control.Load"/> event.
+/// </summary>
+public interface ISupportsPostLoadControl: IControl
+{
+  /// <summary>
+  ///   This method may be called after the <see cref="Control.Load"/> event.
+  /// </summary>
+  void OnPostLoad();
+}
+
+}
