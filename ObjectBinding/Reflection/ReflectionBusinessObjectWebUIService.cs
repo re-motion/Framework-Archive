f140244cd1fbc25c8910a0687cf8dfe1438cb7c5
Michael Ketting <michael.ketting@rubicon.eu>
2006-02-16 14:25:00 +0000

    Label: "1.4.16.0" User: Build        Date: 15.02.06   Time: 16:41 Labeled

--- ObjectBinding/Reflection/ReflectionBusinessObjectWebUIService.cs
+++ ObjectBinding/Reflection/ReflectionBusinessObjectWebUIService.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Web;
 using System.Web.UI.WebControls;
 using Rubicon.ObjectBinding.Web;
 using Rubicon.Web.UI.Controls;
+using Rubicon.Web.Utilities;
 
 namespace Rubicon.ObjectBinding.Reflection
 {
@@ -11,9 +13,15 @@ public class ReflectionBusinessObjectWebUIService: IBusinessObjectWebUIService
   public IconInfo GetIcon (IBusinessObject obj)
   {
     if (obj == null)
-      return new IconInfo ("Images/NullIcon.gif", Unit.Pixel (16), Unit.Pixel (16));
+    {
+      string url = "~/Images/NullIcon.gif";
+      return new IconInfo (url, Unit.Pixel (16), Unit.Pixel (16));
+    }
     else
-      return new IconInfo ("Images/" + obj.BusinessObjectClass.Identifier + ".gif", Unit.Pixel (16), Unit.Pixel (16));
+    {
+      string url = "~/Images/" + obj.BusinessObjectClass.Identifier + ".gif";
+      return new IconInfo (url, Unit.Pixel (16), Unit.Pixel (16));
+    }
   }
 
   public string GetToolTip (IBusinessObject obj)
