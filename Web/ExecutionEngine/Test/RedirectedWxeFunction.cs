3338391bd3bebce818b2e7ad246dbc3e7eca92c0
Michael Ketting <michael.ketting@rubicon.eu>
2006-02-20 17:26:00 +0000

    Label: "1.4.18.0" User: Build        Date: 20.02.06   Time:  9:43 Labeled

---
+++ Web/ExecutionEngine/Test/RedirectedWxeFunction.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Web;
+using System.Web.SessionState;
+using Rubicon.Web.ExecutionEngine;
+using Rubicon.Web.Utilities;
+
+namespace Rubicon.PageTransition
+{
+
+public class RedirectedSubWxeFunction: WxeFunction
+{
+  public RedirectedSubWxeFunction ()
+  {
+  }
+
+  void Step1 (WxeContext context)
+  {
+    PageUtility.Redirect (context.HttpContext.Response, "Start.aspx?Redirected");
+  }
+}
+ 
+public class RedirectedWxeFunction: WxeFunction
+{
+  public RedirectedWxeFunction ()
+  {
+  }
+
+  public RedirectedWxeFunction (params object[] args)
+    : base (args)
+  {
+  }
+
+  // steps
+
+  WxeStep Step1 = new RedirectedSubWxeFunction();
+
+  WxeStep Step2 = new WxePageStep ("SessionForm.aspx");
+}
+
+}
