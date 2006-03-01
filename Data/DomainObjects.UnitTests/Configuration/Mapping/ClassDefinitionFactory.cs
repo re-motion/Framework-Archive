c1fccfe4b9049bf7e085b74d7efd6f271ac32940
Markus Leimhofer <markus.leimhofer@rubicon.eu>
2006-03-01 16:02:00 +0000

    Label: "1.4.24.0" User: Build        Date:  1.03.06   Time: 13:45 Labeled

---
+++ Data/DomainObjects.UnitTests/Configuration/Mapping/ClassDefinitionFactory.cs
@@ -0,0 +1,51 @@
+using System;
+
+using Rubicon.Data.DomainObjects.Mapping;
+using Rubicon.Data.DomainObjects.UnitTests.TestDomain;
+
+namespace Rubicon.Data.DomainObjects.UnitTests.Configuration.Mapping
+{
+public sealed class ClassDefinitionFactory
+{
+  // types
+
+  // static members and constants
+
+  public static ClassDefinition CreateOrderDefinition ()
+  {
+    return new ClassDefinition ("Order", "OrderTable", "StorageProviderID", typeof (Order));
+  }
+
+  public static ClassDefinition CreateOrderDefinitionWithResolvedCustomerProperty ()
+  {
+    ClassDefinition classDefinition = CreateOrderDefinition ();
+    classDefinition.MyPropertyDefinitions.Add (PropertyDefinitionFactory.CreateCustomerDefinition ());
+
+    return classDefinition;
+  }
+
+  public static ClassDefinition CreateWithUnresolvedClassType ()
+  {
+    return new ClassDefinition ("ClassID", "Entity", "StorageProviderID", "UnresolvedTypeName", false);
+  }
+
+  public static ClassDefinition CreateWithUnresolvedRelationProperty ()
+  {
+    ClassDefinition classDefinition = CreateWithUnresolvedClassType ();
+    classDefinition.MyPropertyDefinitions.Add (PropertyDefinitionFactory.CreateWithUnresolvedRelationPropertyType ());
+
+    return classDefinition;
+  }
+
+  // member fields
+
+  // construction and disposing
+
+  private ClassDefinitionFactory ()
+  {
+  }
+
+  // methods and properties
+
+}
+}
