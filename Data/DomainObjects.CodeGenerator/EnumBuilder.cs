56a3d62926b7cd42bda9898b011987be5d35e39d
Stefan Wenig <stefan.wenig@rubicon.eu>
2006-03-01 11:21:00 +0000

    Label: "1.4.23.0" User: Build        Date: 25.02.06   Time: 20:54 Labeled

--- Data/DomainObjects.CodeGenerator/EnumBuilder.cs
+++ Data/DomainObjects.CodeGenerator/EnumBuilder.cs
@@ -1,41 +1,48 @@
 using System;
-
+using System.IO;
 using Rubicon.Utilities;
 
 namespace Rubicon.Data.DomainObjects.CodeGenerator
 {
-public class EnumBuilder : CodeFileBuilder
+
+public class EnumBuilder
 {
-  // types
-
-  // static members and constants
-
-  // member fields
-  private Type _type;
-
-  // construction and disposing
-
-  public EnumBuilder (string fileName, Type type) : base (fileName)
+  private class Builder: CodeFileBuilder
   {
+    public Builder (TextWriter writer) : base (writer)
+    {
+    }
+
+    public void Build (Type type)
+    {
+      BeginNamespace (type.Namespace);
+      WriteEnum (type.Name);
+      EndNamespace ();
+      FinishFile ();
+    }
+  }
+
+  public static void Build (string filename, Type type)
+  {
+    ArgumentUtility.CheckNotNullOrEmpty ("filename", filename);
+
+    using (TextWriter writer = new StreamWriter (filename))
+    {
+      Build (writer, type);
+    }
+  }
+
+  public static void Build (TextWriter writer, Type type)
+  {
+    ArgumentUtility.CheckNotNull ("writer", writer);
     ArgumentUtility.CheckNotNull ("type", type);
 
-    _type = type;
+    Builder builder = new Builder (writer);
+    builder.Build (type);
   }
 
-  // methods and properties
-
-  public override void Build()
+  private EnumBuilder()
   {
-    OpenFile ();
-
-    BeginNamespace (_type.Namespace);
-
-    WriteEnum (_type.Name);
-
-    EndNamespace ();
-
-    CloseFile ();
   }
-
 }
 }
