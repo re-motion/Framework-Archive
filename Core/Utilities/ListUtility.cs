25d72e652499568750d4b9dc741f73629aaa2e3b
Michael Ketting <michael.ketting@rubicon.eu>
2006-03-08 09:30:00 +0000

    Label: "1.4.25.0" User: Build        Date:  7.03.06   Time: 16:25 Labeled

--- Core/Utilities/ListUtility.cs
+++ Core/Utilities/ListUtility.cs
@@ -209,6 +209,65 @@ public sealed class ListUtility
       return list;
     }
   }
+
+  public static int IndexOf (IList list, object value)
+  {
+    ArgumentUtility.CheckNotNull ("list", list);
+    try
+    {
+      return list.IndexOf (value);
+    }
+    catch (NotSupportedException)
+    {
+      return IndexOfInternal (list, value);
+    }
+  }
+
+  public static int[] IndicesOf (IList list, IList values)
+  {
+    return IndicesOf (list, values, true);
+  }
+
+  public static int[] IndicesOf (IList list, IList values, bool includeMissing)
+  {
+    ArgumentUtility.CheckNotNull ("list", list);
+    ArgumentUtility.CheckNotNull ("values", values);
+
+    ArrayList indices = new ArrayList (values.Count);
+    bool isIndexOfSupported = true;
+    for (int i = 0; i < values.Count; i++)
+    {
+      int index = -1;
+      if (isIndexOfSupported)
+      {
+        try
+        {
+          index = list.IndexOf (values[i]);
+        }
+        catch (NotSupportedException)
+        {
+          isIndexOfSupported = false;
+        }
+      }
+
+      if (! isIndexOfSupported)
+        index = IndexOfInternal (list, values[i]);
+
+      if (index > -1 || includeMissing)
+        indices.Add (index);
+    }
+    return (int[]) indices.ToArray (typeof (int));
+  }
+
+  private static int IndexOfInternal (IList list, object value)
+  {
+    for (int i = 0; i < list.Count; i++)
+    {
+      if (list[i] == value)
+        return i;
+    }
+    return -1;
+  }
 }
 
 }
