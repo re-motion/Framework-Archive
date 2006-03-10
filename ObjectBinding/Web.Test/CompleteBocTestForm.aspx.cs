b2540b00f7f58ad96c2aaf613868949701dcb024
Michael Ketting <michael.ketting@rubicon.eu>
2006-03-10 18:12:00 +0000

    Label: "1.4.33.0" User: Build        Date:  9.03.06   Time:  7:51 Labeled

--- ObjectBinding/Web.Test/CompleteBocTestForm.aspx.cs
+++ ObjectBinding/Web.Test/CompleteBocTestForm.aspx.cs
@@ -125,7 +125,7 @@ namespace OBWTest
     menuItem.ItemID = "Copy";
     menuItem.Text = "Copy";
     menuItem.Category = "Edit";
-    menuItem.Icon = "Images/CopyItem.gif";
+    menuItem.Icon.Url = "Images/CopyItem.gif";
     menuItem.RequiredSelection = RequiredSelection.OneOrMore;
     menuItem.Command.Type = CommandType.Event;
     ReferenceField.OptionsMenuItems.Add (menuItem);
@@ -149,8 +149,8 @@ namespace OBWTest
     menuItem.ItemID = "Delete";
     menuItem.Text = "Delete";
     menuItem.Category = "Edit";
-    menuItem.Icon = "Images/DeleteItem.gif";
-    menuItem.DisabledIcon = "Images/DeleteItemDisabled.gif";
+    menuItem.Icon.Url = "Images/DeleteItem.gif";
+    menuItem.DisabledIcon.Url = "Images/DeleteItemDisabled.gif";
     menuItem.RequiredSelection = RequiredSelection.OneOrMore;
     menuItem.Style = WebMenuItemStyle.Icon;
     menuItem.Command.Type = CommandType.Event;
--- ObjectBinding/Web.Test/CompleteBocTestUserControl.ascx.cs
+++ ObjectBinding/Web.Test/CompleteBocTestUserControl.ascx.cs
@@ -117,7 +117,7 @@ public class CompleteBocUserControl :
     menuItem.ItemID = "Copy";
     menuItem.Text = "Copy";
     menuItem.Category = "Edit";
-    menuItem.Icon = "Images/CopyItem.gif";
+    menuItem.Icon.Url = "Images/CopyItem.gif";
     menuItem.RequiredSelection = RequiredSelection.OneOrMore;
     menuItem.Command.Type = CommandType.Event;
     ReferenceField.OptionsMenuItems.Add (menuItem);
@@ -141,8 +141,8 @@ public class CompleteBocUserControl :
     menuItem.ItemID = "Delete";
     menuItem.Text = "Delete";
     menuItem.Category = "Edit";
-    menuItem.Icon = "Images/DeleteItem.gif";
-    menuItem.DisabledIcon = "Images/DeleteItemDisabled.gif";
+    menuItem.Icon.Url = "Images/DeleteItem.gif";
+    menuItem.DisabledIcon.Url = "Images/DeleteItemDisabled.gif";
     menuItem.RequiredSelection = RequiredSelection.OneOrMore;
     menuItem.Style = WebMenuItemStyle.Icon;
     menuItem.Command.Type = CommandType.Event;
