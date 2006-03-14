21b9995cadfcdd7265965cbffba84e4515f2092c
Michael Ketting <michael.ketting@rubicon.eu>
2006-03-14 16:11:00 +0000

    Label: "1.4.34.0" User: Build        Date: 14.03.06   Time: 12:26 Labeled

--- ObjectBinding/Web/UI/Controls/BocDateTimeValue.cs
+++ ObjectBinding/Web/UI/Controls/BocDateTimeValue.cs
@@ -158,7 +158,10 @@ public class BocDateTimeValue: BusinessObjectBoundModifiableWebControl, IPostBac
   /// <summary> Invokes the <see cref="LoadPostData"/> method. </summary>
   bool IPostBackDataHandler.LoadPostData (string postDataKey, NameValueCollection postCollection)
   {
-    return LoadPostData (postDataKey, postCollection);
+    if (HasBeenRenderedInPreviousLifecycle)
+      return LoadPostData (postDataKey, postCollection);
+    else
+      return false;
   }
 
   /// <summary> Invokes the <see cref="RaisePostDataChangedEvent"/> method. </summary>
