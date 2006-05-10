eca7b62bafc560de29247b78377ebc7639d42837
Michael Ketting <michael.ketting@rubicon.eu>
2006-05-10 06:25:55 +0000

    Added XML Comments

--- Core/Data/PermanentGuidAttribute.cs
+++ Core/Data/PermanentGuidAttribute.cs
@@ -12,11 +12,18 @@ namespace Rubicon.Data
   {
     private readonly Guid _value;
 
+    /// <summary>
+    ///   Initializes a new instance of the <see cref="PermanentGuidAttribute"/> class.
+    /// </summary>
+    /// <param name="value"> The <see cref="String"/> representation of a <see cref="Guid"/>. </param>
     public PermanentGuidAttribute (string value)
     {
       _value = new Guid (value);
     }
 
+    /// <summary>
+    ///   Gets the <see cref="Guid"/> supplied during initialization.
+    /// </summary>
     public Guid Value
     {
       get { return _value; }
