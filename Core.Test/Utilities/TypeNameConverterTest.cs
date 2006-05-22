d65385faf432ee502c9827c0a165039fb74f777d
Michael Ketting <michael.ketting@rubicon.eu>
2006-05-22 16:09:40 +0000

    New: Rubicon.Utilities.TypeNameConverter for converting (abbreviated) type names into dotNet types.

---
+++ Core.Test/Utilities/TypeNameConverterTest.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using NUnit.Framework;
+
+using Rubicon.Utilities;
+
+namespace Rubicon.Core.UnitTests.Utilities
+{
+  [TestFixture]
+  public class TypeNameConverterTest
+  {
+    // types
+
+    // static members
+
+    // member fields
+
+    TypeNameConverter _converter;
+
+    // construction and disposing
+
+    public TypeNameConverterTest ()
+    {
+    }
+
+    // methods and properties
+
+    [SetUp]
+    public void SetUp ()
+    {
+      _converter = new TypeNameConverter ();
+    }
+
+    [Test]
+    public void CanConvertToString ()
+    {
+      Assert.IsTrue (_converter.CanConvertTo (typeof (string)));
+    }
+
+    [Test]
+    public void CanConvertFromString ()
+    {
+      Assert.IsTrue (_converter.CanConvertFrom (typeof (string)));
+    }
+
+    [Test]
+    public void ConvertToString ()
+    {
+      Type destinationType = typeof (string);
+
+      Assert.AreEqual ("", _converter.ConvertTo (null, null, null, destinationType));
+      StringAssert.StartsWith (
+          "Rubicon.Core.UnitTests.Utilities.TypeNameConverterTest, Rubicon.Core.UnitTests", 
+          (string) _converter.ConvertTo (null, null, typeof (TypeNameConverterTest), destinationType));
+    }
+
+    [Test]
+    public void ConvertFromString ()
+    {
+      Assert.AreEqual (null, _converter.ConvertFrom (null, null, ""));
+      Assert.AreEqual (
+          typeof (TypeNameConverterTest),
+          _converter.ConvertFrom (null, null, "Rubicon.Core.UnitTests.Utilities.TypeNameConverterTest, Rubicon.Core.UnitTests"));
+      Assert.AreEqual (
+          typeof (TypeNameConverterTest),
+          _converter.ConvertFrom (null, null, "Rubicon.Core.UnitTests::Utilities.TypeNameConverterTest"));
+    }
+  }
+}
