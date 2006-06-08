ddb53a6a017f2d6f1a4e07a40952e748bb829a42
Michael Ketting <michael.ketting@rubicon.eu>
2006-06-08 13:24:46 +0000

    Removed SchemaBase from Rubicon.Web since it was moved to Rubicon.Core.

--- Web/Core/ExecutionEngine/UrlMapping/UrlMappingSchema.cs
+++ Web/Core/ExecutionEngine/UrlMapping/UrlMappingSchema.cs
@@ -12,26 +12,6 @@ using Rubicon.Web.Configuration;
 namespace Rubicon.Web.ExecutionEngine.UrlMapping
 {
 
-public abstract class SchemaBase
-{
-  protected abstract string SchemaFile { get; }
-  public abstract string SchemaUri { get; }
-
-  /// <summary> Gets an <see cref="XmlReader"/> reader for the schema embedded in the assembly. </summary>
-  /// <exception cref="ApplicationException"> Thrown if the schema file could not be loaded. </exception>
-  public XmlReader GetSchemaReader ()
-  {
-    Assembly assembly = Assembly.GetExecutingAssembly();
-    Stream schemaStream = assembly.GetManifestResourceStream (GetType(), SchemaFile);
-    if (schemaStream == null)
-    {
-      throw new ApplicationException (string.Format (
-          "Error loading schema resource '{0}' from assembly '{1}'.", SchemaFile, assembly.FullName));
-    }
-    return new XmlTextReader (schemaStream);
-  }
-}
-
 public class UrlMappingSchema: SchemaBase
 {
   public UrlMappingSchema()
