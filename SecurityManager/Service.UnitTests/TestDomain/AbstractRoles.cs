1e60594014fb9ea1a111134d1f4b0ac6bfd4f326
Andreas Schlapsi <andreas.schlapsi@rubicon.eu>
2006-06-19 18:51:25 +0000

    Added missing test domain classes.

---
+++ SecurityManager/Service.UnitTests/TestDomain/AbstractRoles.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Rubicon.Security;
+
+namespace Rubicon.SecurityManager.Service.UnitTests.TestDomain
+{
+  [AbstractRole]
+  public enum ProjectRole
+  {
+    QualityManager,
+    Developer
+  }
+}
---
+++ SecurityManager/Service.UnitTests/TestDomain/SecurityStates.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Rubicon.Security;
+
+namespace Rubicon.SecurityManager.Service.UnitTests.TestDomain
+{
+  [SecurityState]
+  public enum OrderState
+  {
+    Received = 0,
+    Delivered
+  }
+
+  [SecurityState]
+  public enum PaymentState
+  {
+    None,
+    Paid
+  }
+}
