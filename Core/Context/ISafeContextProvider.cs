4e3535b53c096b93d950a9b94e29cbf6c1c595f6
Michael Ketting <michael.ketting@rubicon.eu>
2006-07-25 15:09:17 +0000

    Added ISafeContextProvider and SafeContext as placeholder.

---
+++ Core/Context/ISafeContextProvider.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Rubicon.Utilities;
+
+namespace Rubicon.Context
+{
+  // CallContextProvider
+  // HttpContextProvider
+  internal interface ISafeContextProvider
+  {
+  }
+}
---
+++ Core/Context/SafeContext.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Rubicon.Utilities;
+
+namespace Rubicon.Context
+{
+//  Irgendwos CurrentIrgendwos 
+//{
+//  get { return (Irgendwos) SafeContextStorage.GetValue ("Irgendwos"); }
+
+//  get { return SafeContextStorage.GetValue<Irgendwos> ("Irgendwos"); }
+//  get { return SafeContextStorage.GetValue<Irgendwos> (); }
+
+
+//  get { return SafeContextStorage.GetContextSingleton<Irgendwos> (); }
+
+
+//  get 
+//  {
+//    return SafeContextStorage.GetOrCreateValue<Irgendwos> (
+//      "Irgendwos",
+//      delegate() { return new Irgendwos(...); });
+//  }
+
+  internal class SafeContext
+  {
+    // types
+
+    // static members
+
+    // member fields
+
+    // construction and disposing
+
+    public SafeContext ()
+    {
+    }
+
+    // methods and properties
+  }
+}
