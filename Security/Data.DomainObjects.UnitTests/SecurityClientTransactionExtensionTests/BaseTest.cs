4a07ae87867a317949f2f083fc77090f41b355e7
Michael Ketting <michael.ketting@rubicon.eu>
2006-07-28 09:57:56 +0000

    Fixed build script.

--- Security/Data.DomainObjects.UnitTests/SecurityClientTransactionExtensionTests/BaseTest.cs
+++ Security/Data.DomainObjects.UnitTests/SecurityClientTransactionExtensionTests/BaseTest.cs
@@ -13,9 +13,9 @@ namespace Rubicon.Security.Data.DomainObjects.UnitTests.SecurityClientTransactio
     [TestFixtureSetUp]
     public virtual void TestFixtureSetUp ()
     {
-      MappingConfiguration.SetCurrent (new MappingConfiguration (@"Security.Data.DomainObjects.UnitTests.Mapping.xml"));
-      StorageProviderConfiguration.SetCurrent (new StorageProviderConfiguration (@"Security.Data.DomainObjects.UnitTests.StorageProviders.xml"));
-      QueryConfiguration.SetCurrent (new QueryConfiguration (@"Security.Data.DomainObjects.UnitTests.Queries.xml"));
+      MappingConfiguration.SetCurrent (new MappingConfiguration (@"Rubicon.Security.Data.DomainObjects.UnitTests.Mapping.xml"));
+      StorageProviderConfiguration.SetCurrent (new StorageProviderConfiguration (@"Rubicon.Security.Data.DomainObjects.UnitTests.StorageProviders.xml"));
+      QueryConfiguration.SetCurrent (new QueryConfiguration (@"Rubicon.Security.Data.DomainObjects.UnitTests.Queries.xml"));
     }
   }
 }
