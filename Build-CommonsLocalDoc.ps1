a85bbd6cf952ca9dd2202f71e4de9ddf124545f7
Michael Ketting <michael.ketting@rubicon.eu>
2007-02-05 10:32:29 +0000

    Fixed documentation for MSDN2 style.

--- Build-CommonsLocalDoc.ps1
+++ Build-CommonsLocalDoc.ps1
@@ -2,7 +2,7 @@ set-alias nant "C:\Program Files\NAnt\bin.net-2.0\nant.exe";
 
 nant "-f:Commons.build" "-D:solution.global-dir=\Development\global" "-t:net-2.0" "-nologo" `
     cleantemp `
-    resources doc-internal;
+    doc-internal;
 
 if ($LastExitCode -ne 0) 
 { 
--- localbuild.cmd
+++
@@ -1,3 +0,0 @@
-@set target=%1
-@if "%1"=="" set target=all
-nant local clean resources && nant -t:net-1.1 local setdebug %target% && nant local cleantemp 
