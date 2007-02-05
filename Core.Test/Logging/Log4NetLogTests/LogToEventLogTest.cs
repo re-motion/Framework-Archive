d8bbaa1b68046463e46cc8d06cdbed77dca1bff4
Michael Ketting <michael.ketting@rubicon.eu>
2007-02-05 12:36:31 +0000

    Added tests for log to event log.

---
+++ Core.Test/Logging/Log4NetLogTests/LogToEventLogTest.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Rubicon.Utilities;
+using NUnit.Framework;
+using System.Globalization;
+
+namespace Rubicon.Core.UnitTests.Logging.Log4NetLogTests
+{
+  [TestFixture]
+  public class LogToEventLogTest
+  {
+    [Test]
+    [Ignore]
+    public void LogToEventLog ()
+    {
+      Assert.Fail ("Not implemented");
+    }
+
+    [Test]
+    [Ignore]
+    public void Log_WithEventIDGreaterThan0xFFFF ()
+    {
+      Assert.Fail ("Not implemented");
+    }
+
+    [Test]
+    [Ignore]
+    public void Log_WithEventIDGreaterThanZero ()
+    {
+      Assert.Fail ("Not implemented");
+    }
+  }
+}
