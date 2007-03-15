18c741a8cbe59843c4f68a343e67a51c97f6273e
Michael Ketting <michael.ketting@rubicon.eu>
2007-03-15 12:45:10 +0000

    Added falg to build script to prevent updating the assembly info files.

--- Build-CommonsLocal.ps1
+++ Build-CommonsLocal.ps1
@@ -1,6 +1,7 @@
 set-alias nant "C:\Program Files\NAnt\bin.net-2.0\nant.exe";
 
 nant "-f:Commons.build" "-D:solution.global-dir=\Development\global" "-t:net-2.0" "-nologo" `
+    "-D:build.update.assembly-info=false" `
     cleantemp `
     resources debug;
 
