ec8a441b750992360663b7e90cf9639e079c1402
Michael Ketting <michael.ketting@rubicon.eu>
2007-03-20 07:35:38 +0000

    Reverted FileSystemItem: Removed Name property

--- Data/DomainObjects.UnitTests/TestDomain/FileSystemItem.cs
+++ Data/DomainObjects.UnitTests/TestDomain/FileSystemItem.cs
@@ -48,12 +48,6 @@ namespace Rubicon.Data.DomainObjects.UnitTests.TestDomain
 
     // methods and properties
 
-    public string Name
-    {
-      get { return (string) DataContainer["Name"]; }
-      set { DataContainer["Name"] = value; }
-    }
-
     public Folder ParentFolder
     {
       get { return (Folder) GetRelatedObject ("ParentFolder"); }
