b41e07a3d0417afba07fe5623e2dac6eeb3992a7
Fabian Schmied <fabian.schmied@rubicon.eu>
2007-04-20 08:30:14 +0000

    Renamed to DefaultMixinRules.cs

---
+++ Mixins/Mixins/Validation/Rules/DefaultMixinRules.cs
@@ -0,0 +1,24 @@
+using System;
+using Mixins.Definitions;
+
+namespace Mixins.Validation.Rules
+{
+  public class DefaultMixinRules : RuleSetBase
+  {
+    public override void Install (ValidatingVisitor visitor)
+    {
+      visitor.MixinRules.Add (new DelegateValidationRule<MixinDefinition> (MixinCannotBeInterface));
+      visitor.MixinRules.Add (new DelegateValidationRule<MixinDefinition> (MixinMustBePublic));
+    }
+
+    private void MixinCannotBeInterface (DelegateValidationRule<MixinDefinition>.Args args)
+    {
+      SingleMust (!args.Definition.Type.IsInterface, args.Log, args.Self);
+    }
+
+    private void MixinMustBePublic (DelegateValidationRule<MixinDefinition>.Args args)
+    {
+      SingleMust (args.Definition.Type.IsPublic, args.Log, args.Self);
+    }
+  }
+}
