7e6d26df30d090eb8e50a378b186710eb9f1bd05
Michael Ketting <michael.ketting@rubicon.eu>
2007-07-02 14:15:06 +0000

    Updated Testdata.

--- Data/DomainObjects.Web.Test/Database/CreateTestData.sql
+++ Data/DomainObjects.Web.Test/Database/CreateTestData.sql
@@ -29,6 +29,17 @@ insert into [TableWithAllDataTypes] (ID, ClassID, [Boolean], [Byte], [Date], [Da
     null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
     '{7E86BD49-6D7D-4d73-87AC-9F7C5F46DEFB}', null)
     
+insert into [TableWithAllDataTypes] (ID, ClassID, [Boolean], [Byte], [Date], [DateTime], [Decimal], [Double], [Enum], [Guid], [Int16], [Int32], [Int64], [Single], [String], [StringWithoutMaxLength], [DelimitedStringArray], [Binary],
+    [NaBoolean], [NaByte], [NaDate], [NaDateTime], [NaDecimal], [NaDouble], [NaEnum], [NaGuid], [NaInt16], [NaInt32], [NaInt64], [NaSingle], 
+    [StringWithNullValue], [DelimitedNullStringArray], [NaBooleanWithNullValue], [NaByteWithNullValue], [NaDateWithNullValue], [NaDateTimeWithNullValue], [NaDecimalWithNullValue], [NaDoubleWithNullValue], [NaEnumWithNullValue], [NaGuidWithNullValue], [NaInt16WithNullValue], [NaInt32WithNullValue], [NaInt64WithNullValue], [NaSingleWithNullValue], [NullableBinary],
+    [TableForRelationTestMandatory], [TableForRelationTestOptional]) 
+    values 
+    ('{D2146236-FBD4-4b93-A835-26563FE3F043}', 'ClassWithAllDataTypes', 
+    0, 85, '2005/01/01', '2005/01/01 17:00', 123456.789, 987654.321, 1, '{236C2DCE-43BD-45ad-BDE6-15F8C05C4B29}', 32767, 2147483647, 9223372036854775807, 6789.321, 'abcdeföäü', '12345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890', 'Arthur;Dent;42', '',
+    1, 78, '2005/02/01', '2005/02/01 05:00', 765.098, 654321.789, 2, '{19B2DFBE-B7BB-448e-8002-F4DBF6032AE8}', 12000, -2147483647, 3147483647, 12.456,
+    null, null, null, null, null, null, null, null, null, null, null, null, null, null, null,
+    '{0498C3E5-30FA-45f1-A9A1-966D4655C3C7}', '{EDCFDFDB-C7B8-4f9b-BC75-72DDB8E19C42}')
+    
 -- TableForRelationTest
 insert into [TableForRelationTest] (ID, ClassID, [Name], [TableWithAllDataTypesMandatory], [TableWithAllDataTypesOptional])
     values ('{ED85682D-CD85-4af7-972B-FAFCBD3C7699}', 'ClassForRelationTest', 'RelationTest1', '{3F647D79-0CAF-4a53-BAA7-A56831F8CE2D}', null)
