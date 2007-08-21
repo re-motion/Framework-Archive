abc80ead3780d4982101dedf29d2be8cb1e941aa
Fabian Schmied <fabian.schmied@rubicon.eu>
2007-08-21 12:32:01 +0000

    Implemented IList<T> on ObjectList<T>

--- Data/DomainObjects/ObjectList.cs
+++ Data/DomainObjects/ObjectList.cs
@@ -1,9 +1,11 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Rubicon.Data.DomainObjects
 {
   [Serializable]
-  public class ObjectList<T> : DomainObjectCollection where T : DomainObject
+  public class ObjectList<T> : DomainObjectCollection, IList<T> where T : DomainObject
   {
     public ObjectList()
         : base (typeof (T))
@@ -13,6 +15,16 @@ namespace Rubicon.Data.DomainObjects
     public ObjectList (ObjectList<T> collection, bool isCollectionReadOnly)
       : base (collection, isCollectionReadOnly)
     {
+    }
+
+    public int IndexOf (T item)
+    {
+      return base.IndexOf (item);
+    }
+
+    public void Insert (int index, T item)
+    {
+      base.Insert (index, item);
     }
 
     public new T this [int index]
@@ -29,6 +41,34 @@ namespace Rubicon.Data.DomainObjects
     public new ObjectList<T> Clone ()
     {
       return (ObjectList<T>) base.Clone();
+    }
+
+    public void Add (T item)
+    {
+      base.Add (item);
+    }
+
+    bool ICollection<T>.Contains (T item)
+    {
+      return base.ContainsObject (item);
+    }
+
+    public void CopyTo (T[] array, int arrayIndex)
+    {
+      base.CopyTo (array, arrayIndex);
+    }
+
+    public bool Remove (T item)
+    {
+      bool result = ContainsObject (item);
+      base.Remove (item);
+      return result;
+    }
+
+    public IEnumerator<T> GetEnumerator ()
+    {
+      foreach (T t in (IEnumerable) this)
+        yield return t;
     }
   }
 }
