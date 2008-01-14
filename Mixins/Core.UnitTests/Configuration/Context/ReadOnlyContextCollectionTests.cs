cdf5cb0747e8557bc2101f8a32e5b54dffa2c5e5
Fabian Schmied <fabian.schmied@rubicon.eu>
2008-01-14 14:17:53 +0000

    Added ReadOnlyContextCollection and SynchronizedReadOnlyContextCollection

---
+++ Mixins/Core.UnitTests/Configuration/Context/ReadOnlyContextCollectionTests.cs
@@ -0,0 +1,137 @@
+using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using Rubicon.Mixins.Context;
+using NUnit.Framework.SyntaxHelpers;
+using System.Collections;
+using Rubicon.Utilities;
+
+namespace Rubicon.Mixins.UnitTests.Configuration.Context
+{
+  [TestFixture]
+  public class ReadOnlyContextCollectionTests
+  {
+    private ReadOnlyContextCollection<string, int> _collection;
+
+    [SetUp]
+    public void SetUp ()
+    {
+      _collection = new ReadOnlyContextCollection<string, int> (
+          delegate (int i)
+          {
+            return i.ToString ();
+          }, new int[] { 1, 2, 3 });
+    }
+
+    [Test]
+    public void NewCollection ()
+    {
+      Assert.AreEqual (3, _collection.Count);
+    }
+
+    [Test]
+    [ExpectedException (typeof (ArgumentNullException), ExpectedMessage = "Value cannot be null.\r\nParameter name: values[0]")]
+    public void NewCollection_NullValue ()
+    {
+      new ReadOnlyContextCollection<string, string> ( delegate { return ""; }, new string[] { null });
+    }
+
+    [Test]
+    public void Contains_Key ()
+    {
+      Assert.IsTrue (_collection.Contains ("1"));
+      Assert.IsTrue (_collection.Contains ("2"));
+      Assert.IsTrue (_collection.Contains ("3"));
+      Assert.IsFalse (_collection.Contains ("4"));
+      Assert.IsFalse (_collection.Contains ("§"));
+    }
+
+    [Test]
+    public void Contains_Value ()
+    {
+      ReadOnlyContextCollection<string, int> collection = new ReadOnlyContextCollection<string, int> (
+          delegate (int i)
+          {
+            if (i > 2)
+              return ">2";
+            else
+              return i.ToString ();
+          }, new int[] { 1, 2, 3 });
+
+      Assert.IsTrue (collection.Contains (1));
+      Assert.IsTrue (collection.Contains (2));
+      Assert.IsTrue (collection.Contains (3));
+      Assert.IsFalse (collection.Contains (4));
+    }
+
+    [Test]
+    public void GetEnumerator ()
+    {
+      List<int> values = new List<int> (_collection);
+      Assert.That (values, Is.EqualTo (new int[] {1, 2, 3}));
+    }
+
+    [Test]
+    public void GetEnumerator_NonGeneric ()
+    {
+      IEnumerable collectionAsEnumerable = _collection;
+      List<object> values = new List<object> (EnumerableUtility.Cast<object> (collectionAsEnumerable));
+      Assert.That (values, Is.EqualTo (new int[] { 1, 2, 3 }));
+    }
+
+    [Test]
+    public void CopyTo ()
+    {
+      int[] values = new int[5];
+      _collection.CopyTo (values, 1);
+      Assert.That (values, Is.EqualTo (new int[] {0, 1, 2, 3, 0}));
+    }
+
+    [Test]
+    [ExpectedException (typeof (NotSupportedException))]
+    public void Add ()
+    {
+      ((ICollection<int>) _collection).Add (0);
+    }
+
+    [Test]
+    [ExpectedException (typeof (NotSupportedException))]
+    public void Clear ()
+    {
+      ((ICollection<int>) _collection).Clear ();
+    }
+
+    [Test]
+    [ExpectedException (typeof (NotSupportedException))]
+    public void Remove ()
+    {
+      ((ICollection<int>) _collection).Remove (1);
+    }
+
+    [Test]
+    public void IsReadOnly ()
+    {
+      Assert.IsTrue (((ICollection<int>) _collection).IsReadOnly);
+    }
+
+    [Test]
+    public void CopyTo_NonGeneric ()
+    {
+      object[] values = new object[5];
+      ((ICollection) _collection).CopyTo (values, 1);
+      Assert.That (values, Is.EqualTo (new object[] { null, 1, 2, 3, null }));
+    }
+
+    [Test]
+    public void IsSynchronized ()
+    {
+      Assert.IsFalse (((ICollection) _collection).IsSynchronized);
+    }
+
+    [Test]
+    public void SyncRoot ()
+    {
+      Assert.IsNotNull (((ICollection) _collection).SyncRoot);
+    }
+  }
+}
---
+++ Mixins/Core/Context/ReadOnlyContextCollection.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Rubicon.Utilities;
+
+namespace Rubicon.Mixins.Context
+{
+  public class ReadOnlyContextCollection<TKey, TValue> : ICollection<TValue>, ICollection
+  {
+    private readonly Func<TValue, TKey> _keyGenerator;
+    private readonly IDictionary<TKey, TValue> _internalCollection;
+
+    public ReadOnlyContextCollection (Func<TValue, TKey> keyGenerator, IEnumerable<TValue> values)
+    {
+      ArgumentUtility.CheckNotNull ("keyGenerator", keyGenerator);
+      _internalCollection = new Dictionary<TKey, TValue>();
+      _keyGenerator = keyGenerator;
+
+      foreach (TValue value in values)
+      {
+        ArgumentUtility.CheckNotNull ("values[" + _internalCollection.Count + "]", value);
+        _internalCollection.Add (_keyGenerator (value), value);
+      }
+    }
+
+    public virtual int Count
+    {
+      get { return _internalCollection.Count; }
+    }
+
+    public virtual bool Contains (TKey key)
+    {
+      ArgumentUtility.CheckNotNull ("key", key);
+      return _internalCollection.ContainsKey (key);
+    }
+
+    public virtual bool Contains (TValue value)
+    {
+      ArgumentUtility.CheckNotNull ("value", value);
+      TKey key = _keyGenerator (value);
+      TValue foundValue;
+      if (!_internalCollection.TryGetValue (key, out foundValue))
+        return false;
+      else
+        return value.Equals (foundValue);
+    }
+
+    public virtual IEnumerator<TValue> GetEnumerator ()
+    {
+      return _internalCollection.Values.GetEnumerator();
+    }
+
+    public virtual void CopyTo (TValue[] array, int arrayIndex)
+    {
+      _internalCollection.Values.CopyTo (array, arrayIndex);
+    }
+
+    void ICollection<TValue>.Add (TValue item)
+    {
+      throw new NotSupportedException ("This list cannot be changed.");
+    }
+
+    void ICollection<TValue>.Clear ()
+    {
+      throw new NotSupportedException ("This list cannot be changed.");
+    }
+
+    bool ICollection<TValue>.Remove (TValue item)
+    {
+      throw new NotSupportedException ("This list cannot be changed.");
+    }
+
+    bool ICollection<TValue>.IsReadOnly
+    {
+      get { return true; }
+    }
+
+    void ICollection.CopyTo (Array array, int index)
+    {
+      ((ICollection) _internalCollection.Values).CopyTo (array, index);
+    }
+
+    object ICollection.SyncRoot
+    {
+      get { return ((ICollection)_internalCollection).SyncRoot; }
+    }
+
+    bool ICollection.IsSynchronized
+    {
+      get { return false; }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator ()
+    {
+      return GetEnumerator();
+    }
+  }
+}
