794e828cee7a54f0028c4e66dd1751b3e0256a96
Fabian Schmied <fabian.schmied@rubicon.eu>
2008-03-14 14:57:55 +0000

    Updated docs

--- Mixins/Core/IgnoresClassAttribute.cs
+++ Mixins/Core/IgnoresClassAttribute.cs
@@ -7,8 +7,15 @@ namespace Rubicon.Mixins
   /// the declarative configuration attributes <see cref="UsesAttribute"/>, <see cref="ExtendsAttribute"/>, and <see cref="MixAttribute"/>.
   /// </summary>
   /// <remarks>
+  /// <para>
   /// Use this attribute to exclude a target class inherited from a mixin's base class. This attribute is not inherited, so the target class
   /// exclusion will only work for the exact mixin to which the attribute is applied.
+  /// </para>
+  /// <para>
+  /// Note that when a mixin excludes a generic type definition (e.g. <c>C&lt;&gt;</c>), a corresponding closed generic type (<c>C&lt;int&gt;</c>) can
+  /// still inherit the mixin from its base class. This is by design due to the rule that a closed generic type inherits mixins from both 
+  /// its base class and its generic type definition.
+  /// </para>
   /// </remarks>
   [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
   public class IgnoresClassAttribute : Attribute
--- Mixins/Core/IgnoresMixinAttribute.cs
+++ Mixins/Core/IgnoresMixinAttribute.cs
@@ -7,8 +7,15 @@ namespace Rubicon.Mixins
   /// the declarative configuration attributes <see cref="UsesAttribute"/>, <see cref="ExtendsAttribute"/>, and <see cref="MixAttribute"/>.
   /// </summary>
   /// <remarks>
+  /// <para>
   /// Use this attribute to exclude a mixin that is configured to be applied to a base class. This attribute is not inherited, so the mixin
   /// exclusion will only work for the exact mixin to which the attribute is applied.
+  /// </para>
+  /// <para>
+  /// Note that when a generic type definition (e.g. <c>C&lt;&gt;</c>) excludes a mixin, a corresponding closed generic class (<c>C&lt;int&gt;</c>) can
+  /// still inherit the mixin from its base class. This is by design due to the rule that a closed generic type inherits mixins from both 
+  /// its base class and its generic type definition.
+  /// </para>
   /// </remarks>
   [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
   public class IgnoresMixinAttribute : Attribute
