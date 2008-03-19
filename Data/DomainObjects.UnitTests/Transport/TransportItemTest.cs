db6b8f04e93d3dfdf7b669cc854957fab3afe5d4
Fabian Schmied <fabian.schmied@rubicon.eu>
2008-03-19 18:06:43 +0000

    Added XML serialization support to TransportItem

--- Data/DomainObjects.UnitTests/Transport/TransportItemTest.cs
+++ Data/DomainObjects.UnitTests/Transport/TransportItemTest.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Xml.Serialization;
 using NUnit.Framework;
 using Rubicon.Data.DomainObjects.Transport;
 using Rubicon.Data.DomainObjects.UnitTests.TestDomain;
+using Rubicon.Development.UnitTesting;
+using System.IO;
 
 namespace Rubicon.Data.DomainObjects.UnitTests.Transport
 {
@@ -20,13 +23,64 @@ namespace Rubicon.Data.DomainObjects.UnitTests.Transport
     {
       DataContainer container = Computer.GetObject (DomainObjectIDs.Computer1).InternalDataContainer;
       TransportItem item = TransportItem.PackageDataContainer (container);
-      Assert.AreEqual (DomainObjectIDs.Computer1, item.ID);
-      Assert.AreEqual (container.PropertyValues.Count, item.Properties.Count);
-      foreach (PropertyValue propertyValue in container.PropertyValues)
+
+      CheckEqualData(container, item);
+    }
+
+    [Test]
+    public void Serializable ()
+    {
+      DataContainer container = Computer.GetObject (DomainObjectIDs.Computer1).InternalDataContainer;
+      TransportItem item = TransportItem.PackageDataContainer (container);
+      TransportItem deserializedItem = Serializer.SerializeAndDeserialize (item);
+
+      CheckEqualData (container, deserializedItem);
+    }
+
+    [Test]
+    public void XmlSerializable_ID ()
+    {
+      DataContainer container = Computer.GetObject (DomainObjectIDs.Computer1).InternalDataContainer;
+      TransportItem item = TransportItem.PackageDataContainer (container);
+      TransportItem deserializedItem;
+      using (MemoryStream stream = new MemoryStream ())
+      {
+        XmlSerializer serializer = new XmlSerializer (typeof (TransportItem));
+        serializer.Serialize (stream, item);
+        stream.Seek (0, SeekOrigin.Begin);
+        deserializedItem = (TransportItem) serializer.Deserialize (stream);
+      }
+
+      Assert.AreEqual (container.ID, deserializedItem.ID);
+    }
+
+    [Test]
+    public void XmlSerializable_Properties ()
+    {
+      DataContainer container = Computer.GetObject (DomainObjectIDs.Computer1).InternalDataContainer;
+      TransportItem item = TransportItem.PackageDataContainer (container);
+      TransportItem deserializedItem;
+      using (MemoryStream stream = new MemoryStream ())
+      {
+        XmlSerializer serializer = new XmlSerializer (typeof (TransportItem));
+        serializer.Serialize (stream, item);
+        stream.Seek (0, SeekOrigin.Begin);
+        deserializedItem = (TransportItem) serializer.Deserialize (stream);
+      }
+
+      CheckEqualData (container, deserializedItem);
+    }
+
+    private void CheckEqualData (DataContainer expectedData, TransportItem item)
+    {
+      Assert.AreEqual (expectedData.ID, item.ID);
+      Assert.AreEqual (expectedData.PropertyValues.Count, item.Properties.Count);
+      foreach (PropertyValue propertyValue in expectedData.PropertyValues)
       {
         Assert.IsTrue (item.Properties.ContainsKey (propertyValue.Name));
         Assert.AreEqual (propertyValue.Value, item.Properties[propertyValue.Name]);
       }
     }
+
   }
 }
--- Data/DomainObjects/Transport/TransportItem.cs
+++ Data/DomainObjects/Transport/TransportItem.cs
@@ -1,10 +1,13 @@
 using System;
 using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
 
 namespace Rubicon.Data.DomainObjects.Transport
 {
   [Serializable]
-  public struct TransportItem
+  public struct TransportItem : IXmlSerializable
   {
     public static TransportItem PackageDataContainer (DataContainer container)
     {
@@ -14,14 +17,105 @@ namespace Rubicon.Data.DomainObjects.Transport
       return item;
     }
 
-    public readonly ObjectID ID;
-    public readonly Dictionary<string, object> Properties;
-
+    private ObjectID _id;
+    private Dictionary<string, object> _properties;
 
     public TransportItem (ObjectID id)
     {
-      ID = id;
-      Properties = new Dictionary<string, object>();
+      _id = id;
+      _properties = new Dictionary<string, object>();
     }
+
+    public ObjectID ID
+    {
+      get { return _id; }
+    }
+
+    public Dictionary<string, object> Properties
+    {
+      get { return _properties; }
+    }
+
+    XmlSchema IXmlSerializable.GetSchema ()
+    {
+      return null;
+    }
+
+    void IXmlSerializable.WriteXml (XmlWriter writer)
+    {
+      writer.WriteAttributeString ("ID", ID.ToString ());
+
+      writer.WriteStartElement ("Properties");
+      writer.WriteAttributeString ("Count", Properties.Count.ToString());
+      foreach (KeyValuePair<string, object> property in Properties)
+        SerializeProperty (writer, property);
+      writer.WriteEndElement ();
+    }
+
+    void IXmlSerializable.ReadXml (XmlReader reader)
+    {
+      string idString = reader.GetAttribute ("ID");
+      _id = ObjectID.Parse (idString);
+
+      reader.Read ();
+
+      List<KeyValuePair<string, object>> properties = DeserializeProperties (reader);
+
+      _properties = new Dictionary<string, object> ();
+      for (int i = 0; i < properties.Count; ++i)
+      {
+        Properties.Add (properties[i].Key, properties[i].Value);
+      }
+    }
+
+    private void SerializeProperty (XmlWriter writer, KeyValuePair<string, object> property)
+    {
+      writer.WriteStartElement ("Property");
+      writer.WriteAttributeString ("Name", property.Key);
+      SerializePropertyValue (writer, property.Value);
+      writer.WriteEndElement ();
+    }
+
+    private void SerializePropertyValue (XmlWriter writer, object value)
+    {
+      ObjectID objectID;
+      if ((objectID = value as ObjectID) != null)
+      {
+        writer.WriteAttributeString ("ObjectID", "true");
+        writer.WriteString (objectID.ToString ());
+      }
+      else
+        writer.WriteValue (value);
+    }
+
+    private List<KeyValuePair<string, object>> DeserializeProperties (XmlReader reader)
+    {
+      int propertyCount = int.Parse (reader.GetAttribute ("Count"));
+      reader.ReadStartElement ("Properties");
+      List<KeyValuePair<string, object>> properties = new List<KeyValuePair<string, object>> (propertyCount);
+      for (int i = 0; i < propertyCount; ++i)
+        properties.Add (DeserializeProperty (reader));
+      reader.ReadEndElement ();
+      return properties;
+    }
+
+    private KeyValuePair<string, object> DeserializeProperty (XmlReader reader)
+    {
+      string name = reader.GetAttribute ("Name");
+      string objectIDAttribute = reader.GetAttribute ("ObjectID");
+
+      reader.ReadStartElement ("Property");
+      object value;
+      if (objectIDAttribute != null)
+      {
+        string objectIDString = reader.ReadContentAsString ();
+        value = ObjectID.Parse (objectIDString);
+      }
+      else
+        value = reader.ReadContentAsObject ();
+      reader.ReadEndElement ();
+      return new KeyValuePair<string, object> (name, value);
+    }
+
   }
 }
