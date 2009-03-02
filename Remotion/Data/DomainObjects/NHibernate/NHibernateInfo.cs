2ecb1e23d17e858e451c585138047d67479b4c26
Markus Giegl <markus.giegl@rubicon.eu>
2009-03-02 19:06:20 +0000

    re-linqing NHibernate: RelinquingHql_FromSelectWhereLinqQueryToHqlTest working

--- Remotion/Data/DomainObjects/NHibernate/NHibernateInfo.cs
+++ Remotion/Data/DomainObjects/NHibernate/NHibernateInfo.cs
@@ -23,12 +23,53 @@ namespace Remotion.Data.DomainObjects.NHibernate
 
     public string GetRelatedTableName (MemberInfo relationMember)
     {
-      throw new System.NotImplementedException();
+      //Tuple<RelationDefinition, ClassDefinition, string> relationData = GetRelationData (relationMember);
+      //if (relationData == null)
+      //  return null;
+      //RelationDefinition relationDefinition = relationData.A;
+      //ClassDefinition classDefinition = relationData.B;
+      //string propertyIdentifier = relationData.C;
+      //return relationDefinition.GetOppositeClassDefinition (classDefinition.ID, propertyIdentifier).GetEntityName ();
+      
+      // TODO RELINQUING NHIBERNATE: Return related table if available
+      return null;
     }
 
     public string GetColumnName (MemberInfo member)
     {
-      throw new System.NotImplementedException();
+      //ArgumentUtility.CheckNotNull ("member", member);
+      //PropertyInfo property = member as PropertyInfo;
+      //if (property == null)
+      //  return null;
+      //if (property.Name == "ID" && property.DeclaringType == typeof (DomainObject))
+      //  return "ID";
+
+      //ClassDefinition classDefinition = MappingConfiguration.Current.ClassDefinitions[property.DeclaringType];
+      //if (classDefinition == null)
+      //  return null;
+      //string propertyIdentifier = MappingConfiguration.Current.NameResolver.GetPropertyName (property);
+      //PropertyDefinition propertyDefinition = classDefinition.GetPropertyDefinition (propertyIdentifier);
+      //if (propertyDefinition != null)
+      //  return propertyDefinition.StorageSpecificName;
+      //else
+      //  return null;
+
+      ArgumentUtility.CheckNotNull ("member", member);
+      PropertyInfo property = member as PropertyInfo;
+      if (property == null)
+      {
+        return null;
+      }
+      // TODO RELINQUING NHIBERNATE: Make sure "Id" is correct in all cases (id, ID ?)
+      else if (property.Name == "Id" && property.DeclaringType == typeof (DomainObject))
+      {
+        return "Id";
+      }
+      else
+      {
+        // TODO RELINQUING NHIBERNATE: Use MappingConfiguration as in DatabaseInfo class
+        return property.Name;
+      }
     }
 
     public Tuple<string, string> GetJoinColumnNames (MemberInfo relationMember)
@@ -38,12 +79,26 @@ namespace Remotion.Data.DomainObjects.NHibernate
 
     public object ProcessWhereParameter (object parameter)
     {
-      throw new System.NotImplementedException();
+      //DomainObject domainObject = parameter as DomainObject;
+      //if (domainObject != null)
+      //  return domainObject.ID;
+      //return parameter;
+
+      // TODO RELINQUING NHIBERNATE: return DO Id  as in DatabaseInfo class
+      return parameter;
     }
 
     public MemberInfo GetPrimaryKeyMember (Type entityType)
     {
-      throw new System.NotImplementedException();
+      //ClassDefinition classDefinition = MappingConfiguration.Current.ClassDefinitions[entityType];
+      //if (classDefinition == null)
+      //  return null;
+      //else
+      //  return typeof (DomainObject).GetProperty ("ID");
+
+      //throw new System.NotImplementedException();
+      // TODO RELINQUING NHIBERNATE: Return primary key if available
+      return null;
     }
 
     public bool IsTableType (Type type)
