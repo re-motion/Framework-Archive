05af166600ee93def0b1871ec50a755f074cab2c
Julian Lettner <julian.lettner@rubicon.eu>
2012-02-16 14:29:18 +0000

    Added TODO comment (RM-4641)

--- Remotion/Core/Core/BridgeInterfaces/IAdapterRegistryImplementation.cs
+++ Remotion/Core/Core/BridgeInterfaces/IAdapterRegistryImplementation.cs
@@ -19,6 +19,7 @@ using Remotion.ServiceLocation;
 
 namespace Remotion.BridgeInterfaces
 {
+  // TODO 4614: Will be removed when AdapterRegistry is replaced with ServiceLocator.
   [ConcreteImplementation ("Remotion.BridgeImplementations.AdapterRegistryImplementation, Remotion, Version=<version>, Culture=neutral, PublicKeyToken=<publicKeyToken>",
     Lifetime = LifetimeKind.Singleton)]
   public interface IAdapterRegistryImplementation
