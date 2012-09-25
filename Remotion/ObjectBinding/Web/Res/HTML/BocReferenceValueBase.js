bc3a23e734dcd4e10a48e19403118679c28ac512
Michael Ketting <michael.ketting@rubicon.eu>
2012-09-25 15:09:17 +0000

    Refactored BocReferenceValueBase for dynamic icon. (RM-4985)

--- Remotion/ObjectBinding/Web/Res/HTML/BocReferenceValueBase.js
+++ Remotion/ObjectBinding/Web/Res/HTML/BocReferenceValueBase.js
@@ -59,7 +59,7 @@ BocReferenceValueBase.UpdateCommand = function (oldCommand, businessObject, icon
         params,
         function (result, context, methodName)
         {
-          BocReferenceValueBase.UpdateIconFromWebService(newIcon, result);
+          BocReferenceValueBase.UpdateIconFromWebService(newCommand, newIcon, result);
         },
         function (err, context, methodName)
         {
@@ -114,14 +114,7 @@ BocReferenceValueBase.CreateCommand = function (oldCommand, commandInfo, busines
       newCommand.attr(property, value);
   }
 
-  if (newCommand.attr('href'))
-  {
-    newCommand.addClass('hasIcon');
-  }
-  else
-  {
-    newCommand.removeClass('hasIcon');
-  }
+  newCommand.removeClass('hasIcon');
 
   return newCommand;
 };
@@ -141,7 +134,7 @@ BocReferenceValueBase.CreateEmptyIcon = function (oldIcon, title)
   return newIcon;
 };
 
-BocReferenceValueBase.UpdateIconFromWebService = function (icon, iconInformation)
+BocReferenceValueBase.UpdateIconFromWebService = function (command, icon, iconInformation)
 {
   ArgumentUtility.CheckNotNull('icon', icon);
   ArgumentUtility.CheckTypeIsObject('iconInformation', iconInformation);
@@ -159,6 +152,8 @@ BocReferenceValueBase.UpdateIconFromWebService = function (icon, iconInformation
     icon.attr({ title: iconInformation.ToolTip });
 
   icon.css({ width: iconInformation.Width, heght: iconInformation.Height });
+
+  command.addClass('hasIcon');
 };
 
 BocReferenceValueBase.ResetCommand = function (command)
@@ -168,4 +163,5 @@ BocReferenceValueBase.ResetCommand = function (command)
   command.removeAttr('onclick');
   command.removeAttr('title');
   command.removeAttr('target');
+  command.removeClass('hasIcon');
 };
