b7473fe38d1155bd8cf8f4ec766a895dce60a416
Fabian Schmied <fabian.schmied@rubicon.eu>
2013-04-24 07:53:40 +0000

    Fixed child context for SqlCaseExpression. (RM-4878, RM-3110)

--- Remotion/Data/UnitTests/DomainObjects/Core/Linq/IntegrationTests/SelectIntegrationTest.cs
+++ Remotion/Data/UnitTests/DomainObjects/Core/Linq/IntegrationTests/SelectIntegrationTest.cs
@@ -107,9 +107,13 @@ namespace Remotion.Data.UnitTests.DomainObjects.Core.Linq.IntegrationTests
       CheckQueryResult (query, DomainObjectIDs.Employee2);
     }
 
-
     [Test]
-    [Ignore ("TODO 4878")]
+    [ExpectedException (typeof (NotSupportedException), ExpectedMessage = 
+        "There was an error preparing or resolving query "
+        + "'from Employee e in DomainObjectQueryable<Employee> "
+        + "where (([e].Computer ?? Convert([e])).ID = Employee|c3b2bbc3-e083-4974-bac7-9cee1fb85a5e|System.Guid) select [e]' for SQL generation. "
+        + "Cannot use a complex expression ('new ObjectID(ClassID = [t1].[ClassID] AS ClassID, Value = [t1].[ID] AS Value)') in a place where SQL "
+        + "requires a single value.")]
     public void CoalesceExpression_CompoundMember ()
     {
       var query = from e in QueryFactory.CreateLinqQuery<Employee> ()
@@ -130,7 +134,12 @@ namespace Remotion.Data.UnitTests.DomainObjects.Core.Linq.IntegrationTests
     }
 
     [Test]
-    [Ignore ("TODO 4878")]
+    [ExpectedException (typeof (NotSupportedException), ExpectedMessage = 
+        "There was an error preparing or resolving query "
+        + "'from Employee e in DomainObjectQueryable<Employee> "
+        + "where (IIF(([e].Computer.ID = Computer|c7c26bf5-871d-48c7-822a-e9b05aac4e5a|System.Guid), Convert([e].Computer), Convert([e])).ID = "
+        + "Computer|c7c26bf5-871d-48c7-822a-e9b05aac4e5a|System.Guid) select [e]' for SQL generation. Cannot use a complex expression "
+        + "('new ObjectID(ClassID = [t1].[ClassID] AS ClassID, Value = [t1].[ID] AS Value)') in a place where SQL requires a single value.")]
     public void ConditionalExpression_CompoundMember ()
     {
       var query = from e in QueryFactory.CreateLinqQuery<Employee> ()
