27912760f53c1f3c98f42ed1a02f37ccf507bed7
Julian Lettner <julian.lettner@rubicon.eu>
2013-07-10 13:34:08 +0000

    Refined test. (RM-5370)

--- Remotion/Mixins/UnitTests/Core/MixerTools/MixerPipelineFactoryTest.cs
+++ Remotion/Mixins/UnitTests/Core/MixerTools/MixerPipelineFactoryTest.cs
@@ -16,6 +16,10 @@
 // 
 using NUnit.Framework;
 using Remotion.Mixins.MixerTools;
+using Remotion.Mixins.UnitTests.Core.TestDomain;
+using Remotion.Reflection.TypeDiscovery;
+using Remotion.ServiceLocation;
+using Remotion.TypePipe;
 
 namespace Remotion.Mixins.UnitTests.Core.MixerTools
 {
@@ -35,16 +39,30 @@ namespace Remotion.Mixins.UnitTests.Core.MixerTools
     {
       var pipeline = _factory.CreatePipeline (@"c:\directory");
 
-      // TODO 5370: Inspect settings copied from default pipeline.
+      CheckRemotionPipelineFactoryWasUsedForCreation (pipeline);
 
-      Assert.That (pipeline.CodeManager.AssemblyDirectory, Is.EqualTo (@"c:\directory"));
+      var defaultPipeline = SafeServiceLocator.Current.GetInstance<IPipelineRegistry>().DefaultPipeline;
+      Assert.That (pipeline.ParticipantConfigurationID, Is.EqualTo (defaultPipeline.ParticipantConfigurationID));
+      Assert.That (pipeline.Settings, Is.SameAs (defaultPipeline.Settings));
+      Assert.That (pipeline.Participants, Is.EqualTo (defaultPipeline.Participants));
+
       Assert.That (pipeline.CodeManager.AssemblyNamePattern, Is.EqualTo ("Assembly"));
+      Assert.That (pipeline.CodeManager.AssemblyDirectory, Is.EqualTo (@"c:\directory"));
     }
 
     [Test]
     public void GetModulePath ()
     {
       Assert.That (_factory.GetModulePath (@"c:\directory"), Is.EqualTo (@"c:\directory\Assembly.dll"));
+    }
+
+    private void CheckRemotionPipelineFactoryWasUsedForCreation (IPipeline pipeline)
+    {
+      var assembledType = pipeline.ReflectionService.GetAssembledType (typeof (TargetClassForGlobalMix));
+      var assembly = assembledType.Assembly;
+
+      // RemotionPipelineFactory will add the [NonApplicationAssemblyAttribute] to the generated assembly.
+      Assert.That (assembly.IsDefined (typeof (NonApplicationAssemblyAttribute), inherit: false), Is.True);
     }
   }
 }
