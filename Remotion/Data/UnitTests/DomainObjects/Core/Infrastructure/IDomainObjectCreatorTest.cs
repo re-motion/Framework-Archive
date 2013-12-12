9a15fcaa6eed70d3a73f86411d239371f9493640
Tymon Terlikiewicz <tymon.terlikiewicz@rubicon.eu>
2013-12-12 11:54:08 +0000

    added missing interface tests

---
+++ Remotion/Data/UnitTests/DomainObjects/Core/Infrastructure/IDomainObjectCreatorTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using NUnit.Framework;
+using Remotion.Data.DomainObjects.Infrastructure;
+using Remotion.Data.DomainObjects.Infrastructure.TypePipe;
+using Remotion.ServiceLocation;
+
+namespace Remotion.Data.UnitTests.DomainObjects.Core.Infrastructure
+{
+  public class IDomainObjectCreatorTest
+  {
+    private DefaultServiceLocator _serviceLocator;
+
+    [SetUp]
+    public void SetUp ()
+    {
+      _serviceLocator = new DefaultServiceLocator();
+    }
+
+    [Test]
+    public void GetInstance_Once ()
+    {
+      var factory = _serviceLocator.GetInstance<IDomainObjectCreator>();
+
+      Assert.That (factory, Is.Not.Null);
+      Assert.That (factory, Is.TypeOf (typeof (DomainObjectCreator)));
+    }
+
+    [Test]
+    public void GetInstance_Twice_ReturnsSameInstance ()
+    {
+      var factory1 = _serviceLocator.GetInstance<IDomainObjectCreator>();
+      var factory2 = _serviceLocator.GetInstance<IDomainObjectCreator>();
+
+      Assert.That (factory1, Is.SameAs (factory2));
+    }
+  }
+}
---
+++ Remotion/Web/UnitTests/Core/Context/ISafeContextStorageProviderTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using Remotion.Context;
+using Remotion.ServiceLocation;
+using Remotion.Web.Context;
+
+namespace Remotion.Web.UnitTests.Core.Context
+{
+  [TestFixture]
+  public class ISafeContextStorageProviderTest
+  {
+    private DefaultServiceLocator _serviceLocator;
+
+    [SetUp]
+    public void SetUp ()
+    {
+      _serviceLocator = new DefaultServiceLocator();
+    }
+
+    [Test]
+    public void GetInstance_Once ()
+    {
+      var instances = _serviceLocator.GetAllInstances<ISafeContextStorageProvider>();
+
+      Assert.That (instances, Is.Not.Empty);
+      Assert.That (instances.First(), Is.InstanceOf (typeof (HttpContextStorageProvider)));
+    }
+
+    [Test]
+    public void GetInstance_Twice_ReturnsDifferentInstance ()
+    {
+      var instance1 = _serviceLocator.GetAllInstances<ISafeContextStorageProvider>().First();
+      var instance2 = _serviceLocator.GetAllInstances<ISafeContextStorageProvider>().First();
+
+      Assert.That (instance1, Is.Not.SameAs (instance2));
+    }
+  }
+}
