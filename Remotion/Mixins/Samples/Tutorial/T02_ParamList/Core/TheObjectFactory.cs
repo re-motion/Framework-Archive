701fb9001063324b10639dadaafcb43a4ef44fff
Michael Ketting <michael.ketting@rubicon.eu>
2013-12-23 17:53:01 +0000

    Dropped MethodCaller. Moved ConstructorLookupInfo from Remotion.dll to Remotion.TypePipe.dll. Copied MemberLookupInfo from Remotion.dll to Remotion.TypePipe.dll. (RM-5963)

--- Remotion/Mixins/Samples/Tutorial/T02_ParamList/Core/TheObjectFactory.cs
+++ Remotion/Mixins/Samples/Tutorial/T02_ParamList/Core/TheObjectFactory.cs
@@ -14,8 +14,8 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with re-motion; if not, see http://www.gnu.org/licenses.
 // 
-using Remotion.Reflection;
 using Remotion.TypePipe;
+using Remotion.TypePipe.Implementation;
 
 namespace Remotion.Mixins.Samples.Tutorial.T02_ParamList.Core
 {
