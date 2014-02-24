660c824ca1dbe7285b1d4ac75a6a0e5884cc8639
Michael Ketting <michael.ketting@rubicon.eu>
2014-02-24 10:22:22 +0000

    temp commit: Introduce a cache to AssemblyFinderTypeDiscoveryservice (RM-5928)

--- Remotion/Core/UnitTests/Configuration/TypeDiscovery/FakeRootAssemblyFinder.cs
+++ Remotion/Core/UnitTests/Configuration/TypeDiscovery/FakeRootAssemblyFinder.cs
@@ -15,6 +15,7 @@
 // along with re-motion; if not, see http://www.gnu.org/licenses.
 // 
 using System;
+using System.Collections.Generic;
 using Remotion.Reflection.TypeDiscovery.AssemblyFinding;
 using Remotion.Reflection.TypeDiscovery.AssemblyLoading;
 
@@ -22,7 +23,7 @@ namespace Remotion.UnitTests.Configuration.TypeDiscovery
 {
   public class FakeRootAssemblyFinder : IRootAssemblyFinder
   {
-    public RootAssembly[] FindRootAssemblies ()
+    public IEnumerable<RootAssembly> FindRootAssemblies ()
     {
       throw new NotImplementedException();
     }
